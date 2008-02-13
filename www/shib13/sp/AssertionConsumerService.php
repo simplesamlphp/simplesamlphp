a9e5fe40554a2f5130a27f242679a6eaa0d4be98
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2008-02-13 12:54:01 +0000

    made shib13 acs create a new session to get error handling with track id properly in case of failure.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@290 44740490-163a-0410-bde0-09ae8108e29a

--- www/shib13/sp/AssertionConsumerService.php
+++ www/shib13/sp/AssertionConsumerService.php
@@ -10,16 +10,20 @@ require_once('SimpleSAML/XML/Shib13/AuthnRequest.php');
 require_once('SimpleSAML/Bindings/Shib13/HTTPPost.php');
 require_once('SimpleSAML/XHTML/Template.php');
 
+
+$config = SimpleSAML_Configuration::getInstance();
+$metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
+$session = SimpleSAML_Session::getInstance(true);
+
 try {
 	
-	$config = SimpleSAML_Configuration::getInstance();
-	$metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
+
 	
 	$binding = new SimpleSAML_Bindings_Shib13_HTTPPost($config, $metadata);
 	$authnResponse = $binding->decodeResponse($_POST);
 
 	$authnResponse->validate();
-	$session = $authnResponse->createSession(true);
+	$session = $authnResponse->createSession();
 
 	if (isset($session)) {
 		$relayState = $authnResponse->getRelayState();
