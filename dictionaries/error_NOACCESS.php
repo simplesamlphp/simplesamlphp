6ac9842f76b48c81f7281a215b3bb8c997be97d8
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2008-02-14 13:33:02 +0000

    Adding a error dictionary for no access to endpoint error message
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@300 44740490-163a-0410-bde0-09ae8108e29a

---
+++ dictionaries/error_NOACCESS.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang = array(
+	'en'	=>	array(
+		'title'	=>	'No Access',
+		'descr'	=>	'This endpoint is not enabled. Check the enable options in your configuration of simpleSAMLphp.'
+	)
+);
