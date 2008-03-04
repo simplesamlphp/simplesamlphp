8d3e38eb7dadd73571bb41d6f4506e25abbaf18c
Olav Morken <olav.morken@uninett.no>
2008-03-04 13:17:19 +0000

    MetaDataStorageHandler: Changed to support chaining of metadata sources.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@346 44740490-163a-0410-bde0-09ae8108e29a

--- lib/SimpleSAML/Metadata/MetaDataStorageHandlerFlatfile.php
+++ lib/SimpleSAML/Metadata/MetaDataStorageHandlerFlatfile.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once('SimpleSAML/Configuration.php');
-require_once('SimpleSAML/Utilities.php');
-require_once('SimpleSAML/Metadata/MetaDataStorageHandler.php');
+require_once('SimpleSAML/Metadata/MetaDataStorageSource.php');
 
 /**
- * This file defines a flat file metadata handler.
+ * This file defines a flat file metadata source.
  * Instantiation of session handler objects should be done through
  * the class method getMetadataHandler().
  *
@@ -13,77 +12,119 @@ require_once('SimpleSAML/Metadata/MetaDataStorageHandler.php');
  * @package simpleSAMLphp
  * @version $Id$
  */
-class SimpleSAML_Metadata_MetaDataStorageHandlerFlatFile extends SimpleSAML_Metadata_MetaDataStorageHandler {
+class SimpleSAML_Metadata_MetaDataStorageHandlerFlatFile extends SimpleSAML_Metadata_MetaDataStorageSource {
 
-
-
-
-
-	/* This constructor is included in case it is needed in the the
-	 * future. Including it now allows us to write parent::__construct() in
-	 * the subclasses of this class.
+	/**
+	 * This is the valid metadata sets we know about.
 	 */
-	protected function __construct() {
+	private static $validSets = array(
+		'saml20-sp-hosted', 'saml20-sp-remote','saml20-idp-hosted', 'saml20-idp-remote',
+		'shib13-sp-hosted', 'shib13-sp-remote', 'shib13-idp-hosted', 'shib13-idp-remote',
+		'openid-provider'
+		);
+
+
+	/**
+	 * This is the directory we will load metadata files from. The path will always end
+	 * with a '/'.
+	 */
+	private $directory;
+
+
+	/**
+	 * This is an associative array which stores the different metadata sets we have loaded.
+	 */
+	private $cachedMetadata = array();
+
+
+	/**
+	 * This constructor initializes the flatfile metadata storage handler with the
+	 * specified configuration. The configuration is an associative array with the following
+	 * possible elements:
+	 * - 'directory': The directory we should load metadata from. The default directory is
+	 *                set in the 'metadatadir' configuration option in 'config.php'.
+	 *
+	 * @param $config  An associtive array with the configuration for this handler.
+	 */
+	protected function __construct($config) {
+		assert('is_array($config)');
+
+		/* Get the configuration. */
+		$globalConfig = SimpleSAML_Configuration::getInstance();
+
+
+		/* Find the path to the directory we should search for metadata in. */
+		if(array_key_exists('directory', $config)) {
+			$this->directory = $config['directory'];
+		} else {
+			$this->directory = $globalConfig->getValue('metadatadir', 'metadata/');
+		}
+
+		/* Resolve this directory relative to the simpleSAMLphp directory (unless it is
+		 * an absolute path).
+		 */
+		$this->directory = $globalConfig->resolvePath($this->directory) . '/';
 	}
 
 
+	/**
+	 * This function loads the given set of metadata from a file our metadata directory.
+	 * This function returns NULL if it is unable to locate the given set in the metadata directory.
+	 *
+	 * @param $set  The set of metadata we are loading.
+	 * @return Associative array with the metadata, or NULL if we are unable to load metadata from the given file.
+	 */
+	private function load($set) {
 
-	public function load($set) {
-		$metadata = null;
-		if (!in_array($set, array(
-			'saml20-sp-hosted', 'saml20-sp-remote','saml20-idp-hosted', 'saml20-idp-remote',
-			'shib13-sp-hosted', 'shib13-sp-remote', 'shib13-idp-hosted', 'shib13-idp-remote',
-			'openid-provider'))) {
-				throw new Exception('Trying to load illegal set of Meta data [' . $set . ']');
-		}
-		
-		/* Get the configuration. */
-		$config = SimpleSAML_Configuration::getInstance();
-		assert($config instanceof SimpleSAML_Configuration);
-		
-		$metadatasetfile = $config->getPathValue('metadatadir') . $set . '.php';
-		
+		$metadatasetfile = $this->directory . $set . '.php';
+
 		if (!file_exists($metadatasetfile)) {
-			throw new Exception('Could not open file: ' . $metadatasetfile);
+			return NULL;
 		}
+
+		$metadata = array();
+
 		include($metadatasetfile);
-		
+
 		if (!is_array($metadata)) {
 			throw new Exception('Could not load metadata set [' . $set . '] from file: ' . $metadatasetfile);
 		}
-		foreach ($metadata AS $key => $entry) { 
-			$this->metadata[$set][$key] = $entry;
-			$this->metadata[$set][$key]['entityid'] = $key;
-			
-			if (isset($entry['host'])) {
-				$this->hostmap[$set][$entry['host']] = $key;
-			}
-			
-		}
 
+		return $metadata;
 	}
 
-	
-	public function getMetaData($entityid = null, $set = 'saml20-sp-hosted') {
-		if (!isset($entityid)) {
-			return $this->getMetaDataCurrent($set);
+
+	/**
+	 * This function retrieves the given set of metadata. It will return an empty array if it is
+	 * unable to locate it.
+	 *
+	 * @param $set  The set of metadata we are retrieving.
+	 * @return Asssociative array with the metadata. Each element in the array is an entity, and the
+	 *         key is the entity id.
+	 */
+	public function getMetadataSet($set) {
+		assert('in_array($set, self::$validSets)');
+
+		if(array_key_exists($set, $this->cachedMetadata)) {
+			return $this->cachedMetadata[$set];
 		}
-		
-		//echo 'find metadata for entityid [' . $entityid . '] in metadata set [' . $set . ']';
-		
-		if (!isset($this->metadata[$set])) {
-			$this->load($set);
+
+		$metadataSet = $this->load($set);
+		if($metadataSet === NULL) {
+			$metadataSet = array();
 		}
-		if (!isset($this->metadata[$set][$entityid]) ) {
-			throw new Exception('Could not find metadata for entityid [' . $entityid . '] in metadata set [' . $set . ']');
+
+		/* Add the entity id of an entry to each entry in the metadata. */
+		foreach ($metadataSet AS $entityId => &$entry) {
+			$entry['entityid'] = $entityId;
 		}
-		return $this->metadata[$set][$entityid];
+
+		$this->cachedMetadata[$set] = $metadataSet;
+
+		return $metadataSet;
 	}
-	
 
 
-	
-	
 }
 
 ?>
---
+++ lib/SimpleSAML/Metadata/MetaDataStorageSource.php
@@ -0,0 +1,120 @@
+<?php
+
+require_once('SimpleSAML/Metadata/MetaDataStorageHandlerFlatfile.php');
+
+/**
+ * This abstract class defines an interface for metadata storage sources.
+ *
+ * It also contains the overview of the different metadata storage sources.
+ * A metadata storage source can be loaded by passing the configuration of it
+ * to the getSource static function.
+ *
+ * @author Olav Morken, UNINETT AS.
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+abstract class SimpleSAML_Metadata_MetaDataStorageSource {
+
+
+	/**
+	 * This function creates a metadata source based on the given configuration.
+	 * The type of source is based on the 'type' parameter in the configuration.
+	 * The default type is 'flatfile'.
+	 *
+	 * @param $sourceConfig  Associative array with the configuration for this metadata source.
+	 * @return An instance of a metadata source with the given configuration.
+	 */
+	public static function getSource($sourceConfig) {
+
+		assert(is_array($sourceConfig));
+
+		if(array_key_exists('type', $sourceConfig)) {
+			$type = $sourceConfig['type'];
+		} else {
+			$type = 'flatfile';
+		}
+
+		switch($type) {
+		case 'flatfile':
+			return new SimpleSAML_Metadata_MetaDataStorageHandlerFlatFile($sourceConfig);
+		default:
+			throw new Exception('Invalid metadata source type: "' . $type . '".');
+		}
+	}
+
+
+	/**
+	 * This function attempts to generate an associative array with metadata for all entities in the
+	 * given set. The key of the array is the entity id.
+	 *
+	 * A subclass should override this function if it is able to easily generate this list.
+	 *
+	 * @param $set  The set we want to list metadata for.
+	 * @return An associative array with all entities in the given set, or an empty array if we are
+	 *         unable to generate this list.
+	 */
+	public function getMetadataSet($set) {
+		return array();
+	}
+
+
+	/**
+	 * This function resolves an host/path combination to an entity id.
+	 *
+	 * This class implements this function using the getMetadataSet-function. A subclass should
+	 * override this function if it doesn't implement the getMetadataSet function, or if the
+	 * implementation of getMetadataSet is slow.
+	 *
+	 * @param $hostPath  The host/path combination we are looking up.
+	 * @param $set  Which set of metadata we are looking it up in.
+	 * @return An entity id which matches the given host/path combination, or NULL if
+	 *         we are unable to locate one which matches.
+	 */
+	public function getEntityIdFromHostPath($hostPath, $set) {
+
+		$metadataSet = $this->getMetadataSet($set);
+
+		foreach($metadataSet AS $entityId => $entry) {
+
+			if(!array_key_exists('host', $entry)) {
+				continue;
+			}
+
+			if($hostPath === $entry['host']) {
+				return $entityId;
+			}
+		}
+
+		/* No entries matched - we should return NULL. */
+		return NULL;
+	}
+
+
+	/**
+	 * This function retrieves metadata for the given entity id in the given set of metadata.
+	 * It will return NULL if it is unable to locate the metadata.
+	 *
+	 * This class implements this function using the getMetadataSet-function. A subclass should
+	 * override this function if it doesn't implement the getMetadataSet function, or if the
+	 * implementation of getMetadataSet is slow.
+	 *
+	 * @param $entityId  The entity id we are looking up.
+	 * @param $set  The set we are looking for metadata in.
+	 * @return An associative array with metadata for the given entity, or NULL if we are unable to
+	 *         locate the entity.
+	 */
+	public function getMetaData($entityId, $set) {
+
+		assert('is_string($entityId)');
+
+		$metadataSet = $this->getMetadataSet($set);
+
+		if(!array_key_exists($entityId, $metadataSet)) {
+			return NULL;
+		}
+
+		return $metadataSet[$entityId];
+	}
+
+}
+?>
