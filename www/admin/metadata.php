742f65344478b77e8bfe7c4e6acb867d1b46b351
Olav Morken <olav.morken@uninett.no>
2008-03-13 15:18:40 +0000

    request.signing is an optional attribute for idp-remote metadata.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@411 44740490-163a-0410-bde0-09ae8108e29a

--- www/admin/metadata.php
+++ www/admin/metadata.php
@@ -44,7 +44,7 @@ try {
 		foreach ($metalist AS $entityid => $mentry) {
 			$results[$entityid] = SimpleSAML_Utilities::checkAssocArrayRules($mentry,
 				array('entityid', 'SingleSignOnService', 'SingleLogoutService', 'certFingerprint'),
-				array('name', 'description', 'base64attributes', 'certificate', 'hint.cidr', 'saml2.relaxvalidation', 'SingleLogoutServiceResponse')
+				array('name', 'description', 'base64attributes', 'certificate', 'hint.cidr', 'saml2.relaxvalidation', 'SingleLogoutServiceResponse', 'request.signing')
 			);
 		}
 		$et->data['metadata.saml20-idp-remote'] = $results;
