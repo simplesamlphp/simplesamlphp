78d90fa2628d3ff4f9cc892c5f8bd7db62b713b4
Enrique de la Hoz <enrique.delahoz@uah.es>
2008-04-02 22:33:42 +0000

    Added multilanguage support
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@489 44740490-163a-0410-bde0-09ae8108e29a

--- templates/default/login-ldapmulti.php
+++ templates/default/login-ldapmulti.php
@@ -9,16 +9,16 @@
 		<?php if (isset($this->data['error'])) { ?>
 		<div style="border-left: 1px solid #e8e8e8; border-bottom: 1px solid #e8e8e8; background: #f5f5f5"
 		<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/bomb.png" style="float: left; margin: 15px " />
-		<h2>What you entered was not accepted!</h2>
+		<h2><?php echo $this->t('error_header'); ?></h2>
 		
 		<p><?php echo htmlspecialchars($this->data['error']); ?> </p>
 		</div>
 		<?php } ?>
 	
-		<h2 style="break: both">Enter your username and password</h2>
+		<h2 style="break: both"><?php echo $this->t('user_pass_header'); ?></h2>
 		
 		<p>
-			A service has requested you to authenticate your self. That means you need to enter your username and password in the form below.
+			<?php echo $this->t('user_pass_text'); ?>
 		</p>
 		
 		<form action="?" method="post" name="f">
@@ -26,7 +26,7 @@
 		<table>
 			<tr>
 				<td rowspan="3"><img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/pencil.png" /></td>
-				<td style="padding: .3em;">Username</td>
+				<td style="padding: .3em;"><?php echo $this->t('username'); ?></td>
 				<td><input type="text" tabindex="1" name="username" 
 					<?php if (isset($this->data['username'])) {
 						echo 'value="' . htmlspecialchars($this->data['username']) . '"';
@@ -40,7 +40,7 @@
 			</tr>
 			
 			<tr>
-				<td style="padding: .3em;">Organization</td>
+				<td style="padding: .3em;"><?php echo $this->t('organization'); ?></td>
 				<td><select name="org" tabindex="2">
 					<?php
 					
@@ -55,7 +55,7 @@
 			</tr>
 			
 			<tr>
-				<td style="padding: .3em;">Password</td>
+				<td style="padding: .3em;"><?php echo $this->t('password'); ?></td>
 				<td><input type="password" tabindex="2" name="password" /></td>
 			</tr>
 		</table>
@@ -64,14 +64,11 @@
 		</form>
 		
 		
-		<h2>Help! I don't remember my password.</h2>
+		<h2><?php echo $this->t('help_header'); ?>.</h2>
 		
 		
-		<p>Too bad! - Without your username and password you cannot authenticate your self and access the service.
-		There may be someone that can help you. Contact the help desk at your university!</p>
+		<p><?php echo $this->t('help_text'); ?>!</p>
 		
-		<h2>About simpleSAMLphp</h2>
-		<p>Hey! This simpleSAMLphp thing is pretty cool, where can I read more about it?
-		You can find more information about simpleSAMLphp at <a href="http://rnd.feide.no">the Feide RnD blog</a> over at <a href="http://uninett.no">UNINETT</a>.</p>
+		
 		
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
