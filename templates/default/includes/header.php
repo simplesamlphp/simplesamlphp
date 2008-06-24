2f6d0861dce734de183e0593d1b0657a6e0b2249
Olav Morken <olav.morken@uninett.no>
2008-06-24 06:50:09 +0000

    Add robot meta tag to default template, to prevent bots from indexing the simpleSAMLphp pages.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@699 44740490-163a-0410-bde0-09ae8108e29a

--- templates/default/includes/header.php
+++ templates/default/includes/header.php
@@ -13,6 +13,7 @@ if(array_key_exists('header', $this->data)) {
 	<link rel="stylesheet" type="text/css" href="/<?php echo $this->data['baseurlpath']; ?>resources/default.css" />
 	<link rel="icon" type="image/icon" href="/<?php echo $this->data['baseurlpath']; ?>resources/icons/favicon.ico" />
 	<script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/script.js"></script>
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
 </head>
 <?php
 $onLoad = '';
