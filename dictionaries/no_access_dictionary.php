2b9dafdc80fe23ea7c5fbc711f1230b3c9c08e63
Olav Morken <olav.morken@uninett.no>
2008-07-02 06:54:15 +0000

    Dictionary update.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@740 44740490-163a-0410-bde0-09ae8108e29a

--- dictionaries/no_access_dictionary.php
+++ dictionaries/no_access_dictionary.php
@@ -4,14 +4,17 @@ $lang = array(
 	'access_denied' => array (
 		'da' => 'Du har ikke adgang til denne service',
 		'en' => 'You do not have access to this service',
+		'sv' => 'Du har inte tillgång till denna tjänst',
 	),
 	'no_access_to' => array (
 		'da' => 'Du har ikke adgang til servicen:',
 		'en' => 'You do not have access to the service:',
+		'sv' => 'Du har inte tillgång till angiven tjänst:',
 	),
 	'contact_home' => array (
 		'da' => 'Adgang til services bestemmes af din hjemmeorganisation. Kontakt venligst din lokale helpdesk for spørgsmål.',
 		'en' => 'Access to services is controlled by your home organization. Please contact your local it support for questions.',
+		'sv' => 'Tillgång till tjänster kontrolleras av din hemmaorganisation. Ta kontakt med din lokala support om du har några frågor.',
 	),
 
 );
