31942277abd2f0f5ae3d0b3d8f295c00ea245ed8
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2008-11-14 21:21:42 +0000

    Improved look of sanity check web page
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@995 44740490-163a-0410-bde0-09ae8108e29a

--- modules/sanitycheck/templates/default/check-tpl.php
+++ modules/sanitycheck/templates/default/check-tpl.php
@@ -11,7 +11,7 @@ $this->includeAtTemplateBase('includes/header.php');
 if (count($this->data['errors']) > 0) {
 ?>
 <div style="border: 1px solid #800; background: #caa; margin: 1em; padding: .5em">
-<p><?php echo '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/delete.png" alt="Failed" />'; ?>	
+<p><?php echo '<img style="float: right" src="/' . $this->data['baseurlpath'] . 'resources/icons/delete.png" alt="Failed" />'; ?>	
 These checks failed:</p>
 <?php
 
@@ -29,7 +29,7 @@ echo '</div>';
 if (count($this->data['info']) > 0) {
 ?>
 <div style="border: 1px solid #ccc; background: #eee; margin: 1em; padding: .5em">
-<p><?php echo '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/accept.png" alt="OK" />'; ?>	
+<p><?php echo '<img style="float: right" src="/' . $this->data['baseurlpath'] . 'resources/icons/accept.png" alt="OK" />'; ?>	
 These checks succeeded:</p>
 <?php
 	echo '<ul>';
