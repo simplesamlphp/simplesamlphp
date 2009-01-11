45c1060ae0cc2d8683d76bf12372fe93ad0478d8
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-01-11 09:03:09 +0000

    Adding a preproduction warning module.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1126 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/preprodwarning/dictionaries/warning.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang = array(
+	'warning' => array (
+		'no' => 'Du har nå kommet til et test-oppsett. Dette oppsettet for autentisering er kun til bruk for testing og pre-produksjon verifikasjon. Hvis noen sendte deg en link som pekte hit, og du ikke er <i>en tester</i> så fikk du nok en feil link, og <b>skulle ikke vært her</b>.',
+		'en' => 'You are now accessing a pre-production system. This authentication setup is for testing and pre-production verification only. If someone sent you a link that pointed you here, and you are not <i>a tester</i> you probably got the wrong link, and should <b>not be here</b>.',
+	),
+	'warning_header' => array (
+		'no' => 'Advarsel om at dette er et test oppsett',
+		'en' => 'Warning about accessing a pre-production system',
+	),
+	'yes' => array (
+		'no' => 'Ja, jeg holder på å teste innlogging, og vet dette er et test-system.',
+		'en' => 'Yes, I know I am accessing a pre-production system',
+	),
+
+);
+
+
+?>
---
+++ modules/preprodwarning/lib/Auth/Process/Warning.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Give a warning that the user is accessing a test system, not a production system.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+class sspmod_preprodwarning_Auth_Process_Warning extends SimpleSAML_Auth_ProcessingFilter {
+
+
+
+	/**
+	 * Process a authentication response.
+	 *
+	 * This function saves the state, and redirects the user to the page where the user
+	 * can authorize the release of the attributes.
+	 *
+	 * @param array $state  The state of the response.
+	 */
+	public function process(&$state) {
+		assert('is_array($state)');
+		assert('array_key_exists("UserID", $state)');
+		assert('array_key_exists("Destination", $state)');
+		assert('array_key_exists("entityid", $state["Destination"])');
+		assert('array_key_exists("metadata-set", $state["Destination"])');		
+		assert('array_key_exists("entityid", $state["Source"])');
+		assert('array_key_exists("metadata-set", $state["Source"])');
+
+		/* Save state and redirect. */
+		$id = SimpleSAML_Auth_State::saveState($state, 'consent:request');
+		$url = SimpleSAML_Module::getModuleURL('preprodwarning/showwarning.php');
+		SimpleSAML_Utilities::redirect($url, array('StateId' => $id));
+	}
+	
+
+
+}
+
+?>
