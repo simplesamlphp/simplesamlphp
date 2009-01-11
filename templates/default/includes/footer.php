2412068bfd4747b69754223363452f8c9ae33f23
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-01-11 09:13:21 +0000

    Adding 2009 in the footer... as we just entered a new year.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1127 44740490-163a-0410-bde0-09ae8108e29a

--- templates/default/includes/footer.php
+++ templates/default/includes/footer.php
@@ -4,7 +4,7 @@
 		<hr />
 
 		<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/ssplogo-fish-small.png" alt="Small fish logo" style="float: right" />		
-		Copyright &copy; 2007-2008 <a href="http://rnd.feide.no/">Feide RnD</a>
+		Copyright &copy; 2007-2009 <a href="http://rnd.feide.no/">Feide RnD</a>
 		
 		<br style="clear: right" />
 	
