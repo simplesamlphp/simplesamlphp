01ec5be134bfa9d7825d5205bd9df83c3dd5fa3d
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-01-17 12:32:36 +0000

    Adding a yubikey authentication module dictionary
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1154 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/authYubiKey/dictionaries/yubikey.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang = array(
+	'header' => array (
+		'no' => 'Login med YubiKey',
+		'en' => 'Login with YubiKey',
+	),
+	'intro' => array (
+		'no' => 'En tjeneste har bedt om at du autentiserer deg. For å autentisere seg, trykk på knappen på din YubiKey. Feltet under vil da bli automatisk utfylt med et sikkert engangspassord.',
+		'en' => 'Some service requested authentication. To authenticate, click the button on your YubiKey. The field below should then automatically be filled out with a secure one-time-password.',
+	),
+
+);
+
+
+?>
