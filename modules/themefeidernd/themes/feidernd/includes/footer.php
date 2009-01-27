79c0baf895966a9b03ec2d2305b00521a7604d5a
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-01-27 05:27:58 +0000

    Moving Feide RnD theme into a separate module
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1189 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/themefeidernd/themes/feidernd/includes/footer.php
@@ -0,0 +1,16 @@
+
+
+
+		<hr />
+
+		<img src="/<?php echo $this->data['baseurlpath']; ?>resources/icons/ssplogo-fish-small.png" alt="Small fish logo" style="float: right" />		
+		Copyright &copy; 2007-2008 <a href="http://rnd.feide.no/">Feide RnD</a>
+		
+		<br style="clear: right" />
+	
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ modules/themefeidernd/themes/feidernd/includes/header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/script.js"></script>
+<title><?php
+if(array_key_exists('header', $this->data)) {
+	echo $this->data['header'];
+} else {
+	echo 'simpleSAMLphp';
+}
+?></title>
+
+	<link rel="stylesheet" type="text/css" href="/<?php echo $this->data['baseurlpath']; ?>resources/default.css" />
+	<link rel="icon" type="image/icon" href="/<?php echo $this->data['baseurlpath']; ?>resources/icons/favicon.ico" />
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+
+<?php	
+if(array_key_exists('head', $this->data)) {
+	echo '<!-- head -->' . $this->data['head'] . '<!-- /head -->';
+}
+?>
+</head>
+<?php
+$onLoad = '';
+if(array_key_exists('autofocus', $this->data)) {
+	$onLoad .= 'SimpleSAML_focus(\'' . $this->data['autofocus'] . '\');';
+}
+if (isset($this->data['onLoad'])) {
+	$onLoad .= $this->data['onLoad']; 
+}
+
+if($onLoad !== '') {
+	$onLoad = ' onload="' . $onLoad . '"';
+}
+?>
+<body<?php echo $onLoad; ?>>
+
+<div id="wrap">
+	
+	<div id="header">
+		<h1><a style="text-decoration: none; color: white" href="/<?php echo $this->data['baseurlpath']; ?>"><?php 
+			echo (isset($this->data['header']) ? $this->data['header'] : 'simpleSAMLphp'); 
+		?></a></h1>
+	</div>
+
+	
+	<?php 
+	
+	$includeLanguageBar = TRUE;
+	if (!empty($_POST)) 
+		$includeLanguageBar = FALSE;
+	if (isset($this->data['hideLanguageBar']) && $this->data['hideLanguageBar'] === TRUE) 
+		$includeLanguageBar = FALSE;
+	
+	if ($includeLanguageBar) {
+		
+		
+		echo '<div id="languagebar">';
+		$languages = $this->getLanguageList();
+		$langnames = array(
+			'no' => 'Bokmål',
+			'nn' => 'Nynorsk',
+			'se' => 'Sámi',
+			'da' => 'Dansk',
+			'en' => 'English',
+			'de' => 'Deutsch',
+			'sv' => 'Svenska',
+			'fi' => 'Suomeksi',
+			'es' => 'Español',
+			'fr' => 'Français',
+			'nl' => 'Nederlands',
+			'lb' => 'Luxembourgish', 
+			'sl' => 'Slovenščina', // Slovensk
+			'hr' => 'Hrvatski', // Croatian
+			'hu' => 'Magyar', // Hungarian
+			'pt' => 'Português', // Portuguese
+			'pt-BR' => 'Português brasileiro', // Portuguese
+		);
+		
+		$textarray = array();
+		foreach ($languages AS $lang => $current) {
+			if ($current) {
+				$textarray[] = $langnames[$lang];
+			} else {
+				$textarray[] = '<a href="' . htmlspecialchars(SimpleSAML_Utilities::addURLparameter(SimpleSAML_Utilities::selfURL(), array('language' => $lang))) . '">' .
+					$langnames[$lang] . '</a>';
+			}
+		}
+		echo join(' | ', $textarray);
+		echo '</div>';
+
+	}
+	
+	?>
+	<div id="content">
