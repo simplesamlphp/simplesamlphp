8ba4a4b1ef3cf8d922c061c04864a68caac58584
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-02-17 15:32:36 +0000

    set empty default encoded value
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1284 44740490-163a-0410-bde0-09ae8108e29a

--- modules/saml2debug/www/debug.php
+++ modules/saml2debug/www/debug.php
@@ -46,7 +46,7 @@ function encode($message) {
 }
 
 
-$decoded = '<html>';
+$decoded = '';
 $encoded = 'fZJNT%2BMwEIbvSPwHy%2Fd8tMvHympSdUGISuwS0cCBm%2BtMUwfbk%2FU4zfLvSVMq2Euv45n3fd7xzOb%2FrGE78KTRZXwSp5yBU1hpV2f8ubyLfvJ5fn42I2lNKxZd2Lon%2BNsBBTZMOhLjQ8Y77wRK0iSctEAiKLFa%2FH4Q0zgVrceACg1ny9uMy7rCdaM2%2Bs0BWrtppK2UAdeoVjW2ruq1bevGImcvR6zpHmtJ1MHSUZAuDKU0vY7Si2h6VU5%2BiMuJuLx65az4dPql3SHBKaz1oYnEfVkWUfG4KkeBna7A%2Fxm6M14j1gZihZazBRH4MODcoKPOgl%2BB32kFz08PGd%2BG0JJIkr7v46%2BhRCaEpod17DCRivYZCkmkd4N28B3wfNyrGKP5bws9DS6PKDz%2FMpsl36Tyz%2F%2Fax1jeFmi0emcLY7C%2F8SDD0Z7dobcynHbbV3QVbcZW0TlqQemNhoqzJD%2B4%2Fn8Yw7l8AA%3D%3D';
 
 $activeTab = 0;
