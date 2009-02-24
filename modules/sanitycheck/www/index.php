d1b95affb29cd12caba5c99880810d0544296190
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-02-24 07:45:05 +0000

    clean
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1309 44740490-163a-0410-bde0-09ae8108e29a

--- modules/sanitycheck/www/index.php
+++ modules/sanitycheck/www/index.php
@@ -4,9 +4,6 @@
 $config = SimpleSAML_Configuration::getInstance();
 $sconfig = $config->copyFromBase('sconfig', 'config-sanitycheck.php');
 
-
-
-
 $info = array();
 $errors = array();
 $hookinfo = array(
@@ -26,10 +23,8 @@ if (isset($_REQUEST['output']) && $_REQUEST['output'] == 'text') {
 	exit;
 }
 
-$config = SimpleSAML_Configuration::getInstance();
+
 $t = new SimpleSAML_XHTML_Template($config, 'sanitycheck:check-tpl.php');
 $t->data['errors'] = $errors;
 $t->data['info'] = $info;
 $t->show();
-
-?>
