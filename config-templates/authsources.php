2570aa4d76a0cfeb64d6a8cda1ac9eafbedc8d83
Olav Morken <olav.morken@uninett.no>
2009-03-05 09:14:20 +0000

    Add LDAP & LDAPMulti authentication module.
    This adds two authentication modules which are meant to replace auth/login.php
    and auth/login-ldapmulti.php.
    
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1372 44740490-163a-0410-bde0-09ae8108e29a

--- config-templates/authsources.php
+++ config-templates/authsources.php
@@ -62,6 +62,94 @@ $config = array(
 		'secret' => 'xxxxxxxxxxxxxxxx',
 	),
         
+	/* Example of a LDAP authentication source. */
+	'example-ldap' => array(
+		'ldap:LDAP',
+
+		/* The hostname of the LDAP server. */
+		'hostname' => 'ldap.example.org',
+
+		/* Whether SSL/TLS should be used when contacting the LDAP server. */
+		'enable_tls' => FALSE,
+
+		/*
+		 * Which attributes should be retrieved from the LDAP server.
+		 * This can be an array of attribute names, or NULL, in which case
+		 * all attributes are fetched.
+		 */
+		'attributes' => NULL,
+
+		/*
+		 * The pattern which should be used to create the users DN given the username.
+		 * %username% in this pattern will be replaced with the users username.
+		 *
+		 * This option is not used if the search.enable option is set to TRUE.
+		 */
+		'dnpattern' => 'uid=%username%,ou=people,dc=example,dc=org',
+
+		/*
+		 * As an alternative to specifying a pattern for the users DN, it is possible to
+		 * search for the username in a set of attributes. This is enabled by this option.
+		 */
+		'search.enable' => FALSE,
+
+		/*
+		 * The DN which will be used as a base for the search.
+		 * This can be a single string, in which case only that DN is searched, or an
+		 * array of strings, in which case they will be searched in the order given.
+		 */
+		'search.base' => 'ou=people,dc=example,dc=org',
+
+		/*
+		 * The attribute(s) the username should match against.
+		 *
+		 * This is an array with one or more attribute names. Any of the attributes in
+		 * the array may match the value the username.
+		 */
+		'search.attributes' => array('uid', 'mail'),
+
+		/*
+		 * The username & password the simpleSAMLphp should bind to before searching. If
+		 * this is left as NULL, no bind will be performed before searching.
+		 */
+		'search.username' => NULL,
+		'search.password' => NULL,
+	),
+
+	/* Example of an LDAPMulti authentication source. */
+	'example-ldapmulti' => array(
+		'ldap:LDAPMulti',
+
+		/*
+		 * A list of available LDAP servers / user groups. The value of each element is
+		 * an array in the same format as an LDAP authentication source.
+		 */
+		'employees' => array(
+			/*
+			 * A short name/description for this group. Will be shown in a dropdown list
+			 * when the user logs on.
+			 *
+			 * This option can be a string or an array with language => text mappings.
+			 */
+			'description' => 'Employees',
+
+			/*
+			 * The rest of the options are the same as those available for
+			 * the LDAP authentication source.
+			 */
+			'hostname' => 'ldap.employees.example.org',
+			'dnpattern' => 'uid=%username%,ou=employees,dc=example,dc=org',
+		),
+
+		'students' => array(
+			'description' => 'Students',
+
+			'hostname' => 'ldap.students.example.org',
+			'dnpattern' => 'uid=%username%,ou=students,dc=example,dc=org',
+		),
+
+	),
+
 );
 
 ?>
---
+++ modules/ldap/lib/ConfigHelper.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * LDAP authentication source configuration parser.
+ *
+ * See the ldap-entry in config-templates/authsources.php for information about
+ * configuration of these options.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+class sspmod_ldap_ConfigHelper {
+
+
+	/**
+	 * String with the location of this configuration.
+	 * Used for error reporting.
+	 */
+	private $location;
+
+
+	/**
+	 * The hostname of the LDAP server.
+	 */
+	private $hostname;
+
+
+	/**
+	 * Whether we should use TLS/SSL when contacting the LDAP server.
+	 */
+	private $enableTLS;
+
+
+	/**
+	 * Whether we need to search for the users DN.
+	 */
+	private $searchEnable;
+
+
+	/**
+	 * The username we should bind with before we can search for the user.
+	 */
+	private $searchUsername;
+
+
+	/**
+	 * The password we should bind with before we can search for the user.
+	 */
+	private $searchPassword;
+
+
+	/**
+	 * Array with the base DN(s) for the search.
+	 */
+	private $searchBase;
+
+
+	/**
+	 * The attributes which should match the username.
+	 */
+	private $searchAttributes;
+
+
+	/**
+	 * The DN pattern we should use to create the DN from the username.
+	 */
+	private $dnPattern;
+
+
+	/**
+	 * The attributes we should fetch. Can be NULL in which case we will fetch all attributes.
+	 */
+	private $attributes;
+
+
+
+	/**
+	 * Constructor for this configuration parser.
+	 *
+	 * @param array $config  Configuration.
+	 * @param string $location  The location of this configuration. Used for error reporting.
+	 */
+	public function __construct($config, $location) {
+		assert('is_array($config)');
+		assert('is_string($location)');
+
+		$this->location = $location;
+
+		/* Parse configuration. */
+		$config = SimpleSAML_Configuration::loadFromArray($config, $location);
+
+		$this->hostname = $config->getString('hostname');
+		$this->enableTLS = $config->getBoolean('enable_tls', FALSE);
+		$this->searchEnable = $config->getBoolean('search.enable', FALSE);
+
+		if ($this->searchEnable) {
+			$this->searchUsername = $config->getString('search.username', NULL);
+			if ($this->searchUsername !== NULL) {
+				$this->searchPassword = $config->getString('search.password');
+			}
+
+			$this->searchBase = $config->getString('search.base');
+			$this->searchAttributes = $config->getArray('search.attributes');
+
+		} else {
+			$this->dnPattern = $config->getString('dnpattern');
+		}
+
+		$this->attributes = $config->getArray('attributes', NULL);
+	}
+
+
+	/**
+	 * Attempt to log in using the given username and password.
+	 *
+	 * Will throw a SimpleSAML_Error_Error('WRONGUSERPASS') if the username or password is wrong.
+	 * If there is a configuration problem, an Exception will be thrown.
+	 *
+	 * @param string $username  The username the user wrote.
+	 * @param string $password  The password the user wrote.
+	 * @return array  Associative array with the users attributes.
+	 */
+	public function login($username, $password) {
+		assert('is_string($username)');
+		assert('is_string($password)');
+
+		$ldap = new SimpleSAML_Auth_LDAP($this->hostname, $this->enableTLS);
+
+		if (!$this->searchEnable) {
+			$ldapusername = addcslashes($username, ',+"\\<>;*');
+			$dn = str_replace('%username%', $ldapusername, $this->dnPattern);
+		} else {
+			if ($this->searchUsername !== NULL) {
+				if(!$ldap->bind($this->searchUsername, $this->searchPassword)) {
+					throw new Exception('Error authenticating using search username & password.');
+				}
+			}
+
+			$dn = $ldap->searchfordn($this->searchBase, $this->searchAttributes, $username, TRUE);
+			if ($dn === NULL) {
+				/* User not found with search. */
+				SimpleSAML_Logger::info($this->location . ': Unable to find users DN. username=\'' . $username . '\'');
+				throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+			}
+		}
+
+		if (!$ldap->bind($dn, $password)) {
+			SimpleSAML_Logger::info($this->location . ': '. $username . ' failed to authenticate. DN=' . $dn);
+			throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+		}
+
+		return $ldap->getAttributes($dn, $this->attributes);
+	}
+
+}
+
+
+?>
