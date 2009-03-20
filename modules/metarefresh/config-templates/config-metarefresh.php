8e7fb20e362f61b07fb6747af04cb383aac6e67d
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-03-20 13:53:31 +0000

    Better default configuration for metarefresh. real life example from Kalmar
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1430 44740490-163a-0410-bde0-09ae8108e29a

--- modules/metarefresh/config-templates/config-metarefresh.php
+++ modules/metarefresh/config-templates/config-metarefresh.php
@@ -2,22 +2,27 @@
 
 $config = array(
 
-	'kalmar' => array(
-		'cron'		=> array('hourly'),
-		'sources'	=> array(
-			array(
-				'src' => 'https://aitta.funet.fi/haka/haka_test_metadata_signed.xml',
-				'certFingerprint' => '22:1D:EA:E3:2C:EB:A3:2D:78:72:B6:F4:E9:52:F6:23:31:5A:A5:3D',
-				'template' => array(
-					'tags'	=> array('kalmar'),
+	'sets' => array(
+
+		'kalmar' => array(
+			'cron'		=> array('hourly'),
+			'sources'	=> array(
+				array(
+					'src' => 'https://kalmar.feide.no/simplesaml/module.php/aggregator/?id=kalmarcentral&mimetype=text/plain&exclude=norway',
+					'certFingerprint' => '591d4b4670463eeda91fcc816dc0af2a092aa801',
+					'template' => array(
+						'tags'	=> array('kalmar'),
+						'authproc' => array(
+							51 => array('class' => 'core:AttributeMap', 'oid2name'),
+						),
+					),
 				),
 			),
+			'expireAfter' 		=> 60*60*24*4, // Maximum 4 days cache time.
+			'outputDir' 	=> 'metadata/metadata-kalmar-consuming/',
 		),
-		'maxCache' 		=> 60*60*24*4, // Maximum 4 days cache time.
-		'maxDuration' 	=> 60*60*24*10, // Maximum 10 days duration on ValidUntil.
-		'outputDir' 	=> 'metadata/metadata-kalmar-consuming/',
 	),
-
 );
 
-?>
+
+
