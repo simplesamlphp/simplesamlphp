b1d15f3d70c431fa28c47178303bc3d30a57dc36
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-05-08 14:49:51 +0000

    Added config option to enable and disable the getuserinfo endpoint in oauth module.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1497 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/oauth/config-template/module_oauth.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * Configuration for the OAuth module.
+ * 
+ * $Id$
+ */
+
+$config = array (
+
+	/* Enable the getUserInfo endpoint. Do not enable unless you know what you do.
+	 * It may give external parties access to userInfo unless properly secured.
+	 */
+	'getUserInfo.enable' => FALSE,
+
+);
+
--- modules/oauth/www/getUserInfo.php
+++ modules/oauth/www/getUserInfo.php
@@ -2,6 +2,12 @@
 
 require_once(dirname(dirname(__FILE__)) . '/libextinc/OAuth.php');
 
+$oauthconfig = SimpleSAML_Configuration::getConfig('module_oauth.php');
+
+if (!$oauthconfig->getBoolean('getUserInfo.enable', FALSE)) {
+	throw new Exception('Get user info endpoint is disabled. This endpoint can be enabled in the module_oauth.php configuration file.');
+}
+
 $store = new sspmod_oauth_OAuthStore();
 $server = new sspmod_oauth_OAuthServer($store);
 
