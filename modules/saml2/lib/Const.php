d65116ed5774045b3b4123084dc0e4f1a472169e
Olav Morken <olav.morken@uninett.no>
2009-07-13 06:17:12 +0000

    saml2: Add a class for SAML2 constants.
    This patch adds the SimpleSAML_XML_SAML2 class, which currently contain
    SAML 2 status codes constants. New SAML2 constants should be added to this
    class.
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1569 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/saml2/lib/Const.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Various SAML 2 constants.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+class sspmod_saml2_Const {
+
+	/**
+	 * Top-level status code indicating successful processing of the request.
+	 */
+	const STATUS_SUCCESS = 'urn:oasis:names:tc:SAML:2.0:status:Success';
+
+
+	/**
+	 * Top-level status code indicating that there was a problem with the request.
+	 */
+	const STATUS_REQUESTER = 'urn:oasis:names:tc:SAML:2.0:status:Requester';
+
+
+	/**
+	 * Top-level status code indicating that there was a problem generating the response.
+	 */
+	const STATUS_RESPONDER = 'urn:oasis:names:tc:SAML:2.0:status:Responder';
+
+
+	/**
+	 * Top-level status code indicating that the request was from an unsupported version of the SAML protocol.
+	 */
+	const STATUS_VERSION_MISMATCH = 'urn:oasis:names:tc:SAML:2.0:status:VersionMismatch';
+
+
+
+	/**
+	 * Second-level status code for NoPassive errors.
+	 */
+	const STATUS_NO_PASSIVE = 'urn:oasis:names:tc:SAML:2.0:status:NoPassive';
+
+}
+
+?>
