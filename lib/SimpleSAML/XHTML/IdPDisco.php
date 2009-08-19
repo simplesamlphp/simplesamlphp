9e7bf58602d78b5c5534aa135aca89af7dff53f5
Olav Morken <olav.morken@uninett.no>
2009-08-19 08:30:00 +0000

    SimpleSAML_XHTML_IdPDisco: idpdisco.extDiscoveryStorage is a string, not a boolean.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1693 44740490-163a-0410-bde0-09ae8108e29a

--- lib/SimpleSAML/XHTML/IdPDisco.php
+++ lib/SimpleSAML/XHTML/IdPDisco.php
@@ -399,8 +399,8 @@ class SimpleSAML_XHTML_IdPDisco {
 		$idp = $this->getTargetIdp();
 		if($idp !== NULL) {
 		
-			if ($this->config->getBoolean('idpdisco.extDiscoveryStorage', NULL) != NULL) {
-				$extDiscoveryStorage = $this->config->getBoolean('idpdisco.extDiscoveryStorage');
+			$extDiscoveryStorage = $this->config->getString('idpdisco.extDiscoveryStorage', NULL);
+			if ($extDiscoveryStorage !== NULL) {
 				$this->log('Choice made [' . $idp . '] (Forwarding to external discovery storage)');
 				SimpleSAML_Utilities::redirect($extDiscoveryStorage, array(
 //					$this->returnIdParam => $idp,
