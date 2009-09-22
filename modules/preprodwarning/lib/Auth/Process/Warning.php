318b57ce1c2b7e7379284cf905967ffd87b6dd9a
Olav Morken <olav.morken@uninett.no>
2009-09-22 07:43:39 +0000

    preprodwarning: Don't show warning in passive request.
    Fixes issue 140
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1766 44740490-163a-0410-bde0-09ae8108e29a

--- modules/preprodwarning/lib/Auth/Process/Warning.php
+++ modules/preprodwarning/lib/Auth/Process/Warning.php
@@ -21,6 +21,11 @@ class sspmod_preprodwarning_Auth_Process_Warning extends SimpleSAML_Auth_Process
 	public function process(&$state) {
 		assert('is_array($state)');
 
+		if (isset($state['isPassive']) && $state['isPassive'] === TRUE) {
+			/* We have a passive request. Skip the warning. */
+			return;
+		}
+
 		/* Save state and redirect. */
 		$id = SimpleSAML_Auth_State::saveState($state, 'warning:request');
 		$url = SimpleSAML_Module::getModuleURL('preprodwarning/showwarning.php');
