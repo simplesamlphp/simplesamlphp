f501d0809df49f0eb7890bd12ed6e34da581660f
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-09-29 11:39:37 +0000

    Include default-sp authsource for saml sp module. remove duplicate...
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1804 44740490-163a-0410-bde0-09ae8108e29a

--- config-templates/authsources.php
+++ config-templates/authsources.php
@@ -15,16 +15,29 @@ $config = array(
 
 
 	/*
-	 * This is the default SAML SP, used in examples.
+	 * An authentication source which can authenticate against both SAML 2.0
+	 * and Shibboleth 1.3 IdPs.
 	 */
 	'default-sp' => array(
 		'saml:SP',
+
+		/*
+		 * The entity ID of this SP.
+		 * Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
+		 */
+		'entityID' => NULL,
 
 		/*
 		 * The entity ID of the IdP this should SP should contact.
 		 * Can be NULL/unset, in which case the user will be shown a list of available IdPs.
 		 */
 		'idp' => NULL,
+
+		/*
+		 * The URL to the discovery service.
+		 * Can be NULL/unset, in which case a builtin discovery service will be used.
+		 */
+		'discoURL' => NULL,
 	),
 
 
@@ -80,32 +93,7 @@ $config = array(
 		'authpapi:PAPI',
 	),
 
-	/*
-	 * An authentication source which can authenticate against both SAML 2.0
-	 * and Shibboleth 1.3 IdPs.
-	 */
-	'saml' => array(
-		'saml:SP',
 
-		/*
-		 * The entity ID of this SP.
-		 * Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-		 */
-		'entityID' => NULL,
-
-		/*
-		 * The entity ID of the IdP this should SP should contact.
-		 * Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-		 */
-		'idp' => NULL,
-
-		/*
-		 * The URL to the discovery service.
-		 * Can be NULL/unset, in which case a builtin discovery service will be used.
-		 */
-		'discoURL' => NULL,
-
-	),
 
 
 	'facebook' => array(
