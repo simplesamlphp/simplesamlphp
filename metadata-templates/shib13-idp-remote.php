3e5103df18605444bca49c7684885b22a5527a24
Olav Morken <olav.morken@uninett.no>
2009-10-20 12:11:51 +0000

    Comment out the entry in metadata-templates/shib13-idp-remote.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1885 44740490-163a-0410-bde0-09ae8108e29a

--- metadata-templates/shib13-idp-remote.php
+++ metadata-templates/shib13-idp-remote.php
@@ -7,7 +7,9 @@
  * See: https://rnd.feide.no/content/idp-remote-metadata-reference
  */
 
+/*
 $metadata['theproviderid-of-the-idp'] = array(
 	'SingleSignOnService'  => 'https://idp.example.org/shibboleth-idp/SSO',
 	'certFingerprint'      => 'c7279a9f28f11380509e072441e3dc55fb9ab864',
 );
+*/
