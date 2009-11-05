13eeb394d2980abf21e16cbb0ce08ca201407ba5
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2009-11-05 13:13:03 +0000

    Adding a separate page to load metadata, and verify output in logs...
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@1971 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/metarefresh/www/fetch.php
@@ -0,0 +1,50 @@
+<?php
+
+$config = SimpleSAML_Configuration::getInstance();
+$mconfig = SimpleSAML_Configuration::getConfig('config-metarefresh.php');
+
+SimpleSAML_Utilities::requireAdmin();
+
+SimpleSAML_Logger::setCaptureLog(TRUE);
+
+
+$sets = $mconfig->getConfigList('sets');
+
+foreach ($sets AS $setkey => $set) {
+
+	SimpleSAML_Logger::info('[metarefresh]: Executing set [' . $setkey . ']');
+
+	$expireAfter = $set->getInteger('expireAfter', NULL);
+	if ($expireAfter !== NULL) {
+		$expire = time() + $expireAfter;
+	} else {
+		$expire = NULL;
+	}
+
+	$metaloader = new sspmod_metarefresh_MetaLoader($expire);
+
+	foreach($set->getArray('sources') AS $source) {
+		SimpleSAML_Logger::debug('[metarefresh]: In set [' . $setkey . '] loading source ['  . $source['src'] . ']');
+		$metaloader->loadSource($source);
+	}
+
+	$outputDir = $set->getString('outputDir');
+	$outputDir = $config->resolvePath($outputDir);
+
+	$outputFormat = $set->getValueValidate('outputFormat', array('flatfile', 'serialize'), 'flatfile');
+	switch ($outputFormat) {
+		case 'flatfile':
+			$metaloader->writeMetadataFiles($outputDir);
+			break;
+		case 'serialize':
+			$metaloader->writeMetadataSerialize($outputDir);
+			break;
+	}
+
+}
+
+$logentries = SimpleSAML_Logger::getCapturedLog();
+
+$t = new SimpleSAML_XHTML_Template($config, 'metarefresh:fetch.tpl.php');
+$t->data['logentries'] = $logentries;
+$t->show();
