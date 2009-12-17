9c5346c5098de253a99f23d9b40be72c9dc4153b
Olav Morken <olav.morken@uninett.no>
2009-12-17 08:13:57 +0000

    Shib13_AuthnRequest: Remove unused code.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/branches/ssoservice-merge@2063 44740490-163a-0410-bde0-09ae8108e29a

--- lib/SimpleSAML/XML/Shib13/AuthnRequest.php
+++ lib/SimpleSAML/XML/Shib13/AuthnRequest.php
@@ -10,23 +10,9 @@
  */
 class SimpleSAML_XML_Shib13_AuthnRequest {
 
-	private $metadata = null;
-	
 	private $issuer = null;
-	private $shire = null;
 	private $relayState = null;
-	
-	private $requestid = null;
-	
-	
-	const PROTOCOL = 'shib13';
 
-
-	function __construct() {
-		
-		$this->requestid = SimpleSAML_Utilities::generateID();
-	}
-	
 	public function setRelayState($relayState) {
 		$this->relayState = $relayState;
 	}
@@ -35,44 +21,13 @@ class SimpleSAML_XML_Shib13_AuthnRequest {
 		return $this->relayState;
 	}
 	
-	public function setShire($shire) {
-		$this->shire = $shire;
-	}
-	
-	public function getShire() {
-		return $this->shire;
-	}
-	
 	public function setIssuer($issuer) {
 		$this->issuer = $issuer;
 	}
 	public function getIssuer() {
 		return $this->issuer;
 	}
-	
 
-
-	public function parseGet($get) {
-		if (!isset($get['shire'])) throw new Exception('Could not read shire parameter from HTTP GET request');
-		if (!isset($get['providerId'])) throw new Exception('Could not read providerId parameter from HTTP GET request');
-		if (!isset($get['target'])) throw new Exception('Could not read target parameter from HTTP GET request');
-
-		$this->setIssuer($get['providerId']);
-		$this->setRelayState($get['target']);
-		
-		$this->setShire($get['shire']);
-
-	}
-	
-	public function setNewRequestID() {	
-		$this->requestid = SimpleSAML_Utilities::generateID();
-	}
-	
-	public function getRequestID() {
-		return $this->requestid;
-	}
-
-	
 	public function createRedirect($destination, $shire = NULL) {
 		$metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
 		$idpmetadata = $metadata->getMetaDataConfig($destination, 'shib13-idp-remote');
