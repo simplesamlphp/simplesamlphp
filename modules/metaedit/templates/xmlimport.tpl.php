74f16821c42e21268a2324b61a0ba35e32cacb48
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2010-01-07 11:14:55 +0000

    Using post instead of get...
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2080 44740490-163a-0410-bde0-09ae8108e29a

--- modules/metaedit/templates/xmlimport.tpl.php
+++ modules/metaedit/templates/xmlimport.tpl.php
@@ -13,7 +13,7 @@ $this->includeAtTemplateBase('includes/header.php');
 
 echo('<h1>Import SAML 2.0 XML Metadata</h1>');
 
-echo('<form method="get" action="edit.php">');
+echo('<form method="post" action="edit.php">');
 echo('<p>Paste in SAML 2.0 XML Metadata for the entity that you would like to add.</p>');
 echo('<textarea style="height: 200px; width: 90%; border: 1px solid #aaa;" cols="50" rows="5" name="xmlmetadata"></textarea>');
 echo('<input type="submit" style="margin-top: .5em" name="metasubmit" value="Import metadata" />');
