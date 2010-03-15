eb65cc858be72e8d2fee59d139e7674681d24251
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2010-03-15 13:55:48 +0000

    add module.php CLI to install modules
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2219 44740490-163a-0410-bde0-09ae8108e29a

--- modules/modinfo/templates/modlist.php
+++ modules/modinfo/templates/modlist.php
@@ -10,15 +10,29 @@ $this->includeAtTemplateBase('includes/header.php');
 
 <h2><?php echo($this->data['header']); ?></h2>
 
-<table>
+<table class="modules" style="width: 100%">
 <tr>
-<th><?php echo($this->t('{modinfo:modinfo:modlist_name}')); ?></th>
-<th><?php echo($this->t('{modinfo:modinfo:modlist_status}')); ?></th>
+<th colspan="2"><?php echo($this->t('{modinfo:modinfo:modlist_name}')); ?></th>
+<th ><?php echo($this->t('{modinfo:modinfo:modlist_status}')); ?></th>
+<th colspan="2"><?php echo($this->t('{modinfo:modinfo:version}')); ?></th>
 </tr>
 <?php
+
+$i = 0;
 foreach($this->data['modules'] as $id => $info) {
-	echo('<tr>');
-	echo('<td>' . htmlspecialchars($id) . '</td>');
+	echo('<tr class="' . ($i++ % 2 == 0 ? 'odd' : 'even') . '">');
+	
+	
+	if (isset($info['def'])) {
+		echo('<td><a href="http://simplesamlphp.org/modules/' . htmlspecialchars($id) . '">' . htmlspecialchars($info['def']->def['name']) . '</a></td>');		
+	} else {
+		echo('<td> </td>');
+	}
+	
+	
+	echo('<td><tt>' . htmlspecialchars($id) . '</tt></td>');
+	
+	
 	if($info['enabled']) {
 		echo('<td><img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/accept.png" alt="' .
 			htmlspecialchars($this->t('{modinfo:modinfo:modlist_enabled}')) . '" /></td>');
@@ -26,6 +40,22 @@ foreach($this->data['modules'] as $id => $info) {
 		echo('<td><img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/delete.png" alt="' .
 			htmlspecialchars($this->t('{modinfo:modinfo:modlist_disabled}')) . '" /></td>');
 	}
+	
+	if (isset($info['def'])) {
+		echo('<td>' . htmlspecialchars($info['def']->getVersion()) . ' (' .htmlspecialchars($info['def']->getBranch()) . ')</td>');	
+		if ($info['def']->updateExists()) {
+			echo('<td><img style="display: inline" src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/delete.png" alt="' .
+				htmlspecialchars($this->t('{modinfo:modinfo:update_exists}')) . '" /> ' . 
+				htmlspecialchars($this->t('{modinfo:modinfo:update_exists}')) . '</td>');		
+		} else {
+			echo('<td><img style="display: inline" src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/accept.png" alt="' .
+				htmlspecialchars($this->t('{modinfo:modinfo:latest_version}')) . '" /> ' . 
+				htmlspecialchars($this->t('{modinfo:modinfo:latest_version}')) . '</td>');
+		}
+	} else {
+		echo('<td colspan="2"> </td>');
+	}
+	
 	echo('</tr>');
 }
 ?>
--- modules/modinfo/www/index.php
+++ modules/modinfo/www/index.php
@@ -9,7 +9,19 @@ foreach($modules as $m) {
 	$modinfo[$m] = array(
 		'enabled' => SimpleSAML_Module::isModuleEnabled($m),
 	);
+	if (sspmod_core_ModuleDefinition::isDefined($m)) {
+		$modinfo[$m]['def'] = sspmod_core_ModuleDefinition::load($m);
+	}
+
 }
+
+function cmpa($a, $b) {
+	
+    if (isset($a['def']) && !isset($b['def'])) return -1;
+    if (isset($b['def']) && !isset($a['def'])) return 1;
+	return 0;
+}
+uasort($modinfo, 'cmpa');
 
 $config = SimpleSAML_Configuration::getInstance();
 $t = new SimpleSAML_XHTML_Template($config, 'modinfo:modlist.php');
