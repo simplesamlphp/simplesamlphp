29902ca9e12f3b58bce0064fd14ccd9dd01b8f59
Olav Morken <olav.morken@uninett.no>
2010-04-28 08:16:04 +0000

    IdP: Slight refactoring of resumeauth.php
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2273 44740490-163a-0410-bde0-09ae8108e29a

--- modules/core/www/idp/resumeauth.php
+++ modules/core/www/idp/resumeauth.php
@@ -1,15 +1,14 @@
 <?php
 
-if (isset($_REQUEST['RequestID'])) {
-	/* Backwards-compatibility with old authentication pages. */
-	$session = SimpleSAML_Session::getInstance();
-	$requestcache = $session->getAuthnRequest('saml2', (string)$_REQUEST['RequestID']);
-	if (!$requestcache) {
-		throw new Exception('Could not retrieve cached RequestID = ' . $authId);
-	}
-	$state = $requestcache['State'];
-	SimpleSAML_IdP::postAuth($state);
-
-} else {
+if (!isset($_REQUEST['RequestID'])) {
 	throw new SimpleSAML_Error_BadRequest('Missing required URL parameter.');
 }
+
+/* Backwards-compatibility with old authentication pages. */
+$session = SimpleSAML_Session::getInstance();
+$requestcache = $session->getAuthnRequest('saml2', (string)$_REQUEST['RequestID']);
+if (!$requestcache) {
+	throw new Exception('Could not retrieve cached RequestID = ' . $authId);
+}
+$state = $requestcache['State'];
+SimpleSAML_IdP::postAuth($state);
