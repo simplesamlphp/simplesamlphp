ef795ae2c69e508aa1545f6ed9ad646e9112315b
Olav Morken <olav.morken@uninett.no>
2010-08-03 09:34:22 +0000

    admin/phpinfo.php: Cleanups.
    - No need to catch exceptions here, since _include.php installs a
      default exception handler.
    - The $config and $session variables are unused.
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2468 44740490-163a-0410-bde0-09ae8108e29a

--- www/admin/phpinfo.php
+++ www/admin/phpinfo.php
@@ -2,17 +2,7 @@
 
 require_once('../_include.php');
 
-try {
+/* Make sure that the user has admin access rights. */
+SimpleSAML_Utilities::requireAdmin();
 
-	$config = SimpleSAML_Configuration::getInstance();
-	$session = SimpleSAML_Session::getInstance();
-
-	/* Make sure that the user has admin access rights. */
-	SimpleSAML_Utilities::requireAdmin();
-	phpinfo();
-
-} catch(Exception $e) {
-	SimpleSAML_Utilities::fatalError('na', NULL, $e);
-}
-
-?>
+phpinfo();
