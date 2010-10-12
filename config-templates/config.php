f9ba533de68506ba5443d57ced3f83fe37c34e39
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2010-10-12 13:04:24 +0000

    Language update. General update. Adding new language: Czech, Italiana, LietuviÅ³ kalba
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2577 44740490-163a-0410-bde0-09ae8108e29a

--- config-templates/config.php
+++ config-templates/config.php
@@ -200,7 +200,7 @@ $config = array (
 	/*
 	 * Languages available and what language is default
 	 */
-	'language.available'	=> array('en', 'no', 'nn', 'se', 'fi', 'da', 'sv', 'de', 'es', 'fr', 'nl', 'lb', 'hr', 'hu', 'pl', 'sl', 'pt', 'pt-BR', 'tr'),
+	'language.available'	=> array('en', 'no', 'nn', 'se', 'da', 'de', 'sv', 'fi', 'es', 'fr', 'it', 'nl', 'lb', 'cs', 'sl', 'lt', 'hr', 'hu', 'pl', 'pt', 'pt-BR', 'tr'),
 	'language.default'		=> 'en',
 	
 	/*
