f26589eaf61b3ee63a411bbac10afd68e3b60650
Mads Freek Petersen <freek@wayf.dk>
2010-10-22 11:08:54 +0000

    Added support for cas proxying. Added ttl for tickets. Moved validate functionality to serviceValidate.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2587 44740490-163a-0410-bde0-09ae8108e29a

--- modules/casserver/www/login.php
+++ modules/casserver/www/login.php
@@ -1,4 +1,5 @@
 <?php
+require 'tickets.php';
 
 /*
  * Incomming parameters:
@@ -8,32 +9,17 @@
  *  
  */
 
-
 if (!array_key_exists('service', $_GET))
 	throw new Exception('Required URL query parameter [service] not provided. (CAS Server)');
 
 $service = $_GET['service'];
-$renew = FALSE;
-$gateway = FALSE;
 
-if (array_key_exists('renew', $_GET)) {
-	$renew = TRUE;
-}
+$forceAuthn =isset($_GET['renew']) && $_GET['renew'];
+$isPassive = isset($_GET['gateway']) && $_GET['gateway'];
 
-if (array_key_exists('gateway', $_GET)) {
-	$gateway = TRUE;
-	throw new Exception('CAS gateway to SAML IsPassive: Not yet implemented properly.');
-}
-
-
-
-
-
-/* Load simpleSAMLphp, configuration and metadata */
 $config = SimpleSAML_Configuration::getInstance();
 $casconfig = SimpleSAML_Configuration::getConfig('module_casserver.php');
 $session = SimpleSAML_Session::getInstance();
-
 
 $legal_service_urls = $casconfig->getValue('legal_service_urls');
 if (!checkServiceURL($service, $legal_service_urls))
@@ -41,65 +27,37 @@ if (!checkServiceURL($service, $legal_service_urls))
 
 $auth = $casconfig->getValue('auth', 'saml2');
 if (!in_array($auth, array('saml2', 'shib13')))
-	throw new Exception('CAS Service configured to use [auth] = ' . $auth . ' only [saml2,shib13] is legal.');
-
-if (!$session->isValid($auth) ) {
-	SimpleSAML_Utilities::redirect(
-		'/' . $config->getBaseURL() . $auth . '/sp/initSSO.php',
-		array('RelayState' => SimpleSAML_Utilities::selfURL() )
+ 	throw new Exception('CAS Service configured to use [auth] = ' . $auth . ' only [saml2,shib13] is legal.');
+ 
+/*
+if (!$session->isValid($auth)) {
+	$url = SimpleSAML_Utilities::selfURL();
+	$hints = array(
+		SimpleSAML_Auth_State::RESTART => $url,
+	    'ForceAuthn' => $forceAuthn,
+        'isPassive' => $isPassive,
 	);
+	SimpleSAML_Auth_Default::initLogin($auth, $url, $url, $hints);
 }
+
 $attributes = $session->getAttributes();
+*/
 
-$path = $casconfig->resolvePath($casconfig->getValue('ticketcache', 'ticketcache'));
+$attributes = array('mail' => array('freek@ruc.dk'), 'anton' => array('banton'));
+
+$path = $casconfig->resolvePath($casconfig->getValue('ticketcache', '/tmp'));
+
 $ticket = str_replace( '_', 'ST-', SimpleSAML_Utilities::generateID() );
-storeTicket($ticket, $path, $attributes);
-
-// $test = retrieveTicket($ticket, $path);
-
+storeTicket($ticket, $path, array('service' => $service,
+	'forceAuthn' => $forceAuthn,
+	'attributes' => $attributes,
+	'proxies' => array(),
+	'validbefore' => time() + 5));
 
 SimpleSAML_Utilities::redirect(
 	SimpleSAML_Utilities::addURLparameter($service,
 		array('ticket' => $ticket)
 	)
 );
-
-
-
-function storeTicket($ticket, $path, &$value ) {
-
-	if (!is_dir($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-		
-	if (!is_writable($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] is not writable. ');
-
-	$filename = $path . '/' . $ticket;
-	file_put_contents($filename, serialize($value));
-}
-
-function retrieveTicket($ticket, $path) {
-
-	if (!is_dir($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-
-
-	$filename = $path . '/' . $ticket;
-	return unserialize(file_get_contents($filename));
-}
-
-
-
-function checkServiceURL($service, array $legal_service_urls) {
-	foreach ($legal_service_urls AS $legalurl) {
-		if (strpos($service, $legalurl) === 0) return TRUE;
-	}
-	return FALSE;
-}
-
-
-
-
-
 
 ?>
--- modules/casserver/www/serviceValidate.php
+++ modules/casserver/www/serviceValidate.php
@@ -1,4 +1,8 @@
 <?php
+require 'tickets.php';
+
+# set manually if called directly - ie not included from validate.php or cas.php
+if (!$function) $function = 'serviceValidate';
 
 /*
  * Incomming parameters:
@@ -8,107 +12,90 @@
  *
  */
 
-
-if (!array_key_exists('service', $_GET))
+if (array_key_exists('service', $_GET)) {
+	$service = $_GET['service'];
+	$ticket = $_GET['ticket'];
+	$forceAuthn = isset($_GET['renew']) && $_GET['renew'];
+} else { 
 	throw new Exception('Required URL query parameter [service] not provided. (CAS Server)');
-
-$service = $_GET['service'];
-
-if (!array_key_exists('ticket', $_GET))
-	throw new Exception('Required URL query parameter [ticket] not provided. (CAS Server)');
-
-$ticket = $_GET['ticket'];
-
-$renew = FALSE;
-
-if (array_key_exists('renew', $_GET)) {
-	$renew = TRUE;
 }
 
-
-
 try {
-	/* Load simpleSAMLphp, configuration and metadata */
+/* Load simpleSAMLphp, configuration and metadata */
 	$casconfig = SimpleSAML_Configuration::getConfig('module_casserver.php');
 	
-	
 	$path = $casconfig->resolvePath($casconfig->getValue('ticketcache', 'ticketcache'));
-	
 	$ticketcontent = retrieveTicket($ticket, $path);
 	
 	$usernamefield = $casconfig->getValue('attrname', 'eduPersonPrincipalName');
-	$dosendattributes = $casconfig->getValue('attributes', FALSE);;
+	$dosendattributes = $casconfig->getValue('attributes', FALSE);
 	
-	if (array_key_exists($usernamefield, $ticketcontent)) {
-		returnResponse('YES', $ticketcontent[$usernamefield][0], $dosendattributes ? $ticketcontent : array());
+	$attributes = $ticketcontent['attributes'];
+
+	$pgtiouxml = "";
+
+	if ($ticketcontent['service'] == $service
+			&& $ticketcontent['forceAuthn'] == $forceAuthn
+			&& array_key_exists($usernamefield, $attributes)
+			&& $ticketcontent['validbefore'] > time()) {
+					
+		if (isset($_GET['pgtUrl'])) {
+			$pgtUrl = $_GET['pgtUrl'];
+			$pgtiou = str_replace( '_', 'PGTIOU-', SimpleSAML_Utilities::generateID());
+			$pgt = str_replace( '_', 'PGT-', SimpleSAML_Utilities::generateID());
+			$content = array(
+				'attributes' => $attributes,
+				'forceAuthn' => false,
+				'proxies' => array_merge(array($service), $ticketcontent['proxies']),
+				'validbefore' => time() + 60);
+			file_get_contents($pgtUrl . '?pgtIou=' . $pgtiou . '&pgtId=' . $pgt);
+			storeTicket($pgt, $path, $content);
+			$pgtiouxml = "\n<cas:proxyGrantingTicket>$pgtiou</cas:proxyGrantingTicket>\n";
+		}
+		
+		$proxiesxml = join("\n", array_map(create_function('$a', 'return "<cas:proxy>$a</cas:proxy>";'), $ticketcontent['proxies']));
+		if ($proxiesxml) $proxiesxml = "<cas:proxies>\n$proxiesxml\n</cas:proxies>\n";
+		returnResponse('YES', $function, $attributes[$usernamefield][0], $dosendattributes ? $attributes : array(), $pgtiouxml.$proxiesxml);
 	} else {
-		returnResponse('NO');
+		returnResponse('NO', $function);
 	}
 
 } catch (Exception $e) {
-
-	returnResponse('NO', $e->getMessage());
+	returnResponse('NO', $function, $e->getMessage());
 }
 
-function returnResponse($value, $content = '', $attributes = array()) {
-	if ($value === 'YES') {
-		$attributesxml = "";
-		foreach ($attributes as $attributename => $attributelist) {
-			$attr = htmlspecialchars($attributename);
-			foreach ($attributelist as $attributevalue) {
-				$attributesxml .= "<cas:$attr>" . htmlspecialchars($attributevalue) . "</cas:$attr>\n";
+function returnResponse($value, $function, $usrname = '', $attributes = array(), $xtraxml = "") {
+	if ($value === 'YES') {	
+		if ($function == 'serviceValidate') {
+			$attributesxml = "";
+			foreach ($attributes as $attributename => $attributelist) {
+				$attr = htmlspecialchars($attributename);
+				foreach ($attributelist as $attributevalue) {
+					$attributesxml .= "<cas:$attr>" . htmlspecialchars($attributevalue) . "</cas:$attr>\n";
+				}
 			}
+			if (sizeof($attributes)) $attributesxml = "<cas:attributes>\n" . $attributesxml . "</cas:attributes>\n";
+			echo '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+<cas:authenticationSuccess>
+<cas:user>' . htmlspecialchars($usrname) . '</cas:user>' .
+		$xtraxml .
+		$attributesxml .
+		'</cas:authenticationSuccess>
+</cas:serviceResponse>';
+		} else {
+			echo 'YES' . "\n" . $username;
 		}
-		if (sizeof($attributes)) $attributesxml = '<cas:attributes>' . $attributesxml . '</cas:attributes>';
-		echo '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
-    <cas:authenticationSuccess>
-	<cas:user>' . htmlspecialchars($content) . '</cas:user>' .
-	$attributesxml .
-    '</cas:authenticationSuccess>
-</cas:serviceResponse>';
-
 	} else {
-		echo '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
-    <cas:authenticationFailure code="...">
-	' . $content . '
-    </cas:authenticationFailure>
+		if ($function == 'serviceValidate') {
+			echo '<cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+<cas:authenticationFailure code="">
+</cas:authenticationFailure>
 </cas:serviceResponse>';
+		} else {
+			echo 'NO';
+
+		}
 	}
 }
-
-
-function storeTicket($ticket, $path, &$value ) {
-
-	if (!is_dir($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-		
-	if (!is_writable($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] is not writable. ');
-
-	$filename = $path . '/' . $ticket;
-	file_put_contents($filename, serialize($value));
-}
-
-function retrieveTicket($ticket, $path) {
-
-	if (!preg_match('/^ST-?[a-zA-Z0-9]+$/D', $ticket)) throw new Exception('Invalid characters in ticket');
-
-	if (!is_dir($path)) 
-		throw new Exception('Directory for CAS Server ticket storage [' . $path . '] does not exists. ');
-
-	$filename = $path . '/' . $ticket;
-
-	if (!file_exists($filename))
-		throw new Exception('Could not find ticket');
-	
-	$content = file_get_contents($filename);
-	
-	unlink($filename);
-	
-	return unserialize($content);
-}
-
-
-
 
 ?>
