781fda186d8b830a44c13b7220c32d68a331ba13
Olav Morken <olav.morken@uninett.no>
2010-11-02 11:20:36 +0000

    metadata-templates: Use most recent SLO url.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2615 44740490-163a-0410-bde0-09ae8108e29a

--- metadata-templates/saml20-idp-remote.php
+++ metadata-templates/saml20-idp-remote.php
@@ -34,8 +34,7 @@ $metadata['https://idp-test.feide.no'] = array(
 	'description'                  => 'Feide test environment (idp-test.feide.no). Authenticate with your identity from a school or university in Norway.',
 
 	'SingleSignOnService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SingleLogoutServiceiFrame.php',
-	'SingleLogoutServiceResponse'  => 'https://idp-test.feide.no/simplesaml/saml2/idp/SingleLogoutServiceiFrameResponse.php',
+	'SingleLogoutService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
 
 	'certFingerprint'              => 'fa982efdb69f26e8073c8f815a82a0c5885960a2',
 	'hint.cidr'                    => '158.38.0.0/16',
@@ -48,8 +47,7 @@ $metadata['https://idp.feide.no'] = array(
 		'no' => 'Logg inn med din identitet fra skolen eller universitetet du er tilknyttet (i Norge).',
 	),
 	'SingleSignOnService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SingleLogoutServiceiFrame.php',
-	'SingleLogoutServiceResponse'  => 'https://idp.feide.no/simplesaml/saml2/idp/SingleLogoutServiceiFrameResponse.php',
+	'SingleLogoutService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
 	'certFingerprint'              => 'cde69e332fa7dd0eaa99ee0ddf06916e8942ac53',
 	'hint.cidr'                    => '158.38.0.0/16',
 );
