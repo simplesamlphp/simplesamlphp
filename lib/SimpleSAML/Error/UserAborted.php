abf791c2298bb2103d06ab92111e935effcd865c
Olav Morken <olav.morken@uninett.no>
2011-02-08 13:50:42 +0000

    Error_UserAborted: New error class for when the user aborts the login process.
    Thanks to Brook Schofield for adding this.
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2733 44740490-163a-0410-bde0-09ae8108e29a

---
+++ lib/SimpleSAML/Error/UserAborted.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Exception indicating user aborting the authentication proces.
+ *
+ * @package simpleSAMLphp_base
+ * @version $Id$
+ *
+ */
+class SimpleSAML_Error_UserAborted extends SimpleSAML_Error_User{
+	
+}
+
+?>
