434c01a6d11590f7c0dd015cb2581afcc0d9653c
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2011-05-02 08:09:41 +0000

    Tidy up configuration files, and removed logos...
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2821 44740490-163a-0410-bde0-09ae8108e29a

--- modules/discojuice/config-templates/discojuicecentral.php
+++ modules/discojuice/config-templates/discojuicecentral.php
@@ -1,15 +1,25 @@
 <?php
 
 /**
- * This is the configuration file for the Auth MemCookie example.
+ * This is a DiscoJuice configuration file.
+ * 
+ * This configuration is used for the standalone DiscoJuice instance, that will become available at:
+ *  /simplesaml/module.php/discojuice/central.php
+ *
+ * For details about the configuration option visit the DiscoJuice documentation:
+ *  http://discojuice.simplesamlphp.org/docs
  */
 
 $config = array(
 
+	// Which hostnames should be allowed to read user selections on entities chosen using DiscoJuice.
+	// You need to enable those hostnames that is configured to read data using the DiscoJuiceReadWrite protocol.
+	// 		http://discojuice.simplesamlphp.org/docs/1.0/discoreadwrite
 	'acl' => array(
-		'simplesamlphp.org', 'foodl.org',
+		'simplesamlphp.org', 'example.org',
 	),
 	
+	// DiscoJuice opitions for the central DiscoJuice page.
 	'discojuice.options' => array(
 		"title"=> 'Sign in to <strong>this service</strong>',
 		"subtitle"=> "Select your Provider",
@@ -22,7 +32,5 @@ $config = array(
 		"location"=> true,
 		"debug.weight" => false,
 	),
-	
-	
 
 );
--- modules/discojuice/config-templates/discojuiceembed.php
+++ modules/discojuice/config-templates/discojuiceembed.php
@@ -1,7 +1,12 @@
 <?php
 
 /**
- * This is the configuration file for the Auth MemCookie example.
+ * This is a DiscoJuice configuration file.
+ * 
+ * This configuration is used for the embedded DiscoJuice instance.
+ *
+ * For details about the configuration option visit the DiscoJuice documentation:
+ *  http://discojuice.simplesamlphp.org/docs
  */
 
 $config = array(
@@ -9,6 +14,21 @@ $config = array(
 	'discojuice.options' => array(
 		"title"=> 'Sign in to <strong>Foodle</strong>',
 		"subtitle"=> "Select your Provider",
+		
+		//	Want to override the inline help texts in DiscoJuice, uncomment the following section:
+	
+		/*
+			'textSearch' => 'søk etter noe...',
+			'textHelp' => 'Finner du ikke en innnloggsingstjener du kjenner?',
+			'textHelpMore' => 'Let mer...',
+ 		*/
+
+		// Where to fetch metadata from. DiscoJuiceJSON format..
+		// You may provide a 'callback=?' querystring parameter in ordert to support JSONP.
+		// By default this feed endpoint is automatically configured to be correct for the 
+		// DiscoJuice simpleSAMLphp module (which has a built-in feed generator). 
+		
+		//	"metadata" => 'http://example.org/discojuicejson/index.php?callback=?',
 		
 		"always"=> false,
 		"overlay"=> true,
@@ -23,23 +43,12 @@ $config = array(
 		
 function(e) {
 
-	console.log(e);
-
+	// The auth parameter is indicating which authentication method is being used.
 	var auth = e.auth || null;
-	var returnto = window.location.href || 'https://foodl.org';
-	switch(auth) {
-		
-		case 'twitter':
-			window.location = 'https://foodl.org/simplesaml/module.php/core/as_login.php?AuthId=twitter&ReturnTo=' + escape(returnto);
-		break;
-	
-	
-		case 'saml':
-		default:
-			window.location = 'https://foodl.org/simplesaml/module.php/core/as_login.php?AuthId=saml&ReturnTo=' + escape(returnto) + '&saml:idp=' + escape(e.entityID);
-		break;							
-			
-	}
+	var returnto = window.location.href || 'https://example.org';
+
+	window.location = 'https://foodl.org/simplesaml/module.php/core/as_login.php?AuthId=saml&ReturnTo=' + escape(returnto) + '&saml:idp=' + escape(e.entityID);
+
 }
 
 	"
