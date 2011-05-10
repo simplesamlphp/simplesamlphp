a3ef57c4b2361157b2af199eb58a7a73b24dc48d
Olav Morken <olav.morken@uninett.no>
2011-05-10 08:38:44 +0000

    saml: Add AuthnContextClassRef filter.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2832 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/saml/lib/Auth/Process/AuthnContextClassRef.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Filter for setting the AuthnContextClassRef in the response.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+class sspmod_saml_lib_Auth_Process_AuthnContextClassRef extends SimpleSAML_Auth_ProcessingFilter {
+
+	/**
+	 * The URI we should set as the AuthnContextClassRef in the login response.
+	 *
+	 * @var string
+	 */
+	private $authnContextClassRef;
+
+
+	/**
+	 * Initialize this filter.
+	 *
+	 * @param array $config  Configuration information about this filter.
+	 * @param mixed $reserved  For future use.
+	 */
+	public function __construct($config, $reserved) {
+		parent::__construct($config, $reserved);
+		assert('is_array($config)');
+
+		if (!isset($config['AuthnContextClassRef'])) {
+			throw new SimpleSAML_Error_Exception('Missing AuthnContextClassRef option in processing filter.');
+		}
+	}
+
+
+}
