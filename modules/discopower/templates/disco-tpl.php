ca22bba732c4cb21d68969df10166d7d72360e7f
Olav Morken <olav.morken@uninett.no>
2011-05-26 10:56:01 +0000

    discopower: Hide empty tabs.
    Thanks to Dyonisius Visser for fixing this!
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2851 44740490-163a-0410-bde0-09ae8108e29a

--- modules/discopower/templates/disco-tpl.php
+++ modules/discopower/templates/disco-tpl.php
@@ -149,7 +149,9 @@ if (!empty($faventry)) {
     	
     		$tabs = array_keys( $this->data['idplist']);
     		foreach ($tabs AS $tab) {
-    			echo '<li><a href="#' . $tab . '"><span>' . $this->t('{discopower:tabs:' . $tab . '}') . '</span></a></li> ';
+			if(!empty($this->data['idplist'][$tab])) {
+				echo '<li><a href="#' . $tab . '"><span>' . $this->t('{discopower:tabs:' . $tab . '}') . '</span></a></li> ';
+			}
     		}
     	
     	?>
