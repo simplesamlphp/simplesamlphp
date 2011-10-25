e3decf9f2ef3296bc71e271800ffb28195855094
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2011-10-25 12:04:18 +0000

    Adding helped function to load hosted configuration
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2949 44740490-163a-0410-bde0-09ae8108e29a

--- modules/discojuice/www/discojuice/discojuice.hosted.js
+++ modules/discojuice/www/discojuice/discojuice.hosted.js
@@ -5,35 +5,45 @@
  */
 if (typeof DiscoJuice == "undefined") var DiscoJuice = {};
 
+function getConfig (target, title, spentityid, responseurl, feeds, redirectURL) {
+	var options, i;
+	
+	options = {
+		"title": "Sign in to <strong>" + title + "</strong>",
+		"subtitle": "Select your Provider",
+		"disco": {
+			"spentityid": spentityid,
+			"url": responseurl,
+			"stores": ["https://store.discojuice.org/"],
+			"writableStore": "https://store.discojuice.org/"
+		},
+		"cookie": true,
+		"country": true,
+		"location": true,
+		"countryAPI": "https://store.discojuice.org/country",
+		"discoPath": "https://static.discojuice.org/",
+		"callback": function (e, djc) {
+            var returnto = window.location.href;
+			window.location = redirectURL + escape(e.entityID);
+		},
+		"metadata": []
+	};
+	
+	for(i = 0; i < feeds.length; i++) {
+		options.metadata.push("https://static.discojuice.org/feeds/" + feeds[i]);
+	}
+	return options;
+}
+
+
 DiscoJuice.Hosted = {
 	
+	"getConfig": getConfig,
+		
 	"setup": function (target, title, spentityid, responseurl, feeds, redirectURL) {
-		var options, i;
-		
-		options = {
-			"title": "Sign in to <strong>" + title + "</strong>",
-			"subtitle": "Select your Provider",
-			"disco": {
-				"spentityid": spentityid,
-				"url": responseurl,
-				"stores": ["https://store.discojuice.org/"],
-				"writableStore": "https://store.discojuice.org/"
-			},
-			"cookie": true,
-			"country": true,
-			"location": true,
-			"countryAPI": "https://store.discojuice.org/country",
-			"discoPath": "https://static.discojuice.org/",
-			"callback": function (e, djc) {
-                var returnto = window.location.href;
-				window.location = redirectURL + escape(e.entityID);
-			},
-			"metadata": []
-		};
-		
-		for(i = 0; i < feeds.length; i++) {
-			options.metadata.push("https://static.discojuice.org/feeds/" + feeds[i]);
-		}
+		var options;
+
+		options = getConfig(target, title, spentityid, responseurl, feeds, redirectURL);
 		
 		$(document).ready(function() {
 			$(target).DiscoJuice(options);
