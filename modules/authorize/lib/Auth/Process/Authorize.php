b2306e36fc079cb3e727fe1d9cf2a9f2eeb58d6c
Olav Morken <olav.morken@uninett.no>
2011-11-21 13:01:55 +0000

    authorize:Authorize: Add deny & regex flags.
    This patch adds two new features to the authorize:Authorize filter:
    
    - Add flag to change the policy from default-deny to default-allow. This
      makes it possible to block specific users.
    - Add a flag that changes the matching algorithm from regex matching to
      string matching, which simplifies matching with strings that contain
      special characters.
    
    This patch also makes it simpler to subclass this filter.
    
    Thanks to Ryan Panning for implementing this!
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@2987 44740490-163a-0410-bde0-09ae8108e29a

--- modules/authorize/lib/Auth/Process/Authorize.php
+++ modules/authorize/lib/Auth/Process/Authorize.php
@@ -4,18 +4,32 @@
  * Filter to authorize only certain users.
  * See docs directory.
  *
- * @author Ernesto Revilla, Yaco Sistemas SL.
+ * @author Ernesto Revilla, Yaco Sistemas SL., Ryan Panning
  * @package simpleSAMLphp
  * @version $Id$
  */
 class sspmod_authorize_Auth_Process_Authorize extends SimpleSAML_Auth_ProcessingFilter {
 
 	/**
+	 * Flag to deny/unauthorize the user a attribute filter IS found
+	 *
+	 * @var bool
+	 */
+	protected $deny = FALSE;
+
+	/**
+	 * Flag to turn the REGEX pattern matching on or off
+	 *
+	 * @var bool
+	 */
+	protected $regex = TRUE;
+
+	/**
 	 * Array of valid users. Each element is a regular expression. You should
 	 * user \ to escape special chars, like '.' etc.
 	 *
 	 */
-	private $valid_attribute_values = array();
+	protected $valid_attribute_values = array();
 
 
 	/**
@@ -29,6 +43,20 @@ class sspmod_authorize_Auth_Process_Authorize extends SimpleSAML_Auth_Processing
 		parent::__construct($config, $reserved);
 
 		assert('is_array($config)');
+
+		// Check for the deny option, get it and remove it
+		// Must be bool specifically, if not, it might be for a attrib filter below
+		if (isset($config['deny']) && is_bool($config['deny'])) {
+			$this->deny = $config['deny'];
+			unset($config['deny']);
+		}
+
+		// Check for the regex option, get it and remove it
+		// Must be bool specifically, if not, it might be for a attrib filter below
+		if (isset($config['regex']) && is_bool($config['regex'])) {
+			$this->regex = $config['regex'];
+			unset($config['regex']);
+		}
 
 		foreach ($config as $attribute => $values) {
 			if (is_string($values))
@@ -51,7 +79,7 @@ class sspmod_authorize_Auth_Process_Authorize extends SimpleSAML_Auth_Processing
 	 * @param array &$request  The current request
 	 */
 	public function process(&$request) {
-		$authorize = FALSE;
+		$authorize = $this->deny;
 		assert('is_array($request)');
 		assert('array_key_exists("Attributes", $request)');
 
@@ -64,8 +92,13 @@ class sspmod_authorize_Auth_Process_Authorize extends SimpleSAML_Auth_Processing
 					if (!is_array($values))
 						$values = array($values);
 					foreach ($values as $value){
-						if(preg_match($pattern, $value)) {
-							$authorize = TRUE;
+						if ($this->regex) {
+							$matched = preg_match($pattern, $value);
+						} else {
+							$matched = ($value == $pattern);
+						}
+						if ($matched) {
+							$authorize = ($this->deny ? FALSE : TRUE);
 							break 3;
 						}
 					}
@@ -73,13 +106,30 @@ class sspmod_authorize_Auth_Process_Authorize extends SimpleSAML_Auth_Processing
 			}
 		}
 		if (!$authorize){
-			/* Save state and redirect to 403 page. */
-			$id = SimpleSAML_Auth_State::saveState($request,
-				'authorize:Authorize');
-			$url = SimpleSAML_Module::getModuleURL(
-				'authorize/authorize_403.php');
-			SimpleSAML_Utilities::redirect($url, array('StateId' => $id));
+			$this->unauthorized($request);
 		}
+	}
+
+
+	/**
+	 * When the process logic determines that the user is not
+	 * authorized for this service, then forward the user to
+	 * an 403 unauthorized page.
+	 *
+	 * Separated this code into its own method so that child
+	 * classes can override it and change the action. Forward
+	 * thinking in case a "chained" ACL is needed, more complex
+	 * permission logic.
+	 *
+	 * @param array $request
+	 */
+	protected function unauthorized(&$request) {
+		/* Save state and redirect to 403 page. */
+		$id = SimpleSAML_Auth_State::saveState($request,
+			'authorize:Authorize');
+		$url = SimpleSAML_Module::getModuleURL(
+			'authorize/authorize_403.php');
+		SimpleSAML_Utilities::redirect($url, array('StateId' => $id));
 	}
 }
 
