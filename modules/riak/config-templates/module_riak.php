b67af1993ceb41e686665f92ff2898c5dbdb4022
Olav Morken <olav.morken@uninett.no>
2012-06-29 09:21:17 +0000

    riak: New module which enables storing sessions in Riak.
    Thanks to David Gwynne for implementing this!
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3128 44740490-163a-0410-bde0-09ae8108e29a

---
+++ modules/riak/config-templates/module_riak.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * The configuration of the riak Store module
+ *
+ * $Id$
+ */
+
+$config = array (
+	/*
+	 * This module has the following config options and defaults.
+	 *
+	 * 'path' => 'riak-php-client/riak.php',
+	 * 'host' => 'localhost',
+	 * 'port' => 8098,
+	 * 'bucket' => 'SimpleSAMLphp',
+	 */
+);
