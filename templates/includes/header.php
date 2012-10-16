1d3a030d4c1a21c3828c1d193121c1307466b00f
Olav Morken <olav.morken@uninett.no>
2012-10-16 09:11:57 +0000

    Fix header set after HTML output.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3192 44740490-163a-0410-bde0-09ae8108e29a

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 
 
@@ -40,7 +39,7 @@ if (array_key_exists('pageid', $this->data)) {
  */
 header('X-Frame-Options: SAMEORIGIN');
 
-?>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
