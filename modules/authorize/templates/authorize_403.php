627e991712da513136a4578334cf58bdcca5e999
Jaime Pérez Crespo <jaime.perez@uninett.no>
2012-11-16 10:10:36 +0000

    Fix for issue #522. A logout link is now provided in the 403 error page of the 'authorize' module.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3201 44740490-163a-0410-bde0-09ae8108e29a

--- modules/authorize/templates/authorize_403.php
+++ modules/authorize/templates/authorize_403.php
@@ -10,6 +10,7 @@
  * @version $Id$
  */
 
+global $state;
 
 $this->data['403_header'] = $this->t('{authorize:Authorize:403_header}');
 $this->data['403_text'] = $this->t('{authorize:Authorize:403_text}');
@@ -18,6 +19,7 @@ $this->includeAtTemplateBase('includes/header.php');
 ?>
 <h1><?php echo $this->data['403_header']; ?></h1>
 <p><?php echo $this->data['403_text']; ?></p>
+<p><a href="<?php echo SimpleSAML_Module::getModuleURL('core/authenticate.php', array('as' => $state['Source']['auth']))."&logout"; ?>"><?php echo $this->t('{status:logout}'); ?></a></p>
 <?php
 $this->includeAtTemplateBase('includes/footer.php');
 ?>
