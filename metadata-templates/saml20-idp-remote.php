7db5f39c80aa233017bc3a6d817d715ce6be07c0
Jaime Pérez Crespo <jaime.perez@uninett.no>
2013-05-23 08:33:22 +0000

    Removed some feide entities from the metadata templates.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3244 44740490-163a-0410-bde0-09ae8108e29a

--- metadata-templates/saml20-idp-remote.php
+++ metadata-templates/saml20-idp-remote.php
@@ -22,32 +22,3 @@ $metadata['https://openidp.feide.no'] = array(
 	'certFingerprint'      => 'c9ed4dfb07caf13fc21e0fec1572047eb8a7a4cb'
 );
 
-
-/*
- * Feide, the norwegian federation. Test and production metadata.
- */
-$metadata['https://idp-test.feide.no'] = array(
-	'name' => array(
-		'en' => 'Feide Test environment',
-		'no' => 'Feide testmiljø',
-	),
-	'description'                  => 'Feide test environment (idp-test.feide.no). Authenticate with your identity from a school or university in Norway.',
-
-	'SingleSignOnService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp-test.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-
-	'certFingerprint'              => 'fa982efdb69f26e8073c8f815a82a0c5885960a2',
-	'hint.cidr'                    => '158.38.0.0/16',
-);
-
-$metadata['https://idp.feide.no'] = array(
-	'name' => 'Feide',
-	'description' => array(
-		'en' => 'Authenticate with your identity from a school or university in Norway.',
-		'no' => 'Logg inn med din identitet fra skolen eller universitetet du er tilknyttet (i Norge).',
-	),
-	'SingleSignOnService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SSOService.php',
-	'SingleLogoutService'          => 'https://idp.feide.no/simplesaml/saml2/idp/SingleLogoutService.php',
-	'certFingerprint'              => 'cde69e332fa7dd0eaa99ee0ddf06916e8942ac53',
-	'hint.cidr'                    => '158.38.0.0/16',
-);
--- metadata-templates/shib13-sp-remote.php
+++ metadata-templates/shib13-sp-remote.php
@@ -5,20 +5,12 @@
  * See: https://rnd.feide.no/content/sp-remote-metadata-reference
  */
 
+/*
+ * This is just an example:
+ */
 $metadata['https://sp.shiblab.feide.no'] = array(
 	'AssertionConsumerService' => 'http://sp.shiblab.feide.no/Shibboleth.sso/SAML/POST',
 	'audience'                 => 'urn:mace:feide:shiblab',
 	'base64attributes'         => FALSE,
 );
 
-$metadata['urn:geant:edugain:component:be:switchaai-test:central'] = array(
-	'AssertionConsumerService' => 'https://edugain-login.switch.ch/ShiBE-R/WebSSOResponseListener',
-	'audience'                 => 'urn:geant:edugain:component:be:switchaai-test:central',
-	'base64attributes'         => FALSE,
-);
-
-$metadata['urn:geant:edugain:component:be:rediris:rediris.es'] = array(
-	'AssertionConsumerService' => 'http://serrano.rediris.es:8080/PAPIWebSSOResponseListener/request',
-	'audience'                 => 'urn:geant:edugain:component:be:rediris:rediris.es',
-	'base64attributes'         => FALSE,
-);
