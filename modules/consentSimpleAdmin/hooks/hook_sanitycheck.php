3c161db528b1a194eb952e34c1978d282b786433
Olav Morken <olav.morken@uninett.no>
2013-12-03 13:22:13 +0000

    consentSimpleAdmin: Use new selftest-function on database backend.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3309 44740490-163a-0410-bde0-09ae8108e29a

--- modules/consentSimpleAdmin/hooks/hook_sanitycheck.php
+++ modules/consentSimpleAdmin/hooks/hook_sanitycheck.php
@@ -13,12 +13,18 @@ function consentSimpleAdmin_hook_sanitycheck(&$hookinfo) {
 	
 		// Parse consent config
 		$consent_storage = sspmod_consent_Store::parseStoreConfig($consentconfig->getValue('store'));
-		
-		// Get all consents for user
-		$stats = $consent_storage->getStatistics();
 
-		$hookinfo['info'][] = '[consentSimpleAdmin] Consent Storage connection OK.';	
-		
+		if (!is_callable(array($consent_storage, 'selftest'))) {
+			/* Doesn't support a selftest. */
+			return;
+		}
+		$testres = $consent_storage->selftest();
+		if ($testres) {
+			$hookinfo['info'][] = '[consentSimpleAdmin] Consent Storage selftest OK.';
+		} else {
+			$hookinfo['errors'][] = '[consentSimpleAdmin] Consent Storage selftest failed.';
+		}
+
 	} catch (Exception $e) {
 		$hookinfo['errors'][] = '[consentSimpleAdmin] Error connecting to storage: ' . $e->getMessage();	
 	}
