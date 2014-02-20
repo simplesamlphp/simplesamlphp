889f53751a56a5782a210fb97c77187f533a5fd4
Jaime Pérez Crespo <jaime.perez@uninett.no>
2014-02-20 20:16:17 +0000

    Revert "Add support for binary attributes in AttributeAddFromLDAP (Issue #620). Also, remove hardcoded attribute names, and detect binary attributes 'automatically'."
    This reverts commit ec82fe0480a1891eff80493e5a95e66984c3ac2a.
    
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3369 44740490-163a-0410-bde0-09ae8108e29a

--- lib/SimpleSAML/Auth/LDAP.php
+++ lib/SimpleSAML/Auth/LDAP.php
@@ -361,25 +361,6 @@ class SimpleSAML_Auth_LDAP {
 			);
 		}
 
-        // parse each entry and process its attributes
-        for ($i = 0; $i < $results['count']; $i++) {
-            $entry = $results[$i];
-
-            // iterate over the attributes of the entry
-            for ($j = 0; $j < $entry['count']; $j++) {
-                $name = $entry[$j];
-                $attribute = $entry[$name];
-
-                // decide whether to base64 encode or not
-                for ($k = 0; $k < $attribute['count']; $k++) {
-                    // base64 encode binary attributes
-                    if (!ctype_print($attribute[$k])) {
-                        $results[$i][$name][$k] = base64_encode($attribute[$k]);
-                    }
-                }
-            }
-        }
-
 		// Remove the count and return
 		unset($results['count']);
 		return $results;
@@ -541,12 +522,12 @@ class SimpleSAML_Auth_LDAP {
 					continue;
 				}
 
-                // base64 encode binary attributes
-                if (!ctype_print($value)) {
+				// Base64 encode jpegPhoto.
+				if (strtolower($name) === 'jpegphoto') {
 					$values[] = base64_encode($value);
-				} else {
+				} else
 					$values[] = $value;
-                }
+
 			}
 
 			// Adding.
