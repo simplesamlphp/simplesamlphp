d3b23ac6f9064b6acf1a6bb15bbdd4af294b7428
Jaime Pérez Crespo <jaime.perez@uninett.no>
2014-02-21 08:02:56 +0000

    Remove example authproc filters from config template.
    git-svn-id: https://simplesamlphp.googlecode.com/svn/trunk@3372 44740490-163a-0410-bde0-09ae8108e29a

--- config-templates/config.php
+++ config-templates/config.php
@@ -496,10 +496,16 @@ $config = array (
 
 		/*
 		 * Generate the 'group' attribute populated from other variables, including eduPersonAffiliation.
-		 */
- 		60 => array('class' => 'core:GenerateGroups', 'eduPersonAffiliation'),
- 		// All users will be members of 'users' and 'members' 	
- 		61 => array('class' => 'core:AttributeAdd', 'groups' => array('users', 'members')),
+ 		60 => array(
+			'class' => 'core:GenerateGroups', 'eduPersonAffiliation'
+		),
+		*/
+ 		/*
+ 		 * All users will be members of 'users' and 'members'
+ 		61 => array(
+ 			'class' => 'core:AttributeAdd', 'groups' => array('users', 'members')
+ 		),
+ 		*/
  		
 		// Adopts language from attribute to use in UI
  		90 => 'core:LanguageAdaptor',
