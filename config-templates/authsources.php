f68210e627c8a97062f412eb0859bc755450a033
Jaime Perez <jaime.perez@uninett.no>
2014-04-07 12:16:39 +0200

    Minor fixes to authsources configuration template.

--- config-templates/authsources.php
+++ config-templates/authsources.php
@@ -49,18 +49,18 @@ $config = array(
 		//'signature.algorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
 
 		/*
-		// The attributes parameter must contain an array of desired attributes by the SP.
-		// The attributes can be expresed as an array of names or as an associative array
-		// in the form of 'friendlyName' => 'name'.
-		// The metadata will then be created as follows:
-		// <md:RequestedAttribute FriendlyName="friendlyName" Name="name" />
-		'attributes' => array(
-                     'attrname' => 'urn:oid:x.x.x.x',
-                 ),
-		 'attributes.required' => array (
-                     'urn:oid:x.x.x.x',
-                 ),
+		 * The attributes parameter must contain an array of desired attributes by the SP.
+		 * The attributes can be expressed as an array of names or as an associative array
+		 * in the form of 'friendlyName' => 'name'.
+		 * The metadata will then be created as follows:
+		 * <md:RequestedAttribute FriendlyName="friendlyName" Name="name" />
 		 */
+		/*'attributes' => array(
+			'attrname' => 'urn:oid:x.x.x.x',
+		),*/
+		/*'attributes.required' => array (
+			'urn:oid:x.x.x.x',
+		),*/
 	),
 
 
