e5be6e220926673109f13489a71f695319024652
Jaime Perez <jaime.perez@uninett.no>
2014-07-31 13:07:10 +0200

    Adding Basque language.

--- config-templates/config.php
+++ config-templates/config.php
@@ -354,7 +354,7 @@ $config = array(
     'language.available' => array(
         'en', 'no', 'nn', 'se', 'da', 'de', 'sv', 'fi', 'es', 'fr', 'it', 'nl', 'lb', 'cs',
         'sl', 'lt', 'hr', 'hu', 'pl', 'pt', 'pt-br', 'tr', 'ja', 'zh', 'zh-tw', 'ru', 'et',
-        'he', 'id', 'sr', 'lv', 'ro'
+        'he', 'id', 'sr', 'lv', 'ro', 'eu'
     ),
     'language.rtl' => array('ar', 'dv', 'fa', 'ur', 'he'),
     'language.default' => 'en',
