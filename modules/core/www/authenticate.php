700125a4f34db4dfe348b0a350c4220ea34a82b4
Jaime Perez Crespo <jaime.perez@uninett.no>
2015-01-19 11:14:34 +0100

    Add NameID to attributes status page.

--- modules/core/www/authenticate.php
+++ modules/core/www/authenticate.php
@@ -51,6 +51,8 @@ $t = new SimpleSAML_XHTML_Template($config, 'status.php', 'attributes');
 
 $t->data['header'] = '{status:header_saml20_sp}';
 $t->data['attributes'] = $attributes;
+// if saml:sp:IdP is set, this is SAML auth so we can pass a NameId
+$t->data['nameid'] = !is_null( $as->getAuthData('saml:sp:IdP') ) ? $as->getAuthData('saml:sp:NameID') : FALSE;
 $t->data['logouturl'] = SimpleSAML_Utilities::selfURLNoQuery() . '?as=' . urlencode($asId) . '&logout';
 $t->show();
 
