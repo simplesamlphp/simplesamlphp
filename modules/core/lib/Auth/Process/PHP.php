9f5462471c7dbbc5f27fdac8130466a64d95f762
Jaime Perez Crespo <jaime.perez@uninett.no>
2015-07-31 13:40:25 +0200

    Reformat the PHP authproc filter. Add a new 'function' option that allows to define an anonymous function. Deprecated the old 'code' function. Add tests.

--- modules/core/lib/Auth/Process/PHP.php
+++ modules/core/lib/Auth/Process/PHP.php
@@ -1,50 +1,70 @@
 <?php
 
+
 /**
  * Attribute filter for running arbitrary PHP code.
  *
  * @package simpleSAMLphp
  */
-class sspmod_core_Auth_Process_PHP extends SimpleSAML_Auth_ProcessingFilter {
+class sspmod_core_Auth_Process_PHP extends SimpleSAML_Auth_ProcessingFilter
+{
 
-	/**
-	 * The PHP code that should be run.
-	 *
-	 * @var string
-	 */
-	private $code;
+    /**
+     * The PHP code that should be run.
+     *
+     * @var string
+     */
+    private $code;
 
-
-	/**
-	 * Initialize this filter, parse configuration
-	 *
-	 * @param array $config  Configuration information about this filter.
-	 * @param mixed $reserved  For future use.
-	 */
-	public function __construct($config, $reserved) {
-		parent::__construct($config, $reserved);
-
-		assert('is_array($config)');
-
-		if (!isset($config['code'])) {
-			throw new SimpleSAML_Error_Exception($this->authId . ': Missing required \'code\' option.');
-		}
-
-		$this->code = (string)$config['code'];
-	}
+    /**
+     * @var callable
+     */
+    private $function = null;
 
 
-	/**
-	 * Apply the PHP code to the attribtes.
-	 *
-	 * @param array &$request  The current request
-	 */
-	public function process(&$request) {
-		assert('is_array($request)');
-		assert('array_key_exists("Attributes", $request)');
+    /**
+     * Initialize this filter, parse configuration
+     *
+     * @param array $config Configuration information about this filter.
+     * @param mixed $reserved For future use.
+     */
+    public function __construct($config, $reserved)
+    {
+        parent::__construct($config, $reserved);
 
-		$function = create_function('&$attributes', $this->code);
-		$function($request['Attributes']);
-	}
+        assert('is_array($config)');
+
+        if (isset($config['function'])) {
+            $this->function = $config['function'];
+        } else { // TODO: remove this branch after removing the 'code' option.
+            if (!isset($config['code'])) {
+                throw new SimpleSAML_Error_Exception("core:PHP: Neither 'function' nor 'code' options defined.");
+            }
+            SimpleSAML_Logger::warning(
+                "Deprecated 'code' configuration option in PHP authentication processing filter."
+            );
+            $this->code = (string) $config['code'];
+        }
+    }
+
+
+    /**
+     * Apply the PHP code to the attributes.
+     *
+     * @param array &$request The current request
+     */
+    public function process(&$request)
+    {
+        assert('is_array($request)');
+        assert('array_key_exists("Attributes", $request)');
+
+        if ($this->function) {
+            $function = $this->function;
+            $function($request['Attributes']);
+        } else { // TODO: remove this branch after removing the 'code' option.
+            $function = create_function('&$attributes', $this->code);
+            $function($request['Attributes']);
+        }
+    }
 
 }
---
+++ tests/modules/core/lib/Auth/Process/PHPTest.php
@@ -0,0 +1,103 @@
+<?php
+
+
+/**
+ * Test for the core:PHP filter.
+ */
+class Test_Core_Auth_Process_PHP extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Helper function to run the filter with a given configuration.
+     *
+     * @param array $config The filter configuration.
+     * @param array $request The request state.
+     *
+     * @return array The state array after processing.
+     */
+    private static function processFilter(array $config, array $request)
+    {
+        $filter = new sspmod_core_Auth_Process_PHP($config, null);
+        @$filter->process($request);
+        return $request;
+    }
+
+
+    /**
+     * Test the configuration of the filter.
+     *
+     * @expectedException SimpleSAML_Error_Exception
+     */
+    public function testInvalidConfiguration()
+    {
+        $config = array();
+        new sspmod_core_Auth_Process_PHP($config, null);
+    }
+
+
+
+    /**
+     * Check that defining a function works as expected.
+     */
+    public function testFunctionDefined()
+    {
+        $config = array(
+            'function' => function (&$attributes) {
+                $attributes['key'] = 'value';
+            },
+        );
+        $request = array('Attributes' => array());
+        $expected = array(
+            'Attributes' => array(
+                'key' => 'value',
+            ),
+        );
+
+        $this->assertEquals($expected, $this->processFilter($config, $request));
+    }
+
+
+    /**
+     * Check that defining the code works as expected.
+     */
+    public function testCodeDefined()
+    {
+        $config = array(
+            'code' => '
+                $attributes["key"] = "value";
+            ',
+        );
+        $request = array('Attributes' => array());
+        $expected = array(
+            'Attributes' => array(
+                'key' => 'value',
+            ),
+        );
+
+        $this->assertEquals($expected, $this->processFilter($config, $request));
+    }
+
+
+    /**
+     * Check that when both the function and code are defined, only the function is executed.
+     */
+    public function testOptionsPrecedence()
+    {
+        $config = array(
+            'function' => function (&$attributes) {
+                $attributes['who'] = 'function';
+            },
+            'code'     => '
+                $attributes["who"] = "code";
+            ',
+        );
+        $request = array('Attributes' => array());
+        $expected = array(
+            'Attributes' => array(
+                'who' => 'function',
+            ),
+        );
+
+        $this->assertEquals($expected, $this->processFilter($config, $request));
+    }
+}
