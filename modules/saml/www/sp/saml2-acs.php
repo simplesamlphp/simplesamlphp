94da2b9922346819f707ebc97b7ea03b97f47f7c
Piotr Belina <piotr.belina@kunstmaan.be>
2015-09-21 13:16:27 +0200

    Revert "also return raw response"
    This reverts commit 9d30323f166c5e323eecc8734574cc31a9f71125.

--- modules/saml/www/sp/saml2-acs.php
+++ modules/saml/www/sp/saml2-acs.php
@@ -174,9 +174,6 @@ foreach ($assertions as $assertion) {
     }
 }
 
-// Add the raw xml response so we can store is as prove
-$attributes['raw_response'] = base64_decode($_POST['SAMLResponse']);
- 
 if (!$foundAuthnStatement) {
     $e = new SimpleSAML_Error_Exception('No AuthnStatement found in assertion(s).');
     SimpleSAML_Auth_State::throwException($state, $e);
