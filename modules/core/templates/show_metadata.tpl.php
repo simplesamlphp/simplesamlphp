21beac9bbbf41c3d149147a65551f95646cf5817
Tyler Antonio <tantonio@ualberta.ca>
2015-10-26 11:15:47 -0600

    Corrected typo on backlink

--- modules/core/templates/show_metadata.tpl.php
+++ modules/core/templates/show_metadata.tpl.php
@@ -7,7 +7,7 @@ $metadata['<?php echo $this->data['m']['metadata-index']; unset($this->data['m']
     echo htmlspecialchars(var_export($this->data['m'], true));
 ?>
 </pre>
-<p>[ <a href="<?php echo $this->data['blacklink']; ?>">back</a> ]</p>
+<p>[ <a href="<?php echo $this->data['backlink']; ?>">back</a> ]</p>
 
 <?php
 $this->includeAtTemplateBase('includes/footer.php'); 
