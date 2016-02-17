f704703e6fda1f91226f56d3dd6ec954b1565b91
Jaime Perez Crespo <jaime.perez@uninett.no>
2016-02-17 15:22:37 +0100

    Reformat the files in bin/.

--- bin/pwgen.php
+++ bin/pwgen.php
@@ -10,38 +10,38 @@
 $baseDir = dirname(dirname(__FILE__));
 
 // Add library autoloader
-require_once($baseDir . '/lib/_autoload.php');
+require_once($baseDir.'/lib/_autoload.php');
 
 
 echo "Enter password: ";
 $password = trim(fgets(STDIN));
 
-if(empty($password)) {
-	echo "Need at least one character for a password\n";
-	exit(1);
+if (empty($password)) {
+    echo "Need at least one character for a password\n";
+    exit(1);
 }
 
 $table = '';
 foreach (array_chunk(hash_algos(), 6) as $chunk) {
-	foreach($chunk as $algo) {
-		$table .= sprintf('%-13s', $algo);
-	}
-	$table .= "\n";
+    foreach ($chunk as $algo) {
+        $table .= sprintf('%-13s', $algo);
+    }
+    $table .= "\n";
 }
 
-echo "The following hashing algorithms are available:\n" . $table . "\n";
+echo "The following hashing algorithms are available:\n".$table."\n";
 echo "Which one do you want? [sha256] ";
 $algo = trim(fgets(STDIN));
-if(empty($algo)) {
-	$algo = 'sha256';
+if (empty($algo)) {
+    $algo = 'sha256';
 }
 
-if(!in_array(strtolower($algo), hash_algos())) {
-	echo "Hashing algorithm '$algo' is not supported\n";
-	exit(1);
+if (!in_array(strtolower($algo), hash_algos())) {
+    echo "Hashing algorithm '$algo' is not supported\n";
+    exit(1);
 }
 
 echo "Do you want to use a salt? (yes/no) [yes] ";
 $s = (trim(fgets(STDIN)) == 'no') ? '' : 'S';
 
-echo "\n  " . SimpleSAML\Utils\Crypto::pwHash($password, strtoupper( $s . $algo ) ). "\n\n";
+echo "\n  ".SimpleSAML\Utils\Crypto::pwHash($password, strtoupper($s.$algo))."\n\n";
