59e9cb381bf5bc142e465703187b7f6de31b4a91
Jaime Perez Crespo <jaime.perez@uninett.no>
2016-04-19 20:09:13 +0200

    Add some tests for the new SimpleSAML\Utils\HTTP::guessBasePath().

--- tests/lib/SimpleSAML/Utils/HTTPTest.php
+++ tests/lib/SimpleSAML/Utils/HTTPTest.php
@@ -5,6 +5,53 @@ use SimpleSAML\Utils\HTTP;
 
 class HTTPTest extends \PHPUnit_Framework_TestCase
 {
+
+
+    /**
+     * Test SimpleSAML\Utils\HTTP::guessBasePath().
+     */
+    public function testGuessBasePath()
+    {
+        global $_SERVER;
+
+        $original = $_SERVER;
+
+        $_SERVER['REQUEST_URI'] = '/simplesaml/module.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/simplesaml/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/simplesaml/module.php/some/path/to/other/script.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/simplesaml/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/module.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/module.php/some/path/to/other/script.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/some/path/module.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/some/path/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/some/path/module.php/some/path/to/other/script.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/module.php';
+        $this->assertEquals('/some/path/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/some/dir/in/www/script.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/some/dir/in/www/script.php';
+        $this->assertEquals('/', HTTP::guessBasePath());
+
+        $_SERVER['REQUEST_URI'] = '/simplesaml/some/dir/in/www/script.php';
+        $_SERVER['SCRIPT_FILENAME'] = '/some/path/simplesamlphp/www/some/dir/in/www/script.php';
+        $this->assertEquals('/simplesaml/', HTTP::guessBasePath());
+
+        $_SERVER = $original;
+    }
+
+
     /**
      * Test SimpleSAML\Utils\HTTP::getSelfHost() with and without custom port.
      */
