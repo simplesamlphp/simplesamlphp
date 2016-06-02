f37b09ae0959bbc2e6a69bf4da79f72a4990ad4a
Jaime Perez Crespo <jaime.perez@uninett.no>
2016-06-02 10:46:39 +0200

    If we have an exception registered in the state array while authenticating, we should throw that exception instead of just printing it as plain text and exit. This resolves #401.

--- modules/core/www/authenticate.php
+++ modules/core/www/authenticate.php
@@ -27,12 +27,7 @@ if (array_key_exists(SimpleSAML_Auth_State::EXCEPTION_PARAM, $_REQUEST)) {
 	assert('array_key_exists(SimpleSAML_Auth_State::EXCEPTION_DATA, $state)');
 	$e = $state[SimpleSAML_Auth_State::EXCEPTION_DATA];
 
-	header('Content-Type: text/plain');
-	echo "Exception during login:\n";
-	foreach ($e->format() as $line) {
-		echo $line . "\n";
-	}
-	exit(0);
+	throw $e;
 }
 
 
