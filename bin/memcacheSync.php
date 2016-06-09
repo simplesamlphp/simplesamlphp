de972b674829d24ee6767d2b8fe0be99e686ef0d
Thijs Kinkhorst <thijs@kinkhorst.com>
2016-06-09 08:32:16 +0000

    Update memcacheSync advice to current way of enabling the module.
    Based on patch by Nishanth Aravamudan <nish.aravamudan@canonical.com>

--- bin/memcacheSync.php
+++ bin/memcacheSync.php
@@ -9,9 +9,9 @@ if (!class_exists('Memcache')) {
     echo("This is most likely because PHP doesn't load it for the command line\n");
     echo("version. You probably need to enable it somehow.\n");
     echo("\n");
-    if (is_dir('/etc/php5/cli/conf.d')) {
+    if(is_executable('/usr/sbin/phpenmod')) {
         echo("It is possible that running the following command as root will fix it:\n");
-        echo(" echo 'extension=memcache.so' >/etc/php5/cli/conf.d/memcache.ini\n");
+        echo(" phpenmod -s cli memcache\n");
     }
 
     exit(1);
