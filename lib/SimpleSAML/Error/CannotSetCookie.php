d65798163588971d025c014559814dc2bf110256
Jaime Pérez <jaime.perez@uninett.no>
2016-07-02 17:15:21 +0200

    Add a new SimpleSAML\Error\CannotSetCookie exception.

---
+++ lib/SimpleSAML/Error/CannotSetCookie.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Exception to indicate that we cannot set a cookie.
+ *
+ * @author Jaime Pérez Crespo <jaime.perez@uninett.no>
+ * @package SimpleSAMLphp
+ */
+
+namespace SimpleSAML\Error;
+
+
+class CannotSetCookie extends \SimpleSAML_Error_Exception
+{
+
+}
