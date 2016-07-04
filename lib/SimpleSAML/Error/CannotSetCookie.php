0c660fda14af1111075d9f49b9448d5665b37143
Jaime Pérez <jaime.perez@uninett.no>
2016-07-04 13:54:25 +0200

    Add error codes to the SimpleSAML\Error\CannotSetCookie exception.
    This way we can discern why we couldn't set a cookie, and act accordingly.

--- lib/SimpleSAML/Error/CannotSetCookie.php
+++ lib/SimpleSAML/Error/CannotSetCookie.php
@@ -12,4 +12,25 @@ namespace SimpleSAML\Error;
 class CannotSetCookie extends \SimpleSAML_Error_Exception
 {
 
+    /**
+     * The exception was thrown for unknown reasons.
+     *
+     * @var int
+     */
+    const UNKNOWN = 0;
+
+    /**
+     * The exception was due to the HTTP headers being already sent, and therefore we cannot send additional headers to
+     * set the cookie.
+     *
+     * @var int
+     */
+    const HEADERS_SENT = 1;
+
+    /**
+     * The exception was due to trying to set a secure cookie over an insecure channel.
+     *
+     * @var int
+     */
+    const SECURE_COOKIE = 2;
 }
