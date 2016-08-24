d7733d61a73808eca53d0ae33bac0e50267c25fa
tauceti2 <michalp@ics.muni.cz>
2016-08-24 12:39:23 +0200

    Configurable LinkedIn attributes
    - Attributes which can be get from LinkedIn are now configurable
    - By default these attributes are requested: id,first-name,last-name,headline,summary,specialties,picture-url,email-address
    - Added email-address attribute into the default set
    - Fixed link to the documentation

--- config-templates/authsources.php
+++ config-templates/authsources.php
@@ -212,10 +212,13 @@ $config = array(
     // LinkedIn OAuth Authentication API.
     // Register your application to get an API key here:
     //  https://www.linkedin.com/secure/developer
+    // Attributes definition:
+    //  https://developer.linkedin.com/docs/fields
     'linkedin' => array(
         'authlinkedin:LinkedIn',
         'key' => 'xxxxxxxxxxxxxxxx',
         'secret' => 'xxxxxxxxxxxxxxxx',
+        'attributes' => 'id,first-name,last-name,headline,summary,specialties,picture-url,email-address',
     ),
     */
 
