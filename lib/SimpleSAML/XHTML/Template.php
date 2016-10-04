76d084e04532b25f0813e3110c07feaefc346452
Jaime Pérez <jaime.perez@uninett.no>
2016-10-04 16:41:35 +0200

    Define SimpleSAML\XHTML\Template::$twig as a property (previously only set in the constructor) and make sure we use proper type checking when using it.

--- lib/SimpleSAML/XHTML/Template.php
+++ lib/SimpleSAML/XHTML/Template.php
@@ -52,6 +52,13 @@ class SimpleSAML_XHTML_Template
     private $template = 'default.php';
 
     /**
+     * The twig environment.
+     *
+     * @var false|Twig_Environment
+     */
+    private $twig;
+
+    /**
      * The template name.
      *
      * @var string
@@ -155,7 +162,7 @@ class SimpleSAML_XHTML_Template
         $loader = $this->setupTwigTemplatepaths();
         // abort if twig template does not exist
         if (!$loader->exists($this->twig_template)) {
-            return null;
+            return false;
         }
 
         $twig = new \Twig_Environment($loader,
@@ -317,7 +324,7 @@ class SimpleSAML_XHTML_Template
      */
     public function show()
     {
-        if ($this->twig) {
+        if ($this->twig !== false) {
             $this->twigDefaultContext();
             echo $this->twig->render($this->twig_template, $this->data);
         } else {
