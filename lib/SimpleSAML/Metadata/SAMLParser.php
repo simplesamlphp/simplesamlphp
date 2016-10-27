596f03a5737187f0369b4127bb8ee66db20554f1
Andrius <andrius.kulbis@ktu.lt>
2016-10-27 11:23:35 +0300

    Fix required attributes duplication
    While parsing the XML metadata file of SP, attributes in AttributeConsumerService marked as required where added multiple time causing duplication of attribute keys in attributes.required section.

--- lib/SimpleSAML/Metadata/SAMLParser.php
+++ lib/SimpleSAML/Metadata/SAMLParser.php
@@ -1175,10 +1175,6 @@ class SimpleSAML_Metadata_SAMLParser
             $attrname = $child->Name;
             $sp['attributes'][] = $attrname;
 
-            if ($child->isRequired) {
-                $sp['attributes.required'][] = $attrname;
-            }
-
             if ($child->isRequired !== null && $child->isRequired === true) {
                 $sp['attributes.required'][] = $attrname;
             }
