961a6c766ec1da573a97fff0cde07532961e8264
Tim van Dijen <tvdijen@gmail.com>
2017-01-29 13:04:28 +0100

    Raise exception when memcache-server is down
    The exception was never raised because $stats will be array(false) on failure instead of just false.

--- lib/SimpleSAML/Memcache.php
+++ lib/SimpleSAML/Memcache.php
@@ -439,8 +439,10 @@ class SimpleSAML_Memcache
 
         foreach (self::getMemcacheServers() as $sg) {
             $stats = $sg->getExtendedStats();
-            if ($stats === false) {
-                throw new Exception('Failed to get memcache server status.');
+            foreach ($stats as $server => $data) {
+                if ($data === false) {
+                    throw new Exception('Failed to get memcache server status.');
+                }
             }
 
             $stats = SimpleSAML\Utils\Arrays::transpose($stats);
