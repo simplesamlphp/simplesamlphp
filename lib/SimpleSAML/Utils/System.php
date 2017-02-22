1a12f00cba5838af749818e3231f29be742abe01
Jaime Pérez Crespo <jaime.perez@uninett.no>
2017-02-22 14:55:56 +0100

    bugfix: Looking for “WIN” inside “Darwin” will give positive results.
    To put it differently, to avoid OSX machines being identified as windows boxes, “darwin” should be evaluated *before* “win”.

--- lib/SimpleSAML/Utils/System.php
+++ lib/SimpleSAML/Utils/System.php
@@ -31,11 +31,11 @@ class System
         if (stristr(PHP_OS, 'LINUX')) {
             return self::LINUX;
         }
-        if (stristr(PHP_OS, 'WIN')) {
-            return self::WINDOWS;
-        }
         if (stristr(PHP_OS, 'DARWIN')) {
             return self::OSX;
+        }
+        if (stristr(PHP_OS, 'WIN')) {
+            return self::WINDOWS;
         }
         if (stristr(PHP_OS, 'BSD')) {
             return self::BSD;
