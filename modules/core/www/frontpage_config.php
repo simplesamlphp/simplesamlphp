fe834bcdf200d3642b4953b8e511b3211d768f1e
Jaime Pérez Crespo <jaime.perez@uninett.no>
2017-03-30 10:24:24 +0200

    Add a check for predis/predis in the config page.

--- modules/core/www/frontpage_config.php
+++ modules/core/www/frontpage_config.php
@@ -129,6 +129,11 @@ foreach ($functionchecks AS $func => $descr) {
 	$funcmatrix[] = array('descr' => $descr[1], 'required' => $descr[0], 'enabled' => function_exists($func));
 }
 
+$funcmatrix[] = array(
+    'required' => 'optional',
+    'descr' => 'predis/predis (required if the redis data store is used)',
+    'enabled' => class_exists('\Predis\Client'),
+);
 
 /* Some basic configuration checks */
 
