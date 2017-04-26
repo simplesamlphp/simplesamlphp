cbb14a6151fe2229ef66b6e3d7c5d2804a48eea9
Jaime Pérez Crespo <jaime.perez@uninett.no>
2017-04-26 10:47:46 +0200

    Add a check for mbstring in the config frontpage.

--- modules/core/www/frontpage_config.php
+++ modules/core/www/frontpage_config.php
@@ -87,6 +87,7 @@ $functionchecks = array(
 	'preg_match'       => array('required',  'RegEx support'),
 	'json_decode'      => array('required', 'JSON support'),
 	'class_implements' => array('required', 'Standard PHP Library (SPL)'),
+	'mb_strlen'        => array('required', 'Multibyte String Extension'),
 	'curl_init'        => array('optional', 'cURL (required if automatic version checks are used, also by some modules.'),
 	'mcrypt_module_open'=> array('optional',  'MCrypt (required if digital signatures or encryption are used)'),
 	'session_start'  => array('optional', 'Session Extension (required if PHP sessions are used)'),
