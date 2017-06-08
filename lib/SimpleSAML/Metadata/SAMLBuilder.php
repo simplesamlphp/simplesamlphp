15e68a86230004f674033ec74ceee649ff89c2a2
gyufi <gyufi@sztaki.hu>
2017-06-08 16:50:21 +0200

    SAMLBuilder AttributeAuthorityDescriptor prevent from empty protocolSupportEnumeration.

--- lib/SimpleSAML/Metadata/SAMLBuilder.php
+++ lib/SimpleSAML/Metadata/SAMLBuilder.php
@@ -646,7 +646,7 @@ class SimpleSAML_Metadata_SAMLBuilder
         $metadata = SimpleSAML_Configuration::loadFromArray($metadata, $metadata['entityid']);
 
         $e = new \SAML2\XML\md\AttributeAuthorityDescriptor();
-        $e->protocolSupportEnumeration = $metadata->getArray('protocols', array());
+        $e->protocolSupportEnumeration = $metadata->getArray('protocols', array('urn:oasis:names:tc:SAML:2.0:protocol'));
 
         $this->addExtensions($metadata, $e);
         $this->addCertificate($e, $metadata);
