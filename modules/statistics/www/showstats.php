95965cdafdcd532e7e6d76367fb70b5081e25f77
Tim van Dijen <tvdijen@gmail.com>
2017-07-29 00:43:14 +0200

    PSR-2 compliancy

--- modules/statistics/www/showstats.php
+++ modules/statistics/www/showstats.php
@@ -4,25 +4,36 @@ $config = SimpleSAML_Configuration::getInstance();
 $statconfig = SimpleSAML_Configuration::getConfig('module_statistics.php');
 $session = SimpleSAML_Session::getSessionFromRequest();
 
-
 sspmod_statistics_AccessCheck::checkAccess($statconfig);
-
 
 /*
  * Check input parameters
  */
-$preferRule = NULL;
-$preferRule2 = NULL;
-$preferTime = NULL;
-$preferTimeRes = NULL;
-$delimiter = NULL;
-if(array_key_exists('rule', $_REQUEST)) $preferRule = $_REQUEST['rule'];
-if(array_key_exists('rule2', $_REQUEST)) $preferRule2 = $_REQUEST['rule2'];
-if(array_key_exists('time', $_REQUEST)) $preferTime = $_REQUEST['time'];
-if(array_key_exists('res', $_REQUEST)) $preferTimeRes = $_REQUEST['res'];
-if(array_key_exists('d', $_REQUEST)) $delimiter = $_REQUEST['d'];
+$preferRule = null;
+$preferRule2 = null;
+$preferTime = null;
+$preferTimeRes = null;
+$delimiter = null;
 
-if ($preferRule2 === '_') $preferRule2 = NULL;
+if (array_key_exists('rule', $_REQUEST)) {
+    $preferRule = $_REQUEST['rule'];
+}
+if (array_key_exists('rule2', $_REQUEST)) {
+    $preferRule2 = $_REQUEST['rule2'];
+}
+if (array_key_exists('time', $_REQUEST)) {
+    $preferTime = $_REQUEST['time'];
+}
+if (array_key_exists('res', $_REQUEST)) {
+    $preferTimeRes = $_REQUEST['res'];
+}
+if (array_key_exists('d', $_REQUEST)) {
+    $delimiter = $_REQUEST['d'];
+}
+
+if ($preferRule2 === '_') {
+    $preferRule2 = null;
+}
 
 /*
  * Create statistics data.
@@ -45,9 +56,6 @@ $timeNavigation = $statrule->getTimeNavigation($timeres, $preferTime);
 $dataset->aggregateSummary();
 $dataset->calculateMax();
 
-
-
-
 $piedata = $dataset->getPieData();
 
 $datasets = array();
@@ -59,49 +67,38 @@ $maxes = array();
 
 $maxes[] = $dataset->getMax();
 
-
 if (isset($preferRule2)) {
-	$statrule = $ruleset->getRule($preferRule2);
-	$dataset2 = $statrule->getDataset($preferTimeRes, $preferTime);
-	$dataset2->aggregateSummary();
-	$dataset2->calculateMax();
-	
-	$datasets[] = $dataset2->getPercentValues();
-	$maxes[] = $dataset2->getMax();
+    $statrule = $ruleset->getRule($preferRule2);
+    $dataset2 = $statrule->getDataset($preferTimeRes, $preferTime);
+    $dataset2->aggregateSummary();
+    $dataset2->calculateMax();
+
+    $datasets[] = $dataset2->getPercentValues();
+    $maxes[] = $dataset2->getMax();
 }
-
-
-
-
-
-
-
 
 $dimx = $statconfig->getValue('dimension.x', 800);
 $dimy = $statconfig->getValue('dimension.y', 350);
 $grapher = new sspmod_statistics_Graph_GoogleCharts($dimx, $dimy);
 
 if (array_key_exists('output', $_REQUEST) && $_REQUEST['output'] === 'csv') {
-
-	header('Content-type: text/csv');
-	header('Content-Disposition: attachment; filename="simplesamlphp-data.csv"');
-	$data = $dataset->getDebugData();
-	foreach($data AS $de) {
-		if (isset($de[1])) {
-			echo('"' . $de[0] . '",' . $de[1] . "\n");
-		}
-	}
-	exit;
+    header('Content-type: text/csv');
+    header('Content-Disposition: attachment; filename="simplesamlphp-data.csv"');
+    $data = $dataset->getDebugData();
+    foreach ($data as $de) {
+        if (isset($de[1])) {
+            echo '"' . $de[0] . '",' . $de[1] . "\n";
+        }
+    }
+    exit;
 }
-
-
 
 $t = new SimpleSAML_XHTML_Template($config, 'statistics:statistics.tpl.php');
 $t->data['pageid'] = 'statistics';
 $t->data['header'] = 'stat';
 $t->data['imgurl'] = $grapher->show($axis['axis'], $axis['axispos'], $datasets, $maxes);
 if (isset($piedata)) {
-	$t->data['pieimgurl'] = $grapher->showPie( $dataset->getDelimiterPresentationPie(), $piedata);
+    $t->data['pieimgurl'] = $grapher->showPie( $dataset->getDelimiterPresentationPie(), $piedata);
 }
 $t->data['available.rules'] = $ruleset->availableRulesNames();
 $t->data['available.times'] = $statrule->availableFileSlots($timeres);
@@ -123,4 +120,3 @@ $t->data['availdelimiters'] = $dataset->availDelimiters();
 
 $t->data['delimiterPresentation'] =  $dataset->getDelimiterPresentation();
 $t->show();
-
