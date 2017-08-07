36768213a016588d3cd1393c956bac2e913ef333
Jaime Pérez Crespo <jaime.perez@uninett.no>
2017-08-07 13:23:10 +0200

    bugfix: Fix test for method made protected.

--- tests/modules/ldap/lib/Auth/Process/BaseFilterTest.php
+++ tests/modules/ldap/lib/Auth/Process/BaseFilterTest.php
@@ -7,14 +7,17 @@ class sspmod_ldap_Auth_Process_BaseFilter_Test extends PHPUnit_Framework_TestCas
         $stub = $this->getMockBuilder('sspmod_ldap_Auth_Process_BaseFilter')
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
+        $class = new \ReflectionClass($stub);
+        $method = $class->getMethod('var_export');
+        $method->setAccessible(true);
 
         $this->assertEquals(
             "array ( 'ldap.hostname' => 'ldap://172.17.101.32', 'ldap.port' => 389, 'ldap.password' => '********', )",
-            $stub->var_export(array(
+            $method->invokeArgs($stub, array(array(
                 'ldap.hostname' => 'ldap://172.17.101.32',
                 'ldap.port' => 389,
                 'ldap.password' => 'password',
-            ))
+            )))
         );
     }
 }
