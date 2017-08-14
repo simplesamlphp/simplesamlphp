f3596f022ed7dd31f5f00406d472e1a7da8b35d5
A.J. de Mooij <A.J.deMooij@uu.nl>
2017-08-14 11:27:02 +0200

    Removed need for Utils/HTTP.php entirely

--- templates/post.php
+++ templates/post.php
@@ -1,16 +1,12 @@
-<?php
-use SimpleSAML\Utils\HTTP;
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>POST data</title>
-    <script type="text/javascript" src="<?php echo HTTP::getBaseURL();?>/resources/post.js"></script>
+    <script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/post.js"></script>
     <link 
-        type="text/css" rel="stylesheet" href="<?php echo HTTP::getBaseURL();?>/resources/post.css" />
+        type="text/css" rel="stylesheet" href="/<?php echo $this->data['baseurlpath']; ?>resources/post.css" />
 </head>
 <body>
 
