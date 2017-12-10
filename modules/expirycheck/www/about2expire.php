547058c5307c5e48d8d93ea1ab3434a32f3cedb1
Tim van Dijen <tvdijen@gmail.com>
2017-12-10 23:00:57 +0100

    expirycheck: Add Twig templates

--- modules/expirycheck/www/about2expire.php
+++ modules/expirycheck/www/about2expire.php
@@ -9,22 +9,61 @@
 SimpleSAML\Logger::info('expirycheck - User has been warned that NetID is near to expirational date.');
 
 if (!array_key_exists('StateId', $_REQUEST)) {
-	throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
+    throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
 }
 $id = $_REQUEST['StateId'];
 $state = SimpleSAML_Auth_State::loadState($id, 'expirywarning:about2expire');
 
 if (array_key_exists('yes', $_REQUEST)) {
-	// The user has pressed the yes-button
-	SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
+    // The user has pressed the yes-button
+    SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
 }
 
 $globalConfig = SimpleSAML_Configuration::getInstance();
 
+$daysleft = $state['daysleft'];
+
 $t = new SimpleSAML_XHTML_Template($globalConfig, 'expirycheck:about2expire.php');
+$t->data['autofocus'] = 'yesbutton';
 $t->data['yesTarget'] = SimpleSAML\Module::getModuleURL('expirycheck/about2expire.php');
 $t->data['yesData'] = array('StateId' => $id);
-$t->data['daysleft'] = $state['daysleft'];
+$t->data['warning'] = $warning;
 $t->data['expireOnDate'] = $state['expireOnDate'];
 $t->data['netId'] = $state['netId'];
+
+if ($daysleft == 0) {
+    # netid will expire today
+    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header_today}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId'])
+                        ));
+    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning_today}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId'])
+                        ));
+} elseif ($daysleft == 1) {
+    # netid will expire in one day
+
+    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId']),
+                                '%DAYS%' => $this->t('{expirycheck:expwarning:day}'),
+                                '%DAYSLEFT%' => htmlspecialchars($daysleft),
+                        ));
+    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId']),
+                                '%DAYS%' => $this->t('{expirycheck:expwarning:day}'),
+                                '%DAYSLEFT%' => htmlspecialchars($daysleft),
+                        ));
+} else {
+    # netid will expire in next <daysleft> days
+    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId']),
+                                '%DAYS%' => $this->t('{expirycheck:expwarning:days}'),
+                                '%DAYSLEFT%' => htmlspecialchars($daysleft),
+                        ));
+    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning}', array(
+                                '%NETID%' => htmlspecialchars($this->data['netId']),
+                                '%DAYS%' => $this->t('{expirycheck:expwarning:days}'),
+                                '%DAYSLEFT%' => htmlspecialchars($daysleft),
+                        ));
+}
+
 $t->show();
--- modules/expirycheck/www/expired.php
+++ modules/expirycheck/www/expired.php
@@ -9,13 +9,14 @@
 SimpleSAML\Logger::info('expirycheck - User has been warned that NetID is near to expirational date.');
 
 if (!array_key_exists('StateId', $_REQUEST)) {
-	throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
+    throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
 }
 $state = SimpleSAML_Auth_State::loadState($_REQUEST['StateId'], 'expirywarning:expired');
 
 $globalConfig = SimpleSAML_Configuration::getInstance();
 
 $t = new SimpleSAML_XHTML_Template($globalConfig, 'expirycheck:expired.php');
-$t->data['expireOnDate'] = $state['expireOnDate'];
-$t->data['netId'] = $state['netId'];
+$t->data['header'] = $this->t('{expirycheck:expwarning:access_denied}');
+$t->data['expireOnDate'] = htmlspecialchars($state['expireOnDate']);
+$t->data['netId'] = htmlspecialchars($state['netId']);
 $t->show();
