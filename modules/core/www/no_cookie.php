d019b4c7fed2f4a5586b5f83365b1e9f9121a343
Tim van Dijen <tvdijen@gmail.com>
2017-12-12 22:23:23 +0100

    core: Add more Twig templates

--- modules/core/www/no_cookie.php
+++ modules/core/www/no_cookie.php
@@ -9,5 +9,9 @@ if (isset($_REQUEST['retryURL'])) {
 
 $globalConfig = SimpleSAML_Configuration::getInstance();
 $t = new SimpleSAML_XHTML_Template($globalConfig, 'core:no_cookie.tpl.php');
+
+$t->data['header'] = htmlspecialchars($t->t('{core:no_cookie:header}'));
+$t->data['description'] = htmlspecialchars($t->t('{core:no_cookie:description}'));
+$t->data['retry'] = htmlspecialchars($t->t('{core:no_cookie:retry}'));
 $t->data['retryURL'] = $retryURL;
 $t->show();
