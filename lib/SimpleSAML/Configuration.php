c4752c3efe110512934ba099f65962f98d34f902
Tim van Dijen <tvdijen@gmail.com>
2018-01-06 23:22:42 +0100

    Fix mistake

--- lib/SimpleSAML/Configuration.php
+++ lib/SimpleSAML/Configuration.php
@@ -93,11 +93,6 @@ class SimpleSAML_Configuration implements \SimpleSAML\Utils\ClearableState
         $this->location = $location;
     }
 
-    public function getLocation()
-    {
-        return $this->location;
-    }
-
     /**
      * Load the given configuration file.
      *
