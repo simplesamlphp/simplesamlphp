1cba48676420d7889ba8223d4f2ea1b9867c0184
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-02-27 10:17:30 +0100

    Releasing SimpleSAMLphp 1.15.3

--- lib/SimpleSAML/Configuration.php
+++ lib/SimpleSAML/Configuration.php
@@ -386,7 +386,7 @@ class SimpleSAML_Configuration implements \SimpleSAML\Utils\ClearableState
      */
     public function getVersion()
     {
-        return '1.15.2';
+        return '1.15.3';
     }
 
 
