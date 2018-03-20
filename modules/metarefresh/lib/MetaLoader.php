1deb0d21a67ca9a827cc81c7d36c82ac5a2541fb
Tim van Dijen <tvdijen@gmail.com>
2018-03-20 22:34:21 +0100

    Minor improvement

--- modules/metarefresh/lib/MetaLoader.php
+++ modules/metarefresh/lib/MetaLoader.php
@@ -227,10 +227,10 @@ class sspmod_metarefresh_MetaLoader
     {
 		if (isset($source['conditionalGET']) && $source['conditionalGET']) {
 			// Headers section
-			$candidates = array('last-modified', 'etag');
+            if ($responseHeaders !== null) {
+			    $candidates = array('last-modified', 'etag');
 
-			foreach ($candidates as $candidate) {
-                if($responseHeaders !== null) {
+			    foreach ($candidates as $candidate) {
 				    if (array_key_exists($candidate, $responseHeaders)) {
 					    $this->state[$source['src']][$candidate] = $responseHeaders[$candidate];
 				    }
