602c8b4b94b50096e610270eabadcaa35c7ccc1d
Emmanuel Dreyfus <manu@netbsd.org>
2018-05-28 16:22:03 +0200

    Typo

--- lib/SimpleSAML/Metadata/MetaDataStorageHandlerPdo.php
+++ lib/SimpleSAML/Metadata/MetaDataStorageHandlerPdo.php
@@ -168,7 +168,7 @@ class SimpleSAML_Metadata_MetaDataStorageHandlerPdo extends SimpleSAML_Metadata_
 
             while ($d = $stmt->fetch()) {
                 if (++$rowCount > 1) {
-                    SimpleSAML\Logger::warning("Dulicate match for $entityId in set $set");
+                    SimpleSAML\Logger::warning("Duplicate match for $entityId in set $set");
                     break;
                 }
                 $data = json_decode($d['entity_data'], true);
