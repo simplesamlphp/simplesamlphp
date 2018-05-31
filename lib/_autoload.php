9c428a56f48ac217475d7c4a17d8c43602ec773d
Tim van Dijen <tvdijen@gmail.com>
2018-05-31 19:19:26 +0200

    Fix syntax error

--- lib/_autoload.php
+++ lib/_autoload.php
@@ -9,11 +9,11 @@
  */
 
 // SSP is loaded as a separate project
-$libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/vendor/autoload.php'));
+$libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/vendor/autoload.php');
 if (file_exists($libpath) {
     require_once $libpath;
 } else {  // SSP is loaded as a library
-    $libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/../../autoload.php'));
+    $libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/../../autoload.php');
     if (file_exists($libpath) {
         require_once $libpath;
     } else {
