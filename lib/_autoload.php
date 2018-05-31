32a6be8531cecf86be0de17cf5e2659c3547cbb3
Tim van Dijen <tvdijen@gmail.com>
2018-05-31 20:15:28 +0200

    Another syntax fix
    Murphy hit me in the face

--- lib/_autoload.php
+++ lib/_autoload.php
@@ -10,12 +10,12 @@
 
 // SSP is loaded as a separate project
 $libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/vendor/autoload.php');
-if (file_exists($libpath) {
-    require_once $libpath;
+if (file_exists($libpath)) {
+    require_once($libpath);
 } else {  // SSP is loaded as a library
     $libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/../../autoload.php');
-    if (file_exists($libpath) {
-        require_once $libpath;
+    if (file_exists($libpath)) {
+        require_once($libpath);
     } else {
         throw new Exception('Unable to load Composer autoloader');
     }
