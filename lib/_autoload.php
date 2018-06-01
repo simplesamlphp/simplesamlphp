1cab74e41576aef74fc8a95ee30f453d02813fb9
Tim van Dijen <tvdijen@gmail.com>
2018-06-01 14:46:40 +0200

    Fix autoloader
    Can't use namespaced classes at this point yet

--- lib/_autoload.php
+++ lib/_autoload.php
@@ -8,15 +8,15 @@
  * @package SimpleSAMLphp
  */
 
-// SSP is loaded as a separate project
-$libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/vendor/autoload.php');
-if (file_exists($libpath)) {
-    require_once($libpath);
-} else {  // SSP is loaded as a library
-    $libpath = \SimpleSAML\Utils\System::resolvePath(dirname(dirname(__FILE__)).'/../../autoload.php');
-    if (file_exists($libpath)) {
-        require_once($libpath);
-    } else {
-        throw new Exception('Unable to load Composer autoloader');
-    }
+if (file_exists(dirname(dirname(__FILE__)).'/vendor/autoload.php')) {
+    // SSP is loaded as a separate project
+    require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+} else if (file_exists(dirname(dirname(__FILE__)).'/../../autoload.php')) {
+    // SSP is loaded as a library
+    require_once dirname(dirname(__FILE__)).'/../../autoload.php';
+} else if (file_exists('/../../autoload.php')) {
+    // Windows version
+    require_once '/../../autoload.php';
+} else {
+    throw new Exception('Unable to load Composer autoloader');
 }
