1fcc2c2203f5c8cfa1e700a9f970111630a65c3e
Tim van Dijen <tvdijen@gmail.com>
2018-07-28 20:01:54 +0200

    Fix typo

--- modules/portal/config-templates/module_portal.php
+++ modules/portal/config-templates/module_portal.php
@@ -7,7 +7,7 @@ $config = array (
 
 	'pagesets' => array(
 		array('frontpage_welcome', 'frontpage_config', 'frontpage_auth', 'frontpage_federation'),
-		array('santitycheck', 'statistics'),
+		array('sanitycheck', 'statistics'),
 	),
 	
 );
