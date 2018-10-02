f3e2e491cd7a2da9445b81c4637209bb9d6de711
Guy Halse <guy@tenet.ac.za>
2018-10-02 17:12:32 +0200

    Use array_shift rather than array_values

--- modules/saml/lib/IdP/SAML2.php
+++ modules/saml/lib/IdP/SAML2.php
@@ -986,9 +986,9 @@ class SAML2
 
         if ($nameIdFormat === null || !isset($state['saml:NameID'][$nameIdFormat])) {
             // either not set in request, or not set to a format we supply. Fall back to old generation method
-            $nameIdFormat = array_values($spMetadata->getArrayizeString('NameIDFormat', null))[0];
+            $nameIdFormat = array_shift($spMetadata->getArrayizeString('NameIDFormat', null));
             if ($nameIdFormat === null) {
-                $nameIdFormat = array_values($idpMetadata->getArrayizeString('NameIDFormat', \SAML2\Constants::NAMEID_TRANSIENT))[0];
+                $nameIdFormat = array_shift($idpMetadata->getArrayizeString('NameIDFormat', \SAML2\Constants::NAMEID_TRANSIENT));
             }
         }
 
