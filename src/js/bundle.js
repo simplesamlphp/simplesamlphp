a9e91370dac6541439d1678f9c21819d87986a09
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-11-28 15:58:17 +0100

    Add a webpack configuration to build assets.
    This builds and minifies CSS and a common JS bundle, as well as copy the relevant fonts. In order to build, we now need to run "npm run build" after installing the dependencies with "npm install".

---
+++ src/js/bundle.js
@@ -0,0 +1,19 @@
+import "es6-shim";
+import "clipboard/dist/clipboard";
+import "selectize/dist/js/selectize";
+
+$(document).ready(function () {
+    // get available languages
+    let languages = $.map($('#language-selector option'), function (option) {
+       return option.text.toLowerCase();
+    });
+
+    // initialize selectize
+    $('#language-selector').selectize({
+        onChange: function () {
+            if (-1 !== $.inArray($('#language-selector-selectized').prev().text().toLowerCase(), languages)) {
+                $('#language-form').submit();
+            }
+        },
+    });
+});
