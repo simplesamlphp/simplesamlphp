3a63ee425b25a6ae73ef944047f71fbbde339930
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-11-29 11:30:14 +0100

    Allow setting the main background colors during build.
    We have some defaults, but now we can also pass the colors to the build script like this:
    
    npx webpack -p --env.primaryBackground=#XXX --env.transitionBackground=#YYY --env.secondaryBackground=#ZZZi

--- webpack.config.js
+++ webpack.config.js
@@ -10,66 +10,75 @@ const localConfig = {
     js_filename: 'js/[name].js'
 };
 
-module.exports = {
-    entry: {
-        bundle: './src/js/bundle',
-        stylesheet: './src/js/style'
-    },
-    output: {
-        path: path.resolve(buildDir),
-        filename: localConfig['js_filename']
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /\/node_modules\//,
-                use: {
-                    loader: 'babel-loader'
+module.exports = environment => {
+    const env = typeof environment !== 'undefined' ? environment : {};
+    const primaryBackground = env.hasOwnProperty('primaryBackground') ? env.primaryBackground : '#b8002c';
+    const transitionBackground = env.hasOwnProperty('transitionBackground') ? env.transitionBackground : '#db0100';
+    const secondaryBackground = env.hasOwnProperty('secondaryBackground') ? env.secondaryBackground : '#e8410c';
+    return {
+        entry: {
+            bundle: './src/js/bundle',
+            stylesheet: './src/js/style'
+        },
+        output: {
+            path: path.resolve(buildDir),
+            filename: localConfig['js_filename']
+        },
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /\/node_modules\//,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    url: false,
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    indentedSyntax: false,
+                                    sourceMap: true,
+                                    data: "$primaryBackground: " + primaryBackground + '; ' +
+                                          "$transitionBackground: " + transitionBackground + "; " +
+                                          "$secondaryBackground: " + secondaryBackground + ";"
+                                }
+                            }
+                        ]
+                    })
                 }
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false,
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                indentedSyntax: false,
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
-            }
+            ]
+        },
+        devtool: 'source-map',
+        plugins: [
+            // Provides jQuery for other JS bundled with Webpack
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery'
+            }),
+            new ExtractTextPlugin({
+                filename: localConfig['css_filename'],
+                ignoreOrder: true
+            }),
+            new CopyWebpackPlugin([
+                {
+                    from: path.resolve(__dirname + '/node_modules/font-awesome/fonts/*'),
+                    to: 'fonts/',
+                    flatten: true
+                }
+            ])
         ]
-    },
-    devtool: 'source-map',
-    plugins: [
-        // Provides jQuery for other JS bundled with Webpack
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new ExtractTextPlugin({
-            filename: localConfig['css_filename'],
-            ignoreOrder: true
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname + '/node_modules/font-awesome/fonts/*'),
-                to: 'fonts/',
-                flatten: true
-            }
-        ])
-    ]
+    }
 };
