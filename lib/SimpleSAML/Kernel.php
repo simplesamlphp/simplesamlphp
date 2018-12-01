36adc7d8f930be90ec74558d6ddb319fb77c7e98
Sergio Gómez <sergio@uco.es>
2018-12-01 18:46:18 +0100

    Added Kernel class to handle request

---
+++ lib/SimpleSAML/Kernel.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace SimpleSAML;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Exception\FileLocatorFileNotFoundException;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+/**
+ * A class to create the container and handle a given request.
+ */
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    const VERSION = '1.17.0-DEV';
+    const VERSION_ID = 11700;
+    const MAJOR_VERSION = 1;
+    const MINOR_VERSION = 17;
+    const RELEASE_VERSION = 00;
+    const EXTRA_VERSION = 'DEV';
+
+    const END_OF_MAINTENANCE = 'UNDEFINED';
+    const END_OF_LIFE = 'UNDEFINED';
+
+    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    /**
+     * @var string
+     */
+    private $module;
+
+    public function __construct($module)
+    {
+        $this->module = $module;
+
+        parent::__construct('prod', false);
+    }
+
+    public function getCacheDir()
+    {
+        $configuration = Configuration::getInstance();
+        $cachePath = $configuration->getString('tempdir').'/cache/'.$this->module;
+
+        if (0 === strpos($cachePath, '/')) {
+            return $cachePath;
+        }
+
+        return $configuration->getBaseDir().'/'. $cachePath;
+    }
+
+    public function getLogDir()
+    {
+        $configuration = Configuration::getInstance();
+        $loggingPath = $configuration->getString('loggingdir');
+
+        if (0 === strpos($loggingPath, '/')) {
+            return $loggingPath;
+        }
+
+        return $configuration->getBaseDir().'/'. $loggingPath;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+        ];
+    }
+
+    /**
+     * Get the module loaded in this kernel.
+     *
+     * @return string
+     */
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    /**
+     * Configures the container.
+     *
+     * @param ContainerBuilder $container
+     * @param LoaderInterface $loader
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $confDir = __DIR__;
+        $loader->load($confDir . '/Resources/config/services/*' . self::CONFIG_EXTS, 'glob');
+        $confDir = Module::getModuleDir($this->module) . '/config/services';
+        if (is_dir($confDir)) {
+            $loader->load($confDir . '/**/*' . self::CONFIG_EXTS, 'glob');
+        }
+
+        $container->loadFromExtension('framework', [
+            'secret' => Configuration::getInstance()->getString('secretsalt'),
+        ]);
+
+        $this->registerModuleControllers($container);
+    }
+
+    /**
+     * Import routes.
+     *
+     * @param RouteCollectionBuilder $routes
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $confDir = __DIR__;
+        $routes->import($confDir . '/Resources/config/routes/*' . self::CONFIG_EXTS, '/', 'glob');
+        $confDir = Module::getModuleDir($this->module) . '/config/routes';
+        if (is_dir($confDir)) {
+            $routes->import($confDir . '/**/*' . self::CONFIG_EXTS, $this->module, 'glob');
+        }
+    }
+
+    /**
+     * @param $container
+     */
+    private function registerModuleControllers($container)
+    {
+        try {
+            $definition = new Definition();
+            $definition->setAutowired(true);
+
+            $controllerDir = Module::getModuleDir($this->module) . '/lib/Controller';
+
+            if (!is_dir($controllerDir)) {
+                return;
+            }
+
+            $loader = new \Symfony\Component\DependencyInjection\Loader\DirectoryLoader(
+                $container,
+                new FileLocator($controllerDir . '/')
+            );
+            $loader->registerClasses(
+                $definition,
+                'SimpleSAML\\Module\\' . $this->module . '\\Controller\\',
+                $controllerDir . '/*'
+            );
+        } catch (FileLocatorFileNotFoundException $e) {
+        }
+    }
+
+//
+//    public function loadRoutes(LoaderInterface $loader)
+//    {
+//        $routes = new RouteCollectionBuilder($loader);
+//        $this->configureRoutes($routes);
+//
+//        return $routes->build();
+//    }
+//    public function registerContainerConfiguration(LoaderInterface $loader)
+//    {
+//        $loader->load(function (ContainerBuilder $container) use ($loader) {
+//            $this->configureContainer($container, $loader);
+//
+//            $container->addObjectResource($this);
+//        });
+//    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setAnnotatedClassCache(array $annotatedClasses)
+    {
+        // Disabled cache
+        // parent::setAnnotatedClassCache($annotatedClasses);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function initializeContainer()
+    {
+        // Disabled cache
+        // return parent::initializeContainer();
+        $this->container = $this->buildContainer();
+        $this->container->compile();
+        $this->container->set('kernel', $this);
+    }
+}
