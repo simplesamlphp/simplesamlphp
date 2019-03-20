a7bdfcd1dc5f34e38daa9ebabe3081a87a3f95e3
Tim van Dijen <tvdijen@gmail.com>
2019-03-20 20:34:28 +0100

    Update TemplateControllerInterface.php

--- lib/SimpleSAML/XHTML/TemplateControllerInterface.php
+++ lib/SimpleSAML/XHTML/TemplateControllerInterface.php
@@ -13,11 +13,11 @@ interface TemplateControllerInterface
     /**
      * Implement to modify the twig environment after its initialization (e.g. add filters or extensions).
      *
-     * @param \Twig_Environment $twig The current twig environment.
+     * @param \Twig\Environment $twig The current twig environment.
      *
      * @return void
      */
-    public function setUpTwig(\Twig_Environment &$twig);
+    public function setUpTwig(\Twig\Environment &$twig);
 
 
     /**
