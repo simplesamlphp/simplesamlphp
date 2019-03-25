2ee5ad3404db02ca221edcf1ac0227bdac848239
Tim van Dijen <tvdijen@gmail.com>
2019-03-25 21:51:36 +0100

    Externalize Radius module (#1087)

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -59,6 +59,7 @@ php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-memcookie
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-memcachemonitor
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-oauth
+php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-radius
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-riak
 
 cd $TARGET
