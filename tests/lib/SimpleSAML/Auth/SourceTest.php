d3671e9efbd799848d6ad55782f373e74ec8f107
Pavel Břoušek <433364@mail.muni.cz>
2019-04-12 12:53:15 +0200

    tests for forced username prefill

--- tests/lib/SimpleSAML/Auth/SourceTest.php
+++ tests/lib/SimpleSAML/Auth/SourceTest.php
@@ -4,6 +4,7 @@ namespace SimpleSAML\Test\Auth;
 
 use SimpleSAML\Auth\SourceFactory;
 use SimpleSAML\Test\Utils\ClearStateTestCase;
+use \SimpleSAML\Configuration;
 
 /**
  * Tests for \SimpleSAML\Auth\Source
@@ -24,6 +25,44 @@ class SourceTest extends ClearStateTestCase
         // test instantiation via an auth source factory
         $authSource = $method->invokeArgs(null, ['test', ['SimpleSAML\Test\Auth\TestAuthSourceFactory']]);
         $this->assertInstanceOf('SimpleSAML\Test\Auth\TestAuthSource', $authSource);
+    }
+
+    public function testInitLoginForceUsernameDefault()
+    {
+        $_POST['username'] = 'test_username';
+
+        $this->config = Configuration::loadFromArray(['forced_username_enabled' => true], '[ARRAY]', 'simplesaml');
+        Configuration::setPreLoadedConfig($this->config, 'config.php');
+
+        $mock = $this->getMockBuilder('\SimpleSAML\Auth\Source')->setMethods(array('authenticate', 'loginCompleted'))->disableOriginalConstructor()->getMock();
+        $mock->expects($this->once())
+            ->method('authenticate')
+            ->with($this->callback(function($state){
+                return isset($state['forcedUsername']) && $state['forcedUsername'] === 'test_username';
+            }));
+
+        $mock->initLogin(null);
+    }
+
+    public function testInitLoginForceUsername()
+    {
+        $_POST['prefill'] = '12345@example.com';
+
+        $this->config = Configuration::loadFromArray([
+            'forced_username_enabled' => true,
+            'forced_username_field' => 'prefill',
+            'forced_username_pattern' => '/^(?<username>.*)@example\.com$/'
+        ], '[ARRAY]', 'simplesaml');
+        Configuration::setPreLoadedConfig($this->config, 'config.php');
+
+        $mock = $this->getMockBuilder('\SimpleSAML\Auth\Source')->setMethods(array('authenticate', 'loginCompleted'))->disableOriginalConstructor()->getMock();
+        $mock->expects($this->once())
+            ->method('authenticate')
+            ->with($this->callback(function($state){
+                return isset($state['forcedUsername']) && $state['forcedUsername'] === '12345';
+            }));
+
+        $mock->initLogin(null);
     }
 }
 
