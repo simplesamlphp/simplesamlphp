a29a8ae07033281c92005d9fb82f7995848d6a30
Tim van Dijen <tvdijen@gmail.com>
2019-05-28 15:56:18 +0200

    Externalize sanitycheck (#1130)

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -70,6 +70,7 @@ php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-preprodwarning
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-radius
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-riak
+php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-sanitycheck
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-smartattributes
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-statistics
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-sqlauth
