c806b11c155e52545384ea8af1685ca0d5fda1e0
Tim van Dijen <tim.dijen@minbzk.nl>
2019-05-28 13:40:15 +0200

    Externalize smartattributes (#1126)

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -68,6 +68,7 @@ php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-oauth
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-radius
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-riak
+php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-smartattributes
 php "$TARGET/composer.phar" require --update-no-dev simplesamlphp/simplesamlphp-module-statistics
 
 cd $TARGET
