7317025e17d386092dc066144791b57c7fa5b011
Tim van Dijen <tvdijen@gmail.com>
2019-07-15 23:01:28 +0200

    Fix RedundantCondition

--- tests/modules/saml/lib/Auth/Source/Auth_Source_SP_Test.php
+++ tests/modules/saml/lib/Auth/Source/Auth_Source_SP_Test.php
@@ -294,6 +294,7 @@ class SPTest extends ClearStateTestCase
      */
     public function testForcedAuthn()
     {
+        /** @var bool $state['ForceAuthn'] */
         $state = [
             'ForceAuthn' => true
         ];
