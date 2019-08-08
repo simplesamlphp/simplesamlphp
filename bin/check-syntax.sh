e9cc7a7d835fecfaa88da94c101937cace47b8ad
Tim van Dijen <tim.dijen@minbzk.nl>
2019-08-08 17:35:43 +0200

    Make YAML-check more quiet

--- bin/check-syntax.sh
+++ bin/check-syntax.sh
@@ -26,7 +26,6 @@ for i in `find . -path ./vendor -prune -o -path ./node_modules -prune -o -name '
 do
     if [ -f "$i" ]; then
         FILE="${i%/*}/${i##*/}"
-        echo "Checking file $i"
         $PHP -r "require(dirname(dirname(__FILE__)).'/vendor/autoload.php'); use Symfony\Component\Yaml\Yaml; use Symfony\Component\Yaml\Exception\ParseException; try { Yaml::parseFile('$FILE'); } catch(ParseException \$e) { exit(1); }"
         if [ $? -ne 0 ]; then
             echo "Syntax check failed for ${FILE}"
