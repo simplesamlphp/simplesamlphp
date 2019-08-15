ba028c223c19ec6bf7b341d85e3b94d1dd3befcd
Tim van Dijen <tim.dijen@minbzk.nl>
2019-08-15 16:07:12 +0200

    Fully typehint lib/XHTML/*.php

--- lib/SimpleSAML/XHTML/TemplateControllerInterface.php
+++ lib/SimpleSAML/XHTML/TemplateControllerInterface.php
@@ -21,7 +21,7 @@ interface TemplateControllerInterface
      *
      * @return void
      */
-    public function setUpTwig(Environment &$twig);
+    public function setUpTwig(Environment &$twig) : void;
 
 
     /**
@@ -33,5 +33,5 @@ interface TemplateControllerInterface
      *
      * @return void
      */
-    public function display(&$data);
+    public function display(array &$data) : void;
 }
