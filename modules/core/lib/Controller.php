cab879d9ff51ec9bad205336b0e648ed5bf9f1ea
Jaime Pérez Crespo <jaime.perez@uninett.no>
2019-08-22 13:59:30 +0200

    Start using the new attribute translations

--- modules/core/lib/Controller.php
+++ modules/core/lib/Controller.php
@@ -84,6 +84,8 @@ class Controller
         $attributes = $auth->getAttributes();
 
         $t = new Template($this->config, 'auth_status.twig', 'attributes');
+        $l = $t->getLocalization();
+        $l->addDomain($l->getLocaleDir(), 'attributes');
         $t->data['header'] = '{status:header_saml20_sp}';
         $t->data['attributes'] = $attributes;
         $t->data['nameid'] = !is_null($auth->getAuthData('saml:sp:NameID'))
