59d0d5b51db47c74ab4b825e2b1a7e4b2e39e834
Tim van Dijen <tvdijen@gmail.com>
2019-09-26 19:59:47 +0200

    Fix TypeCoercion

--- lib/SimpleSAML/Console/Application.php
+++ lib/SimpleSAML/Console/Application.php
@@ -2,16 +2,16 @@
 
 namespace SimpleSAML\Console;
 
+use SimpleSAML\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application as BaseApplication;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class Application extends BaseApplication
 {
     /**
-     * @param \Symfony\Component\HttpKernel\HttpKernelInterface $kernel
+     * @param \SimpleSAML\Kernel $kernel
      */
-    public function __construct(HttpKernelInterface $kernel)
+    public function __construct(Kernel $kernel)
     {
         parent::__construct($kernel);
 
