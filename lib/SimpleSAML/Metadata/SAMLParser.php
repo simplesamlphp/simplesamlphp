8e41ba7804b9865909601fd8dee9732434afd081
Tim van Dijen <tvdijen@gmail.com>
2019-11-06 15:48:08 +0100

    Psalm: fix InvalidArgument

--- lib/SimpleSAML/Metadata/SAMLParser.php
+++ lib/SimpleSAML/Metadata/SAMLParser.php
@@ -1162,7 +1162,7 @@ class SAMLParser
 
                 $name = $attribute->getAttribute('Name');
                 $values = array_map(
-                    ['\SimpleSAML\Utils\XML', 'getDOMText'],
+                    '\SimpleSAML\Utils\XML::getDOMText',
                     Utils\XML::getDOMChildren($attribute, 'AttributeValue', '@saml2')
                 );
 
@@ -1322,7 +1322,7 @@ class SAMLParser
      */
     private static function extractEndpoints(array $endpoints): array
     {
-        return array_map(['self', 'parseGenericEndpoint'], $endpoints);
+        return array_map('self::parseGenericEndpoint', $endpoints);
     }
 
 
