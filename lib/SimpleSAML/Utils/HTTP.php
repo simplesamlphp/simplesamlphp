b3b9696bec1bfe5d1a9830c4464cca3229d9376b
Tim van Dijen <tim.dijen@minbzk.nl>
2019-11-07 14:29:18 +0100

    Fix testSetCookieSameSite for PHP <7.3

--- lib/SimpleSAML/Utils/HTTP.php
+++ lib/SimpleSAML/Utils/HTTP.php
@@ -1165,7 +1165,7 @@ class HTTP
             'lifetime' => 0,
             'expire'   => null,
             'path'     => '/',
-            'domain'   => null,
+            'domain'   => version_compare(PHP_VERSION, '7.3.0', '>=') ? null : '',
             'secure'   => false,
             'httponly' => true,
             'raw'      => false,
