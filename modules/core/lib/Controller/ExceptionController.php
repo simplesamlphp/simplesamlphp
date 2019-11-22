a4667fa0980d5892b0084cbbb1fdb789f0fe63d9
Tim van Dijen <tvdijen@gmail.com>
2019-11-22 20:21:06 +0100

    Remove unnecessary use of Translate::t

--- modules/core/lib/Controller/ExceptionController.php
+++ modules/core/lib/Controller/ExceptionController.php
@@ -99,20 +99,6 @@ class ExceptionController
         }
 
         $t = new Template($this->config, 'core:no_cookie.tpl.php');
-        $translator = $t->getTranslator();
-
-        /** @var string $header */
-        $header = $translator->t('{core:no_cookie:header}');
-
-        /** @var string $desc */
-        $desc = $translator->t('{core:no_cookie:description}');
-
-        /** @var string $retry */
-        $retry = $translator->t('{core:no_cookie:retry}');
-
-        $t->data['header'] = htmlspecialchars($header);
-        $t->data['description'] = htmlspecialchars($desc);
-        $t->data['retry'] = htmlspecialchars($retry);
         $t->data['retryURL'] = $retryURL;
         return $t;
     }
@@ -147,10 +133,8 @@ class ExceptionController
 
         $t = new Template($this->config, 'core:short_sso_interval.tpl.php');
         $translator = $t->getTranslator();
-        $t->data['target'] = Module::getModuleURL('core/short_sso_interval.php');
         $t->data['params'] = ['StateId' => $stateId];
         $t->data['trackId'] = $this->session->getTrackID();
-        $t->data['header'] = $translator->t('{core:short_sso_interval:warning_header}');
         $t->data['autofocus'] = 'contbutton';
         return $t;
     }
