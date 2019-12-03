f1c020fd23af6e82a0fa66aa9aa524ed1002ed46
Jaime Pérez Crespo <jaime.perez@uninett.no>
2019-12-03 14:01:42 +0100

    Use a class alias for SimpleSAML\Auth\LDAP
    Extending the other class served no purpose, so a class alias should be enough. Also, enhance a bit the exception message, and log a warning with our own logger.
    
    Signed-off-by: Jaime Pérez Crespo <jaime.perez@uninett.no>

--- lib/SimpleSAML/Auth/LDAP.php
+++ lib/SimpleSAML/Auth/LDAP.php
@@ -2,39 +2,13 @@
 
 namespace SimpleSAML\Auth;
 
-@trigger_error(
-    sprintf('Using the "SimpleSAML\Auth\LDAP" class is deprecated, use "SimpleSAML\Module\ldap\Auth\Ldap" instead.'),
-    E_USER_DEPRECATED
-);
+\SimpleSAML\Logger::warning("The class \SimpleSAML\Auth\LDAP has been moved to the ldap module, please use \SimpleSAML\Module\saml\Auth\Ldap instead.");
 
 /**
  * @deprecated To be removed in 2.0
  */
 if (class_exists('\SimpleSAML\Module\ldap\Auth\Ldap')) {
-    class LDAP extends \SimpleSAML\Module\ldap\Auth\Ldap
-    {
-        /**
-         * Private constructor restricts instantiation to getInstance().
-         *
-         * @param string $hostname
-         * @param bool $enable_tls
-         * @param bool $debug
-         * @param int $timeout
-         * @param int $port
-         * @param bool $referrals
-         * @psalm-suppress NullArgument
-         */
-        public function __construct(
-            $hostname,
-            $enable_tls = true,
-            $debug = false,
-            $timeout = 0,
-            $port = 389,
-            $referrals = true
-        ) {
-            parent::__construct($hostname, $enable_tls, $debug, $timeout, $port, $referrals);
-        }
-    }
+    class_alias(\SimpleSAML\Module\ldap\Auth\Ldap::class, 'SimpleSAML\Auth\LDAP');
 } else {
-    throw new \Exception('Missing ldap-module');
+    throw new \Exception('The ldap module is either missing or disabled');
 }
