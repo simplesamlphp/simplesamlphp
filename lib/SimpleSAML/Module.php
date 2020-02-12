simplesamlphp/simplesamlphp

commit 6607b7fbced6a7bc0d49c59c2479c908c9ee7681

Raise PHP functional level to 7.0 (#1242)
* Raise minimum PHP version to 7.0

* Remove tests pre-PHP 7.2

* Upgrade dev dependencies

* Ignore tests for deprecated class

* Add typehints; not touching public API

* Remove none-array replacements-param; old behaviour from pre-1.4 release

* Psalm fixes

* Add upgrade notes

* PSR-12

lib/SimpleSAML/Module.php
@@ -305,7 +305,7 @@ class Module
      * @param array $mod_config
      * @return bool
      */
-    private static function isModuleEnabledWithConf($module, $mod_config)
+    private static function isModuleEnabledWithConf(string $module, array $mod_config): bool
     {
         if (isset(self::$module_info[$module]['enabled'])) {
             return self::$module_info[$module]['enabled'];
@@ -539,7 +539,7 @@ class Module
                 self::$module_info[$module]['hooks'] = self::getModuleHooks($module);
             }
 
-            if (!isset(self::$module_info[$module]['hooks'][$hook])) {
+            if (!isset(self::$module_info[$module]['hooks'][$hook]) || !empty(self::$module_info[$module]['hooks'][$hook])) {
                 continue;
             }
 

commit 8f0fc128033a665fc9e6868be3ff514cc0353b31

Unbreak all hooks
Broken during refactoring in 6607b7fbced6a7bc0d49c59c2479c908c9ee7681

lib/SimpleSAML/Module.php
@@ -553,7 +553,7 @@ class Module
 
             if (
                 !isset(self::$module_info[$module]['hooks'][$hook])
-                || !empty(self::$module_info[$module]['hooks'][$hook])
+                || empty(self::$module_info[$module]['hooks'][$hook])
             ) {
                 continue;
             }