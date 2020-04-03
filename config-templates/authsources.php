fc1cd638c85e0ac9632c04d657c4ea4b6a773418
Philipp Kolmann <philipp@kolmann.at>
2020-04-03 14:16:26 +0200

    Use auth sources by default by identifying them as "default" in the config. (+3 squashed commits) Squashed commits: [090679e6b] authsources must be an array [1778d0c21] only set the AuthSource if it also exists, but always remove the config variable. [c09669e32] add the possibility to specify one AuthSource to be shown and the rest are used via multiauth:MultiAuth

--- config-templates/authsources.php
+++ config-templates/authsources.php
@@ -1,6 +1,17 @@
 <?php
 
 $config = [
+    /*
+     * When multiple authentication sources are defined, you can specify one to use by default
+     * in order to authenticate users. In order to do that, you just need to name it "default"
+     * here. That authentication source will be used by default then when a user reaches the
+     * SimpleSAMLphp installation from the web browser, without passing through the API.
+     *
+     * If you already have named your auth source with a different name, you don't need to change
+     * it in order to use it as a default. Just create an alias by the end of this file:
+     *
+     * $config['default'] = &$config['your_auth_source'];
+     */
 
     // This is a authentication source which handles admin authentication.
     'admin' => [
