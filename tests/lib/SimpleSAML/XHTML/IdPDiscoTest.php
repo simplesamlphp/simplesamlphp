549a53868ad13d743b1068b72b308a57d4d1f2c6
Tim van Dijen <tvdijen@gmail.com>
2020-06-06 01:46:50 +0200

    Fix signatures

--- tests/lib/SimpleSAML/XHTML/IdPDiscoTest.php
+++ tests/lib/SimpleSAML/XHTML/IdPDiscoTest.php
@@ -43,7 +43,7 @@ class IdPDiscoTest extends TestCase
     /**
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         $config = Configuration::loadFromArray($this->config);
 
@@ -55,7 +55,7 @@ class IdPDiscoTest extends TestCase
 
     /**
      */
-    public function testPreferredTranslation()
+    public function testPreferredTranslation(): void
     {
         $disco = new IdPDisco(['saml20-idp-remote'], 'unittest');
 
@@ -71,7 +71,7 @@ class IdPDiscoTest extends TestCase
 
     /**
      */
-    public function testIdPSortingOrder()
+    public function testIdPSortingOrder(): void
     {
         $disco = new IdPDisco(['saml20-idp-remote'], 'unittest');
 
