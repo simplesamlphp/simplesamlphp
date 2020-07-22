0a0d9ec6bc0db541d528796acdb5dad28675353e
Simão Martins <Lasering@users.noreply.github.com>
2020-07-22 15:14:36 +0100

    Update SP.php

--- modules/saml/lib/Auth/Source/SP.php
+++ modules/saml/lib/Auth/Source/SP.php
@@ -586,10 +586,10 @@ class SP extends \SimpleSAML\Auth\Source
 
         $ar->setRequesterID($requesterID);
 
-        if (isset($state['saml:Extensions'])) {
-            $ar->setExtensions($state['saml:Extensions']);
-        } else if ($this->metadata->getArray('saml:Extensions', null) !== null) {
+        if ($this->metadata->getArray('saml:Extensions', null) !== null) {
             $ar->setExtensions($this->metadata->getArray('saml:Extensions'));
+        } else if (isset($state['saml:Extensions'])) {
+            $ar->setExtensions($state['saml:Extensions']);
         }
         
         $providerName = $this->metadata->getString("ProviderName", null);
