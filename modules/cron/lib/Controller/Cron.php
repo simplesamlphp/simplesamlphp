eee431df3975266f866268bc704135ce5c4d508f
Tim van Dijen <tim.dijen@minbzk.nl>
2020-08-01 16:59:20 +0200

    Remove extraneous phpdoc

--- modules/cron/lib/Controller/Cron.php
+++ modules/cron/lib/Controller/Cron.php
@@ -42,7 +42,6 @@ class Cron
      * It initializes the global configuration and auth source configuration for the controllers implemented here.
      *
      * @param \SimpleSAML\Configuration              $config The configuration to use by the controllers.
-     * @param \SimpleSAML\Configuration              $moduleConfig The module-configuration to use by the controllers.
      * @param \SimpleSAML\Session                    $session The session to use by the controllers.
      *
      * @throws \Exception
