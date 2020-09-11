9aff973c75cd42385af976b85c13d9f103719a0e
Martin Krisell <martin.krisell@gmail.com>
2020-09-11 22:57:19 +0200

    Fix SQLServer ALTER syntax

--- modules/saml/lib/SP/LogoutStore.php
+++ modules/saml/lib/SP/LogoutStore.php
@@ -43,6 +43,11 @@ class LogoutStore
                         'ALTER TABLE ' . $store->prefix . '_saml_LogoutStore ALTER COLUMN _expire TYPE TIMESTAMP'
                     ];
                     break;
+                case 'sqlsrv':
+                    $update = [
+                        'ALTER TABLE ' . $store->prefix . '_saml_LogoutStore ALTER COLUMN _expire DATETIME NOT NULL'
+                    ];
+                    break;
                 case 'sqlite':
                     /**
                      * Because SQLite does not support field alterations, the approach is to:
@@ -119,6 +124,11 @@ class LogoutStore
                     $update = [
                         'ALTER TABLE ' . $store->prefix .
                         '_saml_LogoutStore ALTER COLUMN _authSource TYPE VARCHAR(255)'];
+                    break;
+               case 'sqlsrv':
+                    $update = [
+                        'ALTER TABLE ' . $store->prefix . '_saml_LogoutStore ALTER COLUMN _authSource VARCHAR(255) NOT NULL'
+                    ];
                     break;
                 case 'sqlite':
                     /**
