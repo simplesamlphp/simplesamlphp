3e9381452b9ff82f3a2a66d29b6129276e499d6a
Tim van Dijen <tim.dijen@minbzk.nl>
2020-10-02 15:53:23 +0200

    Fix regression; Do not normalize template files to twig just because we have a theme

--- lib/SimpleSAML/XHTML/Template.php
+++ lib/SimpleSAML/XHTML/Template.php
@@ -233,7 +233,7 @@ class Template extends Response
             $templateName = substr($templateName, 0, $tplpos);
         }
 
-        if ($this->useNewUI || ($this->theme['module'] !== null)) {
+        if ($this->useNewUI) {
             return $templateName . '.twig';
         }
         return $templateName;
