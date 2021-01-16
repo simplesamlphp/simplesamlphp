3f31e7d1ef3510905cacd306730b862788e4fe54
Tim van Dijen <tvdijen@gmail.com>
2021-01-16 12:20:49 +0100

    Remove unused class; closes #1421

--- www/_include.php
+++ www/_include.php
@@ -4,7 +4,7 @@
 require_once(dirname(dirname(__FILE__)) . '/lib/_autoload.php');
 
 // enable assertion handler for all pages
-\SimpleSAML\Error\Assertion::installHandler();
+//\SimpleSAML\Error\Assertion::installHandler();
 
 // show error page on unhandled exceptions
 function SimpleSAML_exception_handler($exception)
