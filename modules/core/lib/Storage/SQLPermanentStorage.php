ea7c23a11b7b64f6d96f3c6719f2d390fd47a50f
Tim van Dijen <tvdijen@gmail.com>
2021-01-17 15:41:19 +0100

    Fix query; apparently under PHP8 the original query would raise PDOException instead of returning false

--- modules/core/lib/Storage/SQLPermanentStorage.php
+++ modules/core/lib/Storage/SQLPermanentStorage.php
@@ -48,6 +48,7 @@ class SQLPermanentStorage
 
         $dbfile = 'sqlite:' . $sqllitedir . $name . '.sqlite';
         if ($this->db = new PDO($dbfile)) {
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
             $q = @$this->db->query('SELECT key1 FROM data LIMIT 1');
             if ($q === false) {
                 $this->db->exec('
