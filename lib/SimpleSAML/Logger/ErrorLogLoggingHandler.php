7c53ff5fe0df0c287fecc8a1a261b58e5e5c481f
Tim van Dijen <tvdijen@gmail.com>
2021-03-11 22:42:25 +0100

    Fix rebase issue

--- lib/SimpleSAML/Logger/ErrorLogLoggingHandler.php
+++ lib/SimpleSAML/Logger/ErrorLogLoggingHandler.php
@@ -21,14 +21,14 @@ class ErrorLogLoggingHandler implements LoggingHandlerInterface
      * @var array
      */
     private static $levelNames = [
-        LogLevel::EMERGENCY,
-        LogLevel::ALERT,
-        LogLevel::CRITICAL,
-        LogLevel::ERROR,
-        LogLevel::WARNING,
-        LogLevel::NOTICE,
-        LogLevel::INFO,
-        LogLevel::DEBUG,
+        LogLevel::EMERGENCY => 0,
+        LogLevel::ALERT => 1,
+        LogLevel::CRITICAL => 2,
+        LogLevel::ERROR => 3,
+        LogLevel::WARNING => 4,
+        LogLevel::NOTICE => 5,
+        LogLevel::INFO => 6,
+        LogLevel::DEBUG => 7,
     ];
 
     /**
