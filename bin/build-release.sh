b4c4bab68593c39feb8599e3af458b0486df6aa7
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-05-10 10:09:01 +0000

    Fix removing of development files

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -64,11 +64,11 @@ rm "$TARGET/www/assets/js/stylesheet.js"*
 rm "$TARGET/.editorconfig"
 rm "$TARGET/.gitattributes"
 rm -r "$TARGET/.github"
-rm "$TARGET"/{,modules}/.php_cs.dist
-rm "$TARGET"/{,modules}/codecov.yml
-rm "$TARGET"/{,modules}/phpcs.xml
-rm "$TARGET"/{,modules}/psalm.xml
-rm "$TARGET"/{,modules}/.gitignore
+rm "$TARGET"/{,modules/*}/.php_cs.dist
+rm "$TARGET"/{,modules/*}/codecov.yml
+rm "$TARGET"/{,modules/*}/phpcs.xml
+rm "$TARGET"/{,modules/*}/psalm.xml
+rm "$TARGET"/{,modules/*}/.gitignore
 rm "$TARGET"/{cache,config,metadata,locales}/.gitkeep
 rm "$TARGET/composer.phar"
 tar --owner 0 --group 0 -cvzf "$TARGET.tar.gz" "$TARGET"
