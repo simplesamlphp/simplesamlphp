a1157508d86de2636d6436cd118d8fc125ec3184
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-08-27 13:03:17 +0000

    Fix filterscopes using hostname when explicit scopes set.
    FilterScopes documented behaviour is that hostname is only used as
    a fallback when there are no explicit scopes (makes sense). However
    the code does not do that but always includes the hostname to match.
    
    This refactors the code so that hostname is only evaluated when
    no explicit scopes are there. Also it takes the whole fetching and
    parsing of the SSOlocation outside of the inner loop whcih is
    inefficient. This is now only done once, and only if needed (when
    no explicit scopes present).
    
    To confirm the changes testcases have been added for this and also
    for some other cases to reach 100% coverage for the filter.

--- tests/modules/saml/lib/Auth/Process/FilterScopesTest.php
+++ tests/modules/saml/lib/Auth/Process/FilterScopesTest.php
@@ -68,19 +68,12 @@ class FilterScopesTest extends TestCase
         $result = $this->processFilter($config, $request);
         $this->assertEquals($request['Attributes'], $result['Attributes']);
 
-        // test implicit scope
-        $request['Attributes'] = [
-            'eduPersonPrincipalName' => ['jdoe@example.org'],
-        ];
-        $result = $this->processFilter($config, $request);
-        $this->assertEquals($request['Attributes'], $result['Attributes']);
-
         // test alternative attributes
         $config['attributes'] = [
             'mail',
         ];
         $request['Attributes'] = [
-            'mail' => ['john.doe@example.org'],
+            'mail' => ['john.doe@example.com'],
         ];
         $result = $this->processFilter($config, $request);
         $this->assertEquals($request['Attributes'], $result['Attributes']);
@@ -91,6 +84,35 @@ class FilterScopesTest extends TestCase
         $this->assertEquals($request['Attributes'], $result['Attributes']);
     }
 
+    /**
+     * Test implict scope matching on IdP hostname
+     */
+    public function testImplicitScopes(): void
+    {
+        $config = [];
+        $request = [
+            'Source'     => [
+                'SingleSignOnService' => [
+                    [
+                        'Binding'  => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+                        'Location' => 'https://example.org/saml2/idp/SSOService.php',
+                    ],
+                ],
+            ],
+            'Attributes' => [
+                'eduPersonPrincipalName' => ['jdoe@example.org'],
+            ],
+        ];
+
+        $result = $this->processFilter($config, $request);
+        $this->assertEquals($request['Attributes'], $result['Attributes']);
+
+        $request['Attributes'] = [
+            'eduPersonPrincipalName' => ['jdoe@example.com'],
+        ];
+        $result = $this->processFilter($config, $request);
+        $this->assertEquals([], $result['Attributes']);
+    }
 
     /**
      * Test invalid scopes.
@@ -135,6 +157,65 @@ class FilterScopesTest extends TestCase
         $request['Attributes'] = [
             'eduPersonPrincipalName' => ['jdoe'],
         ];
+        $result = $this->processFilter($config, $request);
+        $this->assertEquals($request['Attributes'], $result['Attributes']);
+
+    }
+
+    /**
+     * Test that implicit matching is not done when explicit scopes present
+     */
+    public function testNoImplicitMatchingWhenExplicitScopes(): void
+    {
+        // test declared scopes
+        $config = [];
+        $request = [
+            'Source'     => [
+                'SingleSignOnService' => [
+                    [
+                        'Binding'  => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+                        'Location' => 'https://example.org/saml2/idp/SSOService.php',
+                    ],
+                ],
+                'scope' => [
+                    'example.com',
+                    'example.net',
+                ],
+            ],
+            'Attributes' => [
+                'eduPersonPrincipalName' => ['jdoe@example.org'],
+            ],
+        ];
+        $result = $this->processFilter($config, $request);
+        $this->assertEquals([], $result['Attributes']);
+    }
+
+    /**
+     * Test that the scope is considered to be the part after the first @ sign
+     */
+    public function testAttributeValueMultipleAt(): void
+    {
+        $config = [];
+        $request = [
+            'Source'     => [
+                'SingleSignOnService' => [
+                    [
+                        'Binding'  => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+                        'Location' => 'https://example.org/saml2/idp/SSOService.php',
+                    ],
+                ],
+                'scope' => [
+                    'example.com',
+                ],
+            ],
+            'Attributes' => [
+                'eduPersonPrincipalName' => ['jdoe@gmail.com@example.com'],
+            ],
+        ];
+        $result = $this->processFilter($config, $request);
+        $this->assertEquals([], $result['Attributes']);
+
+        $request['Source']['scope'] = ['gmail.com@example.com'];
         $result = $this->processFilter($config, $request);
         $this->assertEquals($request['Attributes'], $result['Attributes']);
     }
