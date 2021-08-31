67ca9cc54954695f082323d91235d8ce2b759eb1
Tim van Dijen <tim.dijen@minbzk.nl>
2021-08-31 20:34:25 +0200

    Fix copy/paste mistake

--- lib/SimpleSAML/SessionHandlerPHP.php
+++ lib/SimpleSAML/SessionHandlerPHP.php
@@ -140,7 +140,7 @@ class SessionHandlerPHP extends SessionHandler
     {
         if ($this->hasSessionCookie()) {
             session_regenerate_id(false);
-            $session_id = session_id();
+            $sessionId = session_id();
         } else {
             // generate new (secure) session id
             $sid_length = (int) ini_get('session.sid_length');
