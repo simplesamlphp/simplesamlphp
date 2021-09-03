213add970a92e581ffad3e3d13ee3e8910e8d9e8
Thijs Kinkhorst <thijs@kinkhorst.com>
2021-09-03 14:55:05 +0000

    getEntityDisplayName to get entityID's name for display
    Reimplement getPreferredTranslation outside of IdPDisco so it's reusable
    and much more complete. The new method will search the current, default
    and fallback language for a displayable string in various metadata
    fields. This should be much more complete and robust than previous
    approches. Make it a Twig filter so it can also be used there.

--- lib/SimpleSAML/IdP.php
+++ lib/SimpleSAML/IdP.php
@@ -169,6 +169,8 @@ class IdP
 
     /**
      * Get SP name.
+     * Only usefd in IFrameLogout it seems.
+     * TODO: probably replace with template Template::getEntityDisplayName()
      *
      * @param string $assocId The association identifier.
      *
---
+++ tests/lib/SimpleSAML/XHTML/TemplateTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XHTML;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Configuration;
+use SimpleSAML\XHTML\Template;
+
+/**
+ * @covers \SimpleSAML\XHTML\Template
+ */
+class TemplateTest extends TestCase
+{
+    private const TEMPLATE = 'sandbox.twig';
+
+    function testSetup(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+        $this->assertEquals(self::TEMPLATE, $t->getTemplateName());
+    }
+
+    function testNormalizeName(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'sandbox');
+        $this->assertEquals(self::TEMPLATE, $t->getTemplateName());
+    }
+
+    function testTemplateModuleNamespace(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'core:login');
+        $this->assertEquals('core:login.twig', $t->getTemplateName());
+    }
+
+    function testGetEntityDisplayNameBasic(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+
+        $data = [
+            'entityid' => 'urn:example.org',
+            'name' => ['nl' => 'Something', 'en' => 'Other lang'],
+        ];
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('Other lang', $name);
+
+        $c = Configuration::loadFromArray(['language.default' => 'nl'], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('Something', $name);
+    }
+
+    function testGetEntityDisplayNamePriorities(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+
+        $data = [
+            'entityid' => 'urn:example.org',
+        ];
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('urn:example.org', $name);
+
+        $data['OrganizationName'] = ['fr' => 'Example Org', 'nl' => 'Anything Org'];
+        $data['OrganizationDisplayName'] = ['fr' => 'DisplayExample', 'nl' => 'DisplayAnything'];
+
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('urn:example.org', $name);
+
+        $data['OrganizationName']['en'] = 'Example Org EN';
+        
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('Example Org EN', $name);
+
+        $c = Configuration::loadFromArray(['language.default' => 'nl'], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+
+        $data['UIInfo']['DisplayName'] = ['de' => 'UIname', 'nl' => 'UIname NL'];
+        $name = $t->getEntityDisplayName($data);
+        $this->assertEquals('UIname NL', $name);
+    }
+
+    function testGetEntityPropertyTranslation(): void
+    {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+
+	$prop = 'description';
+        $data = [
+            'entityid' => 'urn:example.org',
+            $prop => ['nl' => 'Something', 'en' => 'Other lang', 'fr' => 'Another desc'],
+        ];
+        $name = $t->getEntityPropertyTranslation($prop, $data);
+        $this->assertEquals('Other lang', $name);
+
+        $c = Configuration::loadFromArray(['language.default' => 'nl'], '', 'simplesaml');
+        $t = new Template($c, self::TEMPLATE);
+        $name = $t->getEntityPropertyTranslation($prop, $data);
+        $this->assertEquals('Something', $name);
+
+	unset($data[$prop]['nl']);
+        $name = $t->getEntityPropertyTranslation($prop, $data);
+        $this->assertEquals('Other lang', $name);
+
+	unset($data[$prop]['en']);
+        $name = $t->getEntityPropertyTranslation($prop, $data);
+        $this->assertNull($name);
+    }
+}
