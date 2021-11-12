c6e531bb7d212d3072082d689ee0020dc8125c38
Ulrik nielsen <un@ipwsystems.dk>
2021-11-12 13:57:29 +0100

    Added tests to changed templates

---
+++ tests/lib/SimpleSAML/XHTML/TemplateTranslationTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\XHTML;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Configuration;
+use SimpleSAML\XHTML\Template;
+
+/**
+ * @covers \SimpleSAML\XHTML\Template
+ */
+class TemplateTranslationTest extends TestCase
+{
+    public function testCoreCardinalityErrorTemplate(): void {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'core:cardinality_error.twig');
+
+        $t->data['cardinalityErrorAttributes'] = [
+            'test 1' => [0, 1],
+            'test 2' => [1, 2],
+        ];
+
+        $getContent = function() {
+            /** @var Template $this */
+            return $this->getContents();
+        };
+        $html = $getContent->call($t);
+
+        $this->assertStringContainsString('got 0 values, want 1', $html);
+        $this->assertStringContainsString('got 1 values, want 2', $html);
+
+    }
+
+    public function testCoreLoginUserPassTemplate(): void {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'core:loginuserpass.twig');
+
+        $t->data['isProduction'] = false;
+        $t->data['errorcode'] = false;
+        $t->data['forceUsername'] = false;
+        $t->data['username'] = 'h.c oersted';
+        $t->data['rememberUsernameEnabled'] = false;
+        $t->data['rememberMeEnabled'] = false;
+        $t->data['stateparams'] = [];
+
+        $getContent = function() {
+            /** @var Template $this */
+            return $this->getContents();
+        };
+        $html = $getContent->call($t);
+
+        $this->assertStringContainsString('value="h.c oersted"', $html);
+    }
+
+    public function testCoreLogoutIframeTemplate(): void {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'core:logout-iframe.twig');
+
+        $t->data['auth_state'] = 'logout-test';
+        $t->data['type'] = 'test';
+        $t->data['terminated_service'] = [
+            'name' => [
+                'en' => 'ze testing service',
+            ],
+        ];
+        $t->data['remaining_services'] = [
+            'test' => [
+                'entityID' => 1234,
+                'metadata' => [
+                    'name' => [
+                        'en' => 'ze missing service',
+                    ],
+                ],
+                'status' => 'onhold',
+                'logoutURL' => 'https://xxx.yyy/',
+            ],
+        ];
+
+        $getContent = function() {
+            /** @var Template $this */
+            return $this->getContents();
+        };
+        $html = $getContent->call($t);
+
+        $this->assertStringContainsString('You are now successfully logged out from ze testing service.', $html);
+        $this->assertStringContainsString('ze missing service', $html);
+    }
+
+    public function testAuthStatusTemplate(): void {
+        $c = Configuration::loadFromArray([], '', 'simplesaml');
+        $t = new Template($c, 'auth_status.twig');
+
+        $t->data['remaining'] = 2;
+        $t->data['attributes'] = [];
+        $t->data['nameid'] = false;
+        $t->data['trackid'] = '';
+        $t->data['authData'] = false;
+
+        $getContent = function() {
+            /** @var Template $this */
+            return $this->getContents();
+        };
+        $html = $getContent->call($t);
+
+        $this->assertStringContainsString('Your session is valid for ' . $t->data['remaining'] . ' seconds from now.', $html);
+    }
+
+}
