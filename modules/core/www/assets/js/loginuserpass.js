d2d5c75b2381254c22b40a86bec05c4ed51ddddd
Tim van Dijen <tvdijen@gmail.com>
2022-03-16 16:53:35 +0100

    Rewrite JS in ES5 compatible manner

--- modules/core/www/assets/js/loginuserpass.js
+++ modules/core/www/assets/js/loginuserpass.js
@@ -1,7 +1,28 @@
-$(document).ready(function () {
-    $('#submit_button').on('click', function () {
-        $(this).attr('disabled', 'disabled');
-        $(this).html($(this).data('processing'));
-        $(this).parents('form').submit();
-    });
+window.readyHandlers = [];
+window.ready = function ready(handler) {
+  window.readyHandlers.push(handler);
+  handleState();
+};
+
+window.handleState = function handleState () {
+  if (document.readyState === 'interactive' || document.readyState === "complete") {
+    while(window.readyHandlers.length > 0) {
+      (window.readyHandlers.shift())();
+    }
+  }
+};
+
+document.onreadystatechange = window.handleState;
+
+ready(function () {
+  // your code here
+  var button = document.getElementById("submit_button");
+  button.onclick = function () {
+      this.innerHTML = button.getAttribute("data-processing");;
+      this.disabled = true;
+
+      var form = document.getElementById("f");
+      form.submit();
+      return true;
+  };
 });
