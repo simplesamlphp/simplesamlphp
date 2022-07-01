cf68e6ef7aaecf75d434023adfe30913d993f015
Tim van Dijen <tvdijen@gmail.com>
2022-07-01 21:47:50 +0200

    Bump version to 2.0.0-rc1

--- src/SimpleSAML/Configuration.php
+++ src/SimpleSAML/Configuration.php
@@ -42,7 +42,7 @@ class Configuration implements Utils\ClearableState
     /**
      * The release version of this package
      */
-    public const VERSION = 'master';
+    public const VERSION = '2.0.0-rc1';
 
     /**
      * A default value which means that the given option is required.
