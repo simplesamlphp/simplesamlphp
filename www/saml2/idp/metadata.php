85b6327bfe5931e90233793d8f5200f82a93bf6e
Dick Visser <dick.visser@geant.org>
2022-07-20 10:36:28 +0200

    Emit E-tag header for IdP metadata

--- www/saml2/idp/metadata.php
+++ www/saml2/idp/metadata.php
@@ -243,6 +243,7 @@ try {
         $t->show();
     } else {
         header('Content-Type: application/samlmetadata+xml');
+        header('E-tag: ' . hash('sha256', $metaxml));
 
         echo $metaxml;
         exit(0);
