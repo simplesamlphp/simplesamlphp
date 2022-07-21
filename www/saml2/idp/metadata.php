5d1eafedba4931f1832385178f1e12bffeb17143
Dick Visser <dick.visser@geant.org>
2022-07-21 16:40:57 +0200

    Simplify if else

--- www/saml2/idp/metadata.php
+++ www/saml2/idp/metadata.php
@@ -246,16 +246,11 @@ try {
         if(isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
             if($_SERVER['HTTP_IF_NONE_MATCH'] === $etag) {
                 header("HTTP/1.1 304 Not Modified");
-            } else {
-                header('Content-Type: application/samlmetadata+xml');
-                header('ETag: ' . $etag);
-                echo $metaxml;
-            }
-        } else {
-            header('Content-Type: application/samlmetadata+xml');
-            header('ETag: ' . $etag);
-            echo $metaxml;
+                exit(0);
         }
+        header('Content-Type: application/samlmetadata+xml');
+        header('ETag: ' . $etag);
+        echo $metaxml;
         exit(0);
     }
 } catch (\Exception $exception) {
