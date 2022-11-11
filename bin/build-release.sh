de7bd77957bf7716e2315e4b14c3f9f8afe619a7
Tim van Dijen <tvdijen@gmail.com>
2022-11-11 15:29:43 +0100

    Change build-script to leverage composer archive

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -21,6 +21,7 @@ fi
 
 TAG="v$VERSION"
 TARGET="simplesamlphp-$VERSION"
+COMPOSER="/tmp/composer.phar"
 
 cd /tmp
 
@@ -34,39 +35,28 @@ umask 0022
 git clone $REPOPATH $TARGET
 cd $TARGET
 git checkout $TAG
-cd ..
 
-if [ ! -x "$TARGET/composer.phar" ]; then
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=$TARGET
+if [ ! -x "$COMPOSER" ]; then
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp
 fi
 
 # Set the version in composer.json
-php "$TARGET/composer.phar" config version "v$VERSION" -d "$TARGET"
+php "$COMPOSER" config version "v$VERSION"
 
 # Install dependencies (without vcs history or dev tools)
-php "$TARGET/composer.phar" install --no-dev --prefer-dist -o -d "$TARGET"
+php "$COMPOSER" install --no-dev --prefer-dist -o
 
-cd $TARGET
 npm install
 npm audit fix
 npx browserslist@latest --update-db
 npm run build
-cd ..
 
-mkdir -p "$TARGET/config" "$TARGET/metadata" "$TARGET/cert" "$TARGET/log" "$TARGET/data"
-cp -rv "$TARGET/config-templates/"* "$TARGET/config/"
-cp -rv "$TARGET/metadata-templates/"* "$TARGET/metadata/"
-rm -rf "$TARGET/.git"
-rm -rf "$TARGET/node_modules"
-rm "$TARGET/www/assets/js/stylesheet.js"*
-rm "$TARGET/.editorconfig"
-rm "$TARGET/.gitattributes"
-rm -r "$TARGET/.github"
-rm "$TARGET"/phpunit.xml
-rm "$TARGET"/{cache,config,metadata,locales}/.gitkeep
-rm "$TARGET/composer.phar"
-rm "$TARGET/bin/build-release.sh"
-tar --owner 0 --group 0 -cvzf "$TARGET.tar.gz" "$TARGET"
-rm -rf "$TARGET"
+mkdir -p "config" "metadata"
+cp -rv "config-templates/"* "config/"
+cp -rv "metadata-templates/"* "metadata/"
+rm {cache,config,metadata,locales}/.gitkeep
+rm bin/build-release.sh
 
+php "$COMPOSER" archive -f tar.gz --dir /tmp --file "$TARGET"
+rm "$COMPOSER"
 echo `shasum -a 256 /tmp/$TARGET.tar.gz`
