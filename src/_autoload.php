ba9472d54917caecf8670ad62554f31e53de577b
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-01-03 10:50:11 +0100

    Reduce autoload wrapper complexity

--- src/_autoload.php
+++ src/_autoload.php
@@ -12,11 +12,9 @@ declare(strict_types=1);
 // SSP is loaded as a separate project
 if (file_exists(dirname(__FILE__, 2) . '/vendor/autoload.php')) {
     require_once dirname(__FILE__, 2) . '/vendor/autoload.php';
+} elseif (file_exists(dirname(__FILE__, 2) . '/../../autoload.php')) {
+    // SSP is loaded as a library.
+    require_once dirname(__FILE__, 2) . '/../../autoload.php';
 } else {
-    // SSP is loaded as a library
-    if (file_exists(dirname(__FILE__, 2) . '/../../autoload.php')) {
-        require_once dirname(__FILE__, 2) . '/../../autoload.php';
-    } else {
-        throw new Exception('Unable to load Composer autoloader');
-    }
+    throw new Exception('Unable to load Composer autoloader');
 }
