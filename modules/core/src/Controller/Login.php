e4bb90bd0b6f3f66b4f472da74da3167e877d5f5
Thijs Kinkhorst <thijs@kinkhorst.com>
2023-02-28 19:07:50 +0000

    Fix loginpage_links feature.
    It seems with the addition of twig templates another way to process
    these links (or other links) has been started but which is broken. The
    original way still remains in the code. Make it clearer which links this
    concerns by making the template variable more explicit and remove the
    unused tabs code.
    
    Closes: #1770

--- modules/core/src/Controller/Login.php
+++ modules/core/src/Controller/Login.php
@@ -291,7 +291,7 @@ class Login
                 $t->data['organizations'] = $organizations;
             }
         } else {
-            $t->data['links'] = $source->getLoginLinks();
+            $t->data['loginpage_links'] = $source->getLoginLinks();
         }
 
         $t->data['errorcode'] = $errorCode;
