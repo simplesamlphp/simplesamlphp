3eba4f9d7f56eebe95d7a483373e00dae8363412
Tim van Dijen <tvdijen@gmail.com>
2023-02-28 16:08:21 +0100

    Remove unnecessary JS

--- modules/core/public/assets/js/loginuserpass.js
+++ modules/core/public/assets/js/loginuserpass.js
@@ -1,6 +1,5 @@
 ready(function () {
     var button = document.getElementById("submit_button");
-    var form = document.getElementById("f");
 
     window.onpageshow  = function () {
         var button = document.getElementById("submit_button");
@@ -13,11 +12,5 @@ ready(function () {
         button.innerHTML = button.getAttribute("data-processing");
         button.disabled = true;
     }
-
-    button.onclick = function () {
-        var form = document.getElementById("f");
-        form.submit();
-        return true;
-    };
 });
 
