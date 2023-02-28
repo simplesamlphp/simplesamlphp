da2865028f65800fa7690b7091cbed3efad8be9b
Tim van Dijen <tvdijen@gmail.com>
2023-02-28 18:12:14 +0100

    Fix

--- modules/core/public/assets/js/loginuserpass.js
+++ modules/core/public/assets/js/loginuserpass.js
@@ -1,12 +1,11 @@
 ready(function () {
-    var button = document.getElementById("submit_button");
-
     window.onpageshow  = function () {
         var button = document.getElementById("submit_button");
         button.innerHTML = button.getAttribute('data-default');
         button.disabled = false;
     }
 
+    var form = document.getElementById("f");
     form.onsubmit = function () {
         var button = document.getElementById("submit_button");
         button.innerHTML = button.getAttribute("data-processing");
