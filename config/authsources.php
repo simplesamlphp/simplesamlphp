a23aee6c823c284de8113172e2e631ff7371baed
Pat Stapleton <patrick.stapleton@pursuittechnology.com>
2023-06-23 13:02:03 +1000

    Adding in cfg for ssp

--- config/authsources.php
+++ config/authsources.php
@@ -1,5 +1,10 @@
 <?php
+
 global $DB, $CFG;
+
+$sendEmail = $DB->record_exists_sql("SELECT * FROM ac_simplesamlphp_config WHERE name = 'sendEmail' AND value IS NOT NULL");
+$sendUsername = $DB->record_exists_sql("SELECT * FROM ac_simplesamlphp_config WHERE name = 'sendUsername' AND value IS NOT NULL");
+$emailDomain = $DB->record_exists_sql("SELECT * FROM ac_simplesamlphp_config WHERE name = 'emailDomain' AND value IS NOT NULL");
 
 $config = [
     'admin' => [
@@ -7,7 +12,12 @@ $config = [
     ],
     'acorn-default' => [
         'acorn:AcornAuth',
-        'acornDirectory' => $CFG->dirroot
+        'acornDirectory' => $CFG->dirroot,
+        'opts' => [
+            'sendEmail' => $sendEmail,
+            'sendUsername' => $sendUsername,
+            'emailDomain' => (!empty($emailDomain) ? $emailDomain->value : null)
+        ]
     ]
 ];
 
