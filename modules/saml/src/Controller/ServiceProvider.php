83aa7561b52606b1a16921adba4e2444ffb45f7f
Tim van Dijen <tvdijen@gmail.com>
2023-08-11 20:37:45 +0200

    Fix admin.protectmetadata - The metadata endpoints would show a blank page instead of an admin-login page

--- modules/saml/src/Controller/ServiceProvider.php
+++ modules/saml/src/Controller/ServiceProvider.php
@@ -613,7 +613,7 @@ class ServiceProvider
      */
     public function metadata(Request $request, string $sourceId): Response
     {
-        if ($this->config->getOptionalBoolean('admin.protectmetadata', false)) {
+        if ($this->config->getOptionalBoolean('admin.protectmetadata', false) && !$this->authUtils->isAdmin()) {
             return new RunnableResponse([$this->authUtils, 'requireAdmin']);
         }
 
