b99f8ee2f868d75e0ab7e1dd6758a63f6eae767d
Tim van Dijen <tvdijen@gmail.com>
2023-09-02 11:25:03 +0200

    phpcs: Fix long line

--- src/SimpleSAML/Configuration.php
+++ src/SimpleSAML/Configuration.php
@@ -52,7 +52,16 @@ class Configuration implements Utils\ClearableState
      * The default security-headers to be sent on responses.
      */
     public const DEFAULT_SECURITY_HEADERS = [
-        'Content-Security-Policy' => "default-src 'none'; frame-ancestors 'self'; object-src 'none'; script-src 'self'; style-src 'self'; font-src 'self'; connect-src 'self'; img-src 'self' data:; base-uri 'none'",
+        'Content-Security-Policy' =>
+            "default-src 'none'; " .
+            "frame-ancestors 'self'; " .
+            "object-src 'none'; " .
+            "script-src 'self'; " .
+            "style-src 'self'; " .
+            "font-src 'self'; " .
+            "connect-src 'self'; " .
+            "img-src 'self' data:; " .
+            "base-uri 'none'",
         'X-Frame-Options' => 'SAMEORIGIN',
         'X-Content-Type' => 'nosniff',
         'Referer-Policy' => 'origin-when-cross-origin',
