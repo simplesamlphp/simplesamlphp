e63703b3c9f65eed8e48539451b900c39fb6a8ca
Tim van Dijen <tvdijen@gmail.com>
2023-09-04 22:06:37 +0200

    Revert CommandLineKernel; create separate script for translations

--- src/SimpleSAML/Console/Application.php
+++ src/SimpleSAML/Console/Application.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Console;
 
-use SimpleSAML\CommandLineKernel;
+use SimpleSAML\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputOption;
 
 class Application extends BaseApplication
 {
     /**
-     * @param \SimpleSAML\CommandLineKernel $kernel
+     * @param \SimpleSAML\Kernel $kernel
      */
-    public function __construct(CommandLineKernel $kernel)
+    public function __construct(Kernel $kernel)
     {
         parent::__construct($kernel);
+
+        $inputDefinition = $this->getDefinition();
+        $inputDefinition->addOption(
+            new InputOption('--module', '-m', InputOption::VALUE_REQUIRED, 'The module name', $kernel->getModule())
+        );
     }
 }
