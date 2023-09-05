d9a15e0853b3da2b414323aed1c3def0b1e42776
Tim van Dijen <tvdijen@gmail.com>
2023-09-05 11:55:16 +0200

    Remove code no longer in use

--- src/SimpleSAML/Utils/Translate.php
+++ src/SimpleSAML/Utils/Translate.php
@@ -4,41 +4,9 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Utils;
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use SimpleSAML\Configuration;
-use SimpleSAML\XHTML\Template;
-
 /**
  * @package SimpleSAMLphp
  */
 class Translate
 {
-    /**
-     * Compile all Twig templates for the given $module into the given $outputDir.
-     * This is used by the translation extraction tool to find the translatable
-     * strings for this module in the compiled templates.
-     * $module can be '' for the main SimpleSAMLphp templates.
-     */
-    public function compileAllTemplates(string $module, string $outputDir): void
-    {
-        $config = Configuration::loadFromArray(['template.cache' => $outputDir, 'module.enable' => [$module => true]]);
-        $baseDir = $config->getBaseDir();
-        $tplSuffix = DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
-
-        $tplDir = $baseDir . ($module === '' ? '' : 'modules' . DIRECTORY_SEPARATOR . $module) . $tplSuffix;
-        $templateprefix = ($module === '' ? '' : $module . ":");
-
-        foreach (
-            new RecursiveIteratorIterator(
-                new RecursiveDirectoryIterator($tplDir),
-                RecursiveIteratorIterator::LEAVES_ONLY
-            ) as $file
-        ) {
-            if ($file->isFile()) {
-                $p = new Template($config, $templateprefix . str_replace($tplDir, '', $file->getPathname()));
-                $p->compile();
-            }
-        }
-    }
 }
