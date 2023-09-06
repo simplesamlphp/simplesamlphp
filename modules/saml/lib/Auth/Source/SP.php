a29667ecd05b87e81d86f742b6f7ba479288ca59
Ralph van den Houdt <ralphvandenhoudt@users.noreply.github.com>
2023-09-06 11:09:53 +0200

    [PATCH] Add IDPList parameter that overrides SP and IDP metadata (#1859)

--- modules/saml/lib/Auth/Source/SP.php
+++ modules/saml/lib/Auth/Source/SP.php
@@ -651,6 +651,9 @@ class SP extends \SimpleSAML\Auth\Source
                 )
             )
         );
+        if (isset($state['IDPList'])) {
+            $ar->setIDPList($state['IDPList']);
+        }
 
         $ar->setRequesterID($requesterID);
 
