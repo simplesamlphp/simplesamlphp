6cd611adf907ca0fb83cf58bd52705fa75495f23
Tim van Dijen <tvdijen@gmail.com>
2023-12-14 16:05:00 +0100

    Fix rebase-issue

--- tests/modules/core/src/Auth/Process/CardinalitySingleTest.php
+++ tests/modules/core/src/Auth/Process/CardinalitySingleTest.php
@@ -45,13 +45,7 @@ class CardinalitySingleTest extends TestCase
     protected function setUp(): void
     {
         Configuration::loadFromArray([], '[ARRAY]', 'simplesaml');
-<<<<<<< HEAD
-        $this->httpUtils = $this->getMockBuilder(Utils\HTTP::class)
-                           ->setMethods(['redirectTrustedURL'])
-                           ->getMock();
-=======
         $this->httpUtils = $this->createStub(Utils\HTTP::class);
->>>>>>> d7b3e17e7 (Fix unit tests)
     }
 
 
