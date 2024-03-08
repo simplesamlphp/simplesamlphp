3b83a443b741b383751bce119cbd9e2d2a76fb17
Tim van Dijen <tvdijen@gmail.com>
2024-03-08 12:37:36 +0100

    Release v2.2.0

--- src/SimpleSAML/Configuration.php
+++ src/SimpleSAML/Configuration.php
@@ -40,7 +40,7 @@ class Configuration implements Utils\ClearableState
     /**
      * The release version of this package
      */
-    public const VERSION = '2.1.0-rc1';
+    public const VERSION = '2.2.0';
 
     /**
      * A default value which means that the given option is required.
