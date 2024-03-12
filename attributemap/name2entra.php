07b64f74371651912560e42914a229e0ed546ee8
Matt Russell <41727084+Whats-A-MattR@users.noreply.github.com>
2024-03-12 13:20:53 +0000

    updated comments

--- attributemap/name2entra.php
+++ attributemap/name2entra.php
@@ -2,7 +2,7 @@
 
 $attributemap = [
     /**
-     * Renamed Attributes to match other 2name mappings
+     * Renamed Attributes to match other 2name mappings, mapped back to default Entra claim names
      */
     'uid' => 'http://schemas.microsoft.com/identity/claims/objectidentifier',
     'displayName' => 'http://schemas.microsoft.com/identity/claims/displayname',
@@ -12,12 +12,12 @@ $attributemap = [
     'mail' => 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name',
     
     /**
-     * Additional/Optional Claim, using default value
+     * Additional/Optional Claim, using default value, mapped back to original claim names
      */
     'groups' => 'http://schemas.microsoft.com/ws/2008/06/identity/claims/groups',
 
     /**
-     * Additional Attributes from Entra
+     * Additional Attributes from Entra, mapped back to original claims
      */
     'authNMethodsReferences' => 'http://schemas.microsoft.com/claims/authnmethodsreferences',
     'idp' => 'http://schemas.microsoft.com/identity/claims/identityprovider',
