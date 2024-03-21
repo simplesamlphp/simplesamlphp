8f2dccc3d446cdff64195c6eb3d34ec9cf2b5913
Ben Martin <monkeyiq@users.sourceforge.net>
2024-03-21 10:04:47 +1000

    update for php code sniffer

--- src/SimpleSAML/Metadata/MetaDataStorageSource.php
+++ src/SimpleSAML/Metadata/MetaDataStorageSource.php
@@ -102,7 +102,7 @@ abstract class MetaDataStorageSource
             case 'pdo':
                 return new MetaDataStorageHandlerPdo($config, $sourceConfig);
             case 'directory':
-                return new MetaDataStorageHandlerDirectory($config,$sourceConfig);
+                return new MetaDataStorageHandlerDirectory($config, $sourceConfig);
             default:
                 // metadata store from module
                 try {
