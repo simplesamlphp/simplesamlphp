84319260fbf2d99ea49c1e253f72045d7b75a054
Tim van Dijen <tvdijen@gmail.com>
2024-04-09 10:48:32 +0200

    Fix RequestedAuthnContextSelector for case with multiple RACs in request

--- modules/core/src/Auth/Source/RequestedAuthnContextSelector.php
+++ modules/core/src/Auth/Source/RequestedAuthnContextSelector.php
@@ -142,7 +142,7 @@ class RequestedAuthnContextSelector extends AbstractSourceSelector
                             return $context['source'];
                         }
                     }
-                    break 2;
+                    break 1;
                 case 'minimum':
                 case 'maximum':
                 case 'better':
