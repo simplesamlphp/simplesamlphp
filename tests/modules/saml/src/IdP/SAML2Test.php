a091112e8e385bcfb744ebd2545407d9b524e98b
Tim van Dijen <tvdijen@gmail.com>
2024-04-26 09:55:09 +0200

    autofill username when it's hinted by the SP in a request parameter (MS Entra ID)

--- tests/modules/saml/src/IdP/SAML2Test.php
+++ tests/modules/saml/src/IdP/SAML2Test.php
@@ -47,7 +47,8 @@ class SAML2Test extends ClearStateTestCase
         'saml:NameIDFormat' => null,
         'saml:AllowCreate' => true,
         'saml:Extensions' => null,
-        'saml:RequestedAuthnContext' => null
+        'saml:RequestedAuthnContext' => null,
+        'core:username' => null,
     ];
 
 
