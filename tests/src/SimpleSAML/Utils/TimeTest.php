92a0b969e95b0aeafd772775e239a9374b35122a
Tim van Dijen <tvdijen@gmail.com>
2024-05-09 18:26:09 +0200

    Perform validation of the duration before trying to parse it

--- tests/src/SimpleSAML/Utils/TimeTest.php
+++ tests/src/SimpleSAML/Utils/TimeTest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace SimpleSAML\Test\Utils;
 
-use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
+use SimpleSAML\Assert\AssertionFailedException;
 use SimpleSAML\{Configuration, Error, Utils};
 
 use function date_default_timezone_get;
@@ -178,15 +178,15 @@ class TimeTest extends TestCase
             // invalid string
             $timeUtils->parseDuration('abcdefg');
             $this->fail("Did not fail with invalid ISO 8601 duration.");
-        } catch (InvalidArgumentException $e) {
-            $this->assertStringStartsWith('Invalid ISO 8601 duration: ', $e->getMessage());
+        } catch (AssertionFailedException $e) {
+            $this->assertStringMatchesFormat('\'%s\' is not a valid xs:duration', $e->getMessage());
         }
         try {
             // missing T delimiter
             $timeUtils->parseDuration('P1S');
             $this->fail("Did not fail with duration missing T delimiter.");
-        } catch (InvalidArgumentException $e) {
-            $this->assertStringStartsWith('Invalid ISO 8601 duration: ', $e->getMessage());
+        } catch (AssertionFailedException $e) {
+            $this->assertStringMatchesFormat('\'%s\' is not a valid xs:duration', $e->getMessage());
         }
     }
 }
