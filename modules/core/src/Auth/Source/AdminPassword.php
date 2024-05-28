dbf1d49648547b4dddf9dc22772115cae3a6a774
Ben Martin <monkeyiq@users.sourceforge.net>
2024-05-29 08:34:52 +1000

    === update

--- modules/core/src/Auth/Source/AdminPassword.php
+++ modules/core/src/Auth/Source/AdminPassword.php
@@ -59,7 +59,7 @@ class AdminPassword extends UserPassBase
         }
 
         $pwinfo = password_get_info($adminPassword);
-        if (!$pwinfo['algo']) {
+        if ($pwinfo['algo'] === null) {
             throw new Error\Error(Error\ErrorCodes::ADMINNOTHASHED);
         }
 
