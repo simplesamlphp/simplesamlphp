d151a89c3835bd68fa6e86c846b162906d92fae4
Ben Martin <monkeyiq@users.sourceforge.net>
2024-07-26 09:39:20 +1000

    style

--- tests/modules/core/src/Controller/LoginTest.php
+++ tests/modules/core/src/Controller/LoginTest.php
@@ -229,7 +229,7 @@ class LoginTest extends ClearStateTestCase
 
         // Get the default authsources and add a specific configuration
         // of testsauthsource:ThrowCustomErrorCode for this test
-        $config = array();
+        $config = [];
         $config['testsauthsource-ThrowCustomErrorCode'] =  array_merge(
             ['testsauthsource:ThrowCustomErrorCode'],
             $asConfig,
