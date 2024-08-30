6d72259fa32d0738b35c55e984d789acbb1cac9d
monkeyiq <monkeyiq@gmail.com>
2024-08-31 08:53:54 +1000

    plain text admin password follow-up (#2223)

--- modules/core/src/Auth/Source/AdminPassword.php
+++ modules/core/src/Auth/Source/AdminPassword.php
@@ -6,6 +6,7 @@ namespace SimpleSAML\Module\core\Auth\Source;
 
 use SimpleSAML\Configuration;
 use SimpleSAML\Error;
+use SimpleSAML\Logger;
 use SimpleSAML\Module\core\Auth\UserPassBase;
 use Symfony\Component\PasswordHasher\Hasher\NativePasswordHasher;
 
@@ -65,6 +66,9 @@ class AdminPassword extends UserPassBase
             // Continue to allow admin login when the config contains
             // a password that is not hashed
             if ($adminPassword === $password) {
+                Logger::deprecated('Please consider hashing the admin password stored in auth.adminpassword'
+                                 . ' in config.php. Using a plain text password in that config setting'
+                                 . ' will be removed in the future.');
                 return ['user' => ['admin']];
             }
             throw new Error\Error(Error\ErrorCodes::ADMINNOTHASHED);
