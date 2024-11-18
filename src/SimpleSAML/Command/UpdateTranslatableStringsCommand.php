a48b7f3dfe3375d1ba94af7b69dc63a667844155
Guy Halse <ghalse@users.noreply.github.com>
2024-11-18 14:54:02 +0200

    Make translations tool theme-aware (#2315)
    * Make translation tools theme-aware
    
    * Add support for theme.controller (needed to render some module templates)

--- src/SimpleSAML/Command/UpdateTranslatableStringsCommand.php
+++ src/SimpleSAML/Command/UpdateTranslatableStringsCommand.php
@@ -63,6 +63,19 @@ class UpdateTranslatableStringsCommand extends Command
             InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
             'Which modules to perform this action on',
         );
+        $this->addOption(
+            'themes',
+            null,
+            InputOption::VALUE_NEGATABLE,
+            'Include/exclude templates from themes directories of modules (default --no-themes)',
+            false,
+        );
+        $this->addOption(
+            'theme-controller',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Specify a custom TemplateControllerInterface class (usually for use with themes)',
+        );
     }
 
     /**
@@ -127,6 +140,17 @@ class UpdateTranslatableStringsCommand extends Command
         $phpScanner = new PhpScanner(...$translationDomains);
         $phpScanner->setFunctions(['trans' => 'gettext', 'noop' => 'gettext']);
 
+        if ($input->getOption('theme-controller') !== null) {
+            Configuration::setPreLoadedConfig(
+                Configuration::loadFromArray(
+                    array_merge(
+                        Configuration::getInstance()->toArray(),
+                        ['theme.controller' => $input->getOption('theme-controller'),],
+                    ),
+                ),
+            );
+        }
+
         $translationUtils = new Utils\Translate(Configuration::getInstance());
         $twigTranslations = [];
         // Scan files in base
@@ -138,7 +162,10 @@ class UpdateTranslatableStringsCommand extends Command
             $phpScanner = $translationUtils->getTranslationsFromPhp($module, $phpScanner);
 
             // Scan Twig-templates
-            $twigTranslations = array_merge($twigTranslations, $translationUtils->getTranslationsFromTwig($module));
+            $twigTranslations = array_merge(
+                $twigTranslations,
+                $translationUtils->getTranslationsFromTwig($module, $input->getOption('themes')),
+            );
         }
 
         // The catalogue returns an array with strings, while the php-scanner returns Translations-objects.
--- src/SimpleSAML/Utils/Translate.php
+++ src/SimpleSAML/Utils/Translate.php
@@ -6,10 +6,19 @@ namespace SimpleSAML\Utils;
 
 use Gettext\Scanner\PhpScanner;
 use SimpleSAML\Configuration;
+use SimpleSAML\Error\Exception;
+use SimpleSAML\Module;
 use SimpleSAML\XHTML\Template;
 use Symfony\Bridge\Twig\Translation\TwigExtractor;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Translation\MessageCatalogue;
+
+use function array_merge;
+use function explode;
+use function is_dir;
+use function str_starts_with;
+use function strlen;
+use function substr;
 
 /**
  * @package SimpleSAMLphp
@@ -45,19 +54,54 @@ class Translate
     }
 
 
-    public function getTranslationsFromTwig(string $module): array
+    public function getTranslationsFromTwig(string $module, bool $includeThemes = false): array
     {
         $twigTranslations = [];
         $moduleDir = $this->baseDir . ($module === '' ? '' : 'modules/' . $module . '/');
         $moduleTemplateDir = $moduleDir . 'templates/';
+        $moduleThemeDir = $moduleDir . 'themes/';
+        $moduleDirs = [];
+        if (is_dir($moduleTemplateDir)) {
+            $moduleDirs[] = $moduleTemplateDir;
+        }
+        if ($includeThemes && is_dir($moduleThemeDir)) {
+            $moduleDirs[] = $moduleThemeDir;
+        }
 
         // Scan Twig-templates
         $finder = new Finder();
-        foreach ($finder->files()->in($moduleTemplateDir)->name('*.twig') as $file) {
-            $template = new Template(
-                $this->configuration,
-                ($module ? ($module . ':') : '') . $file->getRelativePathname(),
-            );
+        foreach ($finder->files()->in($moduleDirs)->name('*.twig') as $file) {
+            if (!($includeThemes && str_starts_with($file->getPathname(), $moduleThemeDir))) {
+                /* process templates/ directory */
+                $template = new Template(
+                    $this->configuration,
+                    ($module ? ($module . ':') : '') . $file->getRelativePathname(),
+                );
+            } else {
+                /* process themed templates from other modules */
+                list($theme, $themedModule) = explode(
+                    DIRECTORY_SEPARATOR,
+                    substr($file->getPath(), strlen($moduleThemeDir)),
+                    2,
+                );
+                if ($themedModule !== 'default' && !Module::isModuleEnabled($themedModule)) {
+                    throw new Exception(
+                        'The module \'' . $themedModule . '\' (themed by \'' . $module . ':' . $theme . '\') ' .
+                        'is not enabled. Perhaps you need to need to require --dev simplesamlphp-module-' .
+                        $themedModule . ' within the ' . $module . ' module?',
+                    );
+                }
+                $template = new Template(
+                    Configuration::loadFromArray(
+                        array_merge(
+                            $this->configuration->toArray(),
+                            ['theme.use' => $module . ':' . $theme,],
+                        ),
+                    ),
+                    ($themedModule !== 'default' ? ($themedModule . ':') : '') .
+                    substr($file->getRelativePathname(), strlen($theme . DIRECTORY_SEPARATOR . $themedModule) + 1),
+                );
+            }
 
             $catalogue = new MessageCatalogue('en', []);
             $extractor = new TwigExtractor($template->getTwig());
