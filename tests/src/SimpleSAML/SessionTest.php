41a0432943e87eceb7c7f437f69f3ab38f5feedf
Ioannis Igoumenos <ioigoume@gmail.com>
2025-03-11 11:35:01 +0200

    Fix typo. Take into account the Session::DATA_TIMEOUT_SESSION_END

--- tests/src/SimpleSAML/SessionTest.php
+++ tests/src/SimpleSAML/SessionTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace SimpleSAML\Test;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use SimpleSAML\{Configuration, Session};
 use PHPUnit\Framework\Attributes\DataProvider;
 use SimpleSAML\TestUtils\ClearStateTestCase;
+use SimpleSAML\{Configuration, Session};
 
 /**
  */
@@ -49,11 +49,50 @@ class SessionTest extends ClearStateTestCase
         $this->assertEquals(time() + 1000, $this->session->getRememberMeExpire());
     }
 
+    public static function expirationValues(): array
+    {
+        return [
+            'Integer' => [60],
+            'String (Session End)' => [Session::DATA_TIMEOUT_SESSION_END],
+            'String (Any)' => ['Value'],
+        ];
+    }
+
+    #[DataProvider('expirationValues')]
+    public function testSetDataExpiration(int|string $expire): void
+    {
+        if ($expire === 'Value') {
+            $this->expectException(\UnexpectedValueException::class);
+            $this->expectExceptionMessage(
+                "Expected a value identical to \"sessionEndTimeout\". Got: \"Value\"",
+            );
+        }
+        $this->session->setData('testType', 'testKey', 'data', $expire);
+        if ($expire !== 'Value') {
+            $fetchedData = $this->session->getData('testType', 'testKey');
+            $this->assertEquals('data', $fetchedData);
+        }
+    }
+
     public static function getAllowedExpired(): array
     {
         return [
-            'Fetch Expired Entries' => [true],
-            'Do not Fetcn Expired Entries' => [false],
+            'Enable Expired with Expired Entries' => [
+                true,
+                -3600,
+            ],
+            'Enabled Expired with Never Expiring Entries' => [
+                true,
+                Session::DATA_TIMEOUT_SESSION_END,
+            ],
+            'Disable Expired with Expired Entries' => [
+                false,
+                -3600,
+            ],
+            'Disable Expired with Never Expiring Entries' => [
+                false,
+                Session::DATA_TIMEOUT_SESSION_END,
+            ],
         ];
     }
 
@@ -62,12 +101,12 @@ class SessionTest extends ClearStateTestCase
      * @throws \Exception
      */
     #[DataProvider('getAllowedExpired')]
-    public function testGetDataWithExpiredSessionKey(bool $allowedExpired): void
+    public function testGetDataWithExpiredSessionKey(bool $allowedExpired, string|int $timeout): void
     {
         // Set expiration of the testKey in the past
-        $this->session->setData('testType', 'testKey', 'data', -3600);
+        $this->session->setData('testType', 'testKey', 'data', $timeout);
         $fetchedData = $this->session->getData('testType', 'testKey', $allowedExpired);
-        if ($allowedExpired) {
+        if ($allowedExpired || $timeout === Session::DATA_TIMEOUT_SESSION_END) {
             $this->assertEquals('data', $fetchedData);
         } else {
             $this->assertNull($fetchedData);
