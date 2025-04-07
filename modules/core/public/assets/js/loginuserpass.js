51cc845b89f9c178345395b2a52126b1e9d26680
Tim van Dijen <tvdijen@gmail.com>
2025-04-07 23:17:19 +0200

    Fix linter-issues

--- modules/core/public/assets/js/loginuserpass.js
+++ modules/core/public/assets/js/loginuserpass.js
@@ -4,7 +4,7 @@ ready(function () {
         var replacement = document.createTextNode(button.getAttribute("data-default"));
         button.replaceChild(replacement, button.childNodes[0]);
         button.disabled = false;
-    }
+    };
 
     var form = document.getElementById("f");
     form.onsubmit = function () {
@@ -12,6 +12,6 @@ ready(function () {
         var replacement = document.createTextNode(button.getAttribute("data-processing"));
         button.replaceChild(replacement, button.childNodes[0]);
         button.disabled = true;
-    }
+    };
 });
 
