e581b017ee88291197fee457f6f88413957fcfea
1katoda <38652146+1katoda@users.noreply.github.com>
2025-05-06 00:45:21 +0200

    [#2422] Add empty checks to $_SERVER variables in SimpleSAML\Utils\HTTP (#2423)
    * add empty checks to $_SERVER variables
    
    * remove redundant key check
    
    * check current SERVER_NAME for null, revert other new empty checks
    
    * Slight cleanup
    
    ---------
    
    Co-authored-by: Tim van Dijen <tvdijen@gmail.com>

--- src/SimpleSAML/Utils/HTTP.php
+++ src/SimpleSAML/Utils/HTTP.php
@@ -150,11 +150,14 @@ class HTTP
      */
     private function getServerHost(): string
     {
+        $current = null;
         if (array_key_exists('HTTP_HOST', $_SERVER)) {
             $current = $_SERVER['HTTP_HOST'];
         } elseif (array_key_exists('SERVER_NAME', $_SERVER)) {
             $current = $_SERVER['SERVER_NAME'];
-        } else {
+        }
+
+        if (is_null($current)) {
             // almost certainly not what you want, but...
             $current = 'localhost';
         }
