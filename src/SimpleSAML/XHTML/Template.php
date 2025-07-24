0166234d0156f529eb9fc3bafa5ef93273fcb3ab
Ben Martin <monkeyiq@users.sourceforge.net>
2025-07-24 17:44:33 +1000

    Simplify tag input to the mtime/content of the asset itself

--- src/SimpleSAML/XHTML/Template.php
+++ src/SimpleSAML/XHTML/Template.php
@@ -208,20 +208,8 @@ class Template extends Response
         }
 
         $file = new File($file);
+        $tag = $this->getAssetTagForFile($file);
 
-        $tag = $this->configuration->getVersion();
-        if ($module !== null) {
-            // Modules can be updated more frequently than the core. Especially the custom ones.
-            // As a result, we will use a different tagging method
-            $composerLock = new File($baseDir . 'composer.lock');
-            $tag .= $this->getAssetTagForFile($composerLock);
-        }
-
-        if ($tag === 'master') {
-            $tag = strval($file->getMtime());
-        } else {
-            $tag .= $this->getAssetTagForFile($file);
-        }
         // Use the `secretsalt` to enhance security.
         // Do not make it easy to guess the underlying SSP version.
         $salt = $this->configuration->getOptionalString('assets.secretsalt', 'default.assets.secretsalt');
