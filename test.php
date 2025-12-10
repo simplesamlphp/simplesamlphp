e9b1651fe560ee02f147c5a975ab4eea7cde43df
Tim van Dijen <tvdijen@gmail.com>
2025-12-10 23:16:50 +0100

    Remove accidentally committed test-file

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include('vendor/autoload.php');
-
-$domain = 'messages';
-$finder = new Symfony\Component\Finder\Finder();
-$moduleLocalesDir = './modules/admin/locales/';
-$finder->files()->in($moduleLocalesDir . '**/LC_MESSAGES/')->name("{$domain}.po");
