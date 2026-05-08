fbbacbacce360adcb2e54b7a1a7c3dbe17b9a41d
Marko Ivančić <mivanci@srce.hr>
2026-05-08 13:01:22 +0200

    Move public entry points to ObsoleteRouteController

---
+++ src/SimpleSAML/Controller/ObsoleteRouteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SimpleSAML\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ObsoleteRouteController
+{
+    public function __invoke(?string $message = null, int $statusCode = 410): Response
+    {
+        $message ??= 'This route is not used anymore.';
+
+        return new Response(
+            $message,
+            $statusCode,
+            ['Content-Type' => 'text/plain; charset=UTF-8'],
+        );
+    }
+}
