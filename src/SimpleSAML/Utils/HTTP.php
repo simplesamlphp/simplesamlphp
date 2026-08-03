f5895171b360ae9cc3b45cebfaee5cf583c13e1e
ronaldPeer <ronaldPeer@users.noreply.github.com>
2026-08-03 14:25:49 +0200

    Update HTTP.php
    Fix a regression comming from https://github.com/simplesamlphp/simplesamlphp/commit/03092b86d0c369159b5edcbcb8256d3340bd1326
    
    At least make $proxy an array for array_merge when proxyAuth is not available

--- src/SimpleSAML/Utils/HTTP.php
+++ src/SimpleSAML/Utils/HTTP.php
@@ -46,11 +46,15 @@ class HTTP
         if ($proxy !== null) {
             $proxy = preg_replace('/^(tcp:\/\/)+/i', 'http://', $proxy);
             $proxyAuth = $config->getOptionalString('proxy.auth', null);
-            $scheme = parse_url($proxy, PHP_URL_SCHEME);
-
+            
             if ($proxyAuth !== null) {
+                $scheme = parse_url($proxy, PHP_URL_SCHEME);
                 $proxy = ['proxy' => str_replace($scheme . '://', $scheme . '://' . $proxyAuth . '@', $proxy)];
             }
+            else {
+                $proxy = ['proxy' => $proxy];
+            }
+            
             $options = array_merge($proxy, $options);
         }
 
