0eef3a2605ce7bef65b4846ba2ede66ed24826bc
Tim van Dijen <tvdijen@gmail.com>
2026-09-10 14:29:59 +0200

    Remove redundant backslash

--- src/SimpleSAML/Metadata/MetaDataStorageHandlerXML.php
+++ src/SimpleSAML/Metadata/MetaDataStorageHandlerXML.php
@@ -74,7 +74,7 @@ class MetaDataStorageHandlerXML extends MetaDataStorageSource
 
         // To prevent "type errors", check if we have a string to pass to SAMLParser at all.
         if (!is_string($srcXml)) {
-            throw new \Exception('Could not extract XML from metadata source.');
+            throw new Exception('Could not extract XML from metadata source.');
         }
 
         $entities = SAMLParser::parseDescriptorsString($srcXml);
